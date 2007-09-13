abf32a0f9f04ba45791ccac876f6698169304c4c
girlwithglasses <girlwithglasses@6f0e8829-b336-0410-acfb-cb9b228023ad>
2007-09-13 17:16:20 +0000

    Adding scripts

---
+++ go-dev/amigo/scripts/make_misc_key.pl
@@ -0,0 +1,175 @@
+#!/usr/local/bin/perl
+
+#generated key val pair for various options display in AmiGO
+
+use lib qw( /Library/WebServer/cvs/go-dev/new-amigo/perl /Library/WebServer/cvs/go-dev/go-perl /Library/WebServer/cvs/go-dev/go-db-perl );
+
+use FindBin qw($RealBin);
+use lib (($ENV{GO_ROOT})
+         ? "$ENV{GO_ROOT}/go-db-perl" : "$RealBin/../../go-db-perl");
+use lib (($ENV{GO_ROOT})
+         ? "$ENV{GO_ROOT}/amigo/perl" : "$RealBin/../perl");
+
+use strict;
+use GO::SqlWrapper qw(:all);
+use GO::CGI::Session;
+use FreezeThaw qw(freeze thaw);
+
+my $cgi_path = shift @ARGV;
+unless ($cgi_path) {
+    &help();
+    exit;
+}
+
+eval {
+    require "$cgi_path/config.pl";
+};
+my $session = new GO::CGI::Session(-no_update=>1);
+
+
+my $dbh = $session->apph->dbh;
+
+my $hash = {};
+
+#datasource = speciesdb
+my $hl = select_hashlist
+  ($dbh,
+   ["gene_product g", "association a", "dbxref xref"],
+   ["g.dbxref_id=xref.id", "g.id=a.gene_product_id"],
+   ["distinct xref.xref_dbname"],
+   "xref.xref_dbname", #order by
+  );
+#key = val
+#$hash->{speciesdb} = {map{$_->{xref_dbname}=>$_->{xref_dbname}}@{$hl || []}};
+$hash->{speciesdb} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{xref_dbname}), $_->{xref_dbname}) }
+	@{$hl || []}
+];
+
+
+#ontology
+$hl = select_hashlist
+  ($dbh,
+   ["term t", "term2term t2t"],
+   ["t.id=t2t.term1_id", "t.is_root <> 1"],
+   "distinct term_type",
+  );
+#key = val
+#$hash->{ontology} = {map{$_->{term_type}=>$_->{term_type}}@{$hl || []}};
+$hash->{ontology} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{term_type}), $_->{term_type}) }
+	@{$hl || []}
+];
+
+#reltype
+$hl = select_hashlist
+  ($dbh,
+   ["term t", "term2term t2t"],
+   ["t.id=t2t.relationship_type_id"],
+   "distinct t.name",
+  );
+#$hash->{reltype} = {map{$_->{name}=>$_->{name}}@{$hl || []}};
+$hash->{reltype} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{name}), $_->{name}) }
+	@{$hl || []}
+];
+
+#evcode
+$hl = select_hashlist
+  ($dbh,
+   ["association a", "evidence ev"],
+   ["a.id=ev.association_id"],
+   "distinct ev.code",
+  );
+#$hash->{evcode} = {map{$_->{code}=>$_->{code}}@{$hl || []}};
+$hash->{evcode} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{code}), $_->{code}) }
+	@{$hl || []}
+];
+
+#gptype
+$hl = select_hashlist
+  ($dbh,
+   ["gene_product gp", "term t"],
+   ["gp.type_id=t.id"],
+   "distinct t.name",
+  );
+#$hash->{gptype} = {map{$_->{name}=>$_->{name}}@{$hl || []}};
+$hash->{gptype} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{name}), $_->{name}) }
+	@{$hl || []}
+];
+
+#association qualifiers
+$hl = select_hashlist
+  ($dbh,
+   ["association_qualifier aq", "term t"],
+   ["aq.term_id=t.id"],
+   "distinct t.name",
+  );
+#$hash->{qual} = {map{$_->{name}=>$_->{name}}@{$hl || []}};
+$hash->{qual} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{name}), $_->{name}) }
+	@{$hl || []}
+];
+
+#assigned by
+$hl = select_hashlist
+  ($dbh,
+   ["association a", "db"],
+   ["a.source_db_id=db.id"],
+   "distinct db.name",
+  );
+#$hash->{assby} = {map{$_->{name}=>$_->{name}}@{$hl || []}};
+$hash->{assby} = [ 
+	map { (split("\0", $_))[1] }
+	sort 
+	map { join("\0", lc($_->{name}), $_->{name}) }
+	@{$hl || []}
+];
+
+##syn_type
+#$hl = select_hashlist
+# ($dbh,
+#   ["term_synonym ts", "term t"],
+#   ["ts.synonym_type_id=t.id"],
+#   "distinct t.name",
+#  );
+#$hash->{syn_type} = {map{$_->{name}=>$_->{name}}@{$hl || []}};
+
+if ($ENV{DEBUG}) {
+	use Data::Dumper;
+	$Data::Dumper::Indent = 1;
+	print STDERR Dumper($hash);
+#    map{printf STDERR "$_\n%s\n",join("\t",keys %{$hash->{$_}})}keys %$hash;
+}
+
+my $str = freeze $hash;
+my $f = "$cgi_path/misc_keys.pl";
+unlink $f if (-f $f);
+open(W, ">$f") or die "can not open $f: $!";
+print W $str;
+close(W);
+
+$dbh->disconnect;
+print STDERR "$0 Done\n";
+exit;
+
+sub help {
+    print <<EOM;
+  make_misc_key.pl amigo_cgi_dir
+    where amigo_cgi_dir is full_dir_path where go.cgi resides,
+EOM
+}
---
+++ go-dev/amigo/scripts/make_spec_key.pl
@@ -0,0 +1,80 @@
+#!/usr/local/bin/perl
+
+use lib qw( /Library/WebServer/cvs/go-dev/amigo/perl /Library/WebServer/cvs/go-dev/go-perl /Library/WebServer/cvs/go-dev/go-db-perl );
+
+use FindBin qw($RealBin);
+use lib (($ENV{GO_ROOT})
+         ? "$ENV{GO_ROOT}/go-db-perl" : "$RealBin/../../go-db-perl");
+use lib (($ENV{GO_ROOT})
+         ? "$ENV{GO_ROOT}/amigo/perl" : "$RealBin/../perl");
+
+use strict;
+use GO::SqlWrapper qw(:all);
+use GO::CGI::Session;
+use FreezeThaw qw(freeze thaw);
+
+my $cgi_path = shift @ARGV;
+unless ($cgi_path) {
+    &help();
+    exit;
+}
+
+eval {
+    require "$cgi_path/config.pl";
+};
+my $session = new GO::CGI::Session(-no_update=>1);
+
+
+my $dbh = $session->apph->dbh;
+
+my $hl = select_hashlist
+  ($dbh,
+   ["gene_product g", "association a", "species"],
+   ["g.species_id = species.id", "g.id=a.gene_product_id"],
+   ["count(*) as c", "species.genus", "species.species", "species.ncbi_taxa_id"],
+   "c desc",
+   ["ncbi_taxa_id", "genus", "species"],
+  );
+
+my %hash;
+
+my $top = shift @ARGV;
+my $i = 1;
+foreach (@{$hl || []}) {
+    next unless (length($_->{genus}) || length($_->{species}));
+#    my $g = length($_->{species})?substr($_->{genus}, 0, 1).".":$_->{genus};
+#    my $k = "$g $_->{species}";
+#    $k =~ s/\s+$//;
+    #print STDERR "$k: $_->{c}\n";
+#    $hash{$k} = $_->{ncbi_taxa_id};
+     if ($_->{species} =~ /\w/)
+     {	$hash{$_->{ncbi_taxa_id}} = [ $_->{genus}, $_->{species} ];
+     }
+     else
+     {	$hash{$_->{ncbi_taxa_id}} = [ $_->{genus} ];
+     }
+     
+    last if ($top && $i++ >= $top);
+}
+printf  STDERR "%s\nnum species: %d\n", join("\n",map{"$_ => ".join(" ", @{$hash{$_}})}keys %hash), scalar(keys %hash) if ($ENV{DEBUG});
+
+my $h = \%hash;
+
+my $str = freeze $h;
+my $f = "$cgi_path/spec_keys.pl";
+unlink $f if (-f $f);
+open(W, ">$f") or die "can not open $f: $!";
+print W $str;
+close(W);
+
+$dbh->disconnect;
+printf STDERR "$0 Done (%d)\n",scalar(keys %hash);
+exit;
+
+sub help {
+    print <<EOM;
+  make_spec_key.pl amigo_cgi_dir [nn]
+    where amigo_cgi_dir is full_dir_path where go.cgi resides,
+    and nn is top nn species with most of associations
+EOM
+}
