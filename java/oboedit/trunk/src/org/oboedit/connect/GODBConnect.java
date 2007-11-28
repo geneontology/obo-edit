19b54b08b419004b2c430b96a7989ad8e221be48
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2007-11-28 00:14:43 +0000

    Adding stuff to make database connection more dynamic.

--- java/oboedit/trunk/src/org/oboedit/connect/GODBConnect.java
+++ java/oboedit/trunk/src/org/oboedit/connect/GODBConnect.java
@@ -1,20 +1,36 @@
 package org.oboedit.connect;
 
 import java.sql.*;
+import java.util.regex.*;
+
 import org.obo.datamodel.LinkedObject;
 import org.oboedit.graph.OENode;
 
+
 public class GODBConnect {
 
-	// Connector and DB.
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String DB = "jdbc:mysql://spitz/go_latest_lite";
+	// Driver.
+	private String DRIVER = "com.mysql.jdbc.Driver";
+	
+	// Connect.
+	private String JDBC = "jdbc:mysql://";
+	private String HOST = "spitz";
+	private String DB = "go_latest_lite";
+	private String USER = "";
+	private String PASSWORD = "";
+	
+	// SQL.
+	private String sqlFront = "SELECT count(*) AS count FROM term INNER JOIN graph_path ON (term.id = graph_path.term1_id) INNER JOIN association ON ( graph_path.term2_id = association.term_id) WHERE term.acc = '";
+	private String sqlBack = "'";
+	private String variable = "$$$";
 	
 	private Connection connector;
 
+	
+	// 
 	public GODBConnect() {
 
-		// Class.forName(xxx) loads the jdbc classes and creates a drivermanager class factory
+		// Class.forName(xxx) loads the jdbc classes and creates a drivermanager class factory.
 		try {
 			Class.forName(DRIVER);
 		} catch (ClassNotFoundException e) {
@@ -22,42 +38,57 @@ public class GODBConnect {
 		}
 	}
 	
+	
 	//
 	public void connect() {
 
+		// Assemble connection string and SQL string.
+		String connection = JDBC + HOST + "/" + DB;
+
 		// Try to connect.
 		try {
-			connector = DriverManager.getConnection(DB);
+			connector = DriverManager.getConnection(connection);
 		} catch (SQLException e) {
-			System.err.println("ERROR___: " + e.getMessage());
+			System.err.println("___ERROR (" + connection + "): " + e.getMessage());
 		}
 	}
 	
+	
 	//
-	public void disconnect() {
+	public void resetConnection() {
+
+		// Try to connect.
 		try {
 			connector.close();
 		} catch (SQLException e) {
-			System.err.println("ERROR___: " + e.getMessage());
+			System.err.println("___ERROR: " + e.getMessage());
+		}
+
+		// Assemble connection string and SQL string.
+		String connection = JDBC + HOST + "/" + DB;
+
+		// Try to connect again.
+		try {
+			connector = DriverManager.getConnection(connection);
+		} catch (SQLException e) {
+			System.err.println("___ERROR (" + connection + "): " + e.getMessage());
 		}
 	}
 
+	
 	//
-	public int getAssociationCount( String str ) {
+	public int getCount( String sqlKey ) {
 
 		int number = -1;
 		ResultSet rs;
 
-		//System.err.println("___SETH___ into getData(" + str + ")");
+		//this.connect();
 	
 		try {
-			String front = "SELECT count(*) AS count FROM term INNER JOIN graph_path ON (term.id = graph_path.term1_id) INNER JOIN association ON ( graph_path.term2_id = association.term_id) WHERE term.acc = '";
-			String back = "'";
-			String sql = front + str + back;
+			String sql = sqlFront + sqlKey + sqlBack;
 			Statement stmt = connector.createStatement();
 			rs = stmt.executeQuery(sql);
 			
-			//System.err.println("___SETH___ Checking counts...");
 			System.err.println("SQL: " + sql);
 			while( rs.next() ){
 				number = rs.getInt("count");
@@ -66,8 +97,47 @@ public class GODBConnect {
 		} catch (SQLException e) {
 			System.err.println("ERROR___: " + e.getMessage());
 		}
-
+		
+		//this.disconnect();
+		
 		return number;
 	}
 	
+	
+	//
+	public String getDB() { return DB; }
+	public void setDB(String newDB) { DB = newDB; }
+	
+	
+	//
+	public String getHost() { return HOST; }
+	public void setHost(String newHost) { HOST = newHost; }
+	
+	
+	//
+	public String getSQL() { return sqlFront + variable + sqlBack; }
+	public void setSQL(String newSQL) {
+		
+		int stringLength = newSQL.length();
+		int variableLength = variable.length();
+		int index = newSQL.indexOf(variable);
+		String foo = newSQL.substring(0, index);
+		String bar = newSQL.substring(index + variableLength, stringLength);
+
+		//System.err.println("___FRONT: " + foo);
+		//System.err.println("___BACK: " + bar);
+		
+		sqlFront = foo;
+		sqlBack = bar;
+	}
+	
+	
+	//
+	public String getUser() { return USER; }
+	public void setUser(String newUser) { USER = newUser; }
+	
+	
+	//
+	public String getPassword() { return PASSWORD; }
+	public void setPassword(String newPassword) { PASSWORD = newPassword; }
 }
