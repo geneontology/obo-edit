62328b96d614de23371195c90e1cdf4d1f86c151
Nomi Harris <nlharris@users.noreply.github.com>
2007-11-29 23:14:13 +0000

    Added internal class to fix the issue on Macs where the MacOS adds a top-level menu for each application (such as OBO-Edit) that includes menu items such as "About" and "Quit" (shortcut command-Q).  If the Mac user selected Quit from that top-level menu (or typed command-Q), OBO-Edit would instantly quit without going through the standard OBO-Edit exit method (with a confirm dialog). This class uses an Open Source library, org.simplericity.macify.eawt, that lets us control that top-level menu on Macs without requiring any Mac-specific code that would cause runtime or compile-time problems on non-Macs.

--- java/oboedit/trunk/src/org/oboedit/launcher/OBOEdit.java
+++ java/oboedit/trunk/src/org/oboedit/launcher/OBOEdit.java
@@ -9,8 +9,11 @@ import org.bbop.swing.*;
 import org.bbop.util.*;
 import java.util.*;
 import java.io.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JFrame;
+import javax.swing.AbstractAction;
 import javax.swing.SwingUtilities;
 
 import org.obo.dataadapter.OBOAdapter;
@@ -19,6 +22,10 @@ import org.obo.datamodel.*;
 import org.oboedit.controller.SessionManager;
 import org.oboedit.gui.*;
 import org.oboedit.gui.tasks.DefaultGUIStartupTask;
+import org.oboedit.gui.menu.OEHelpMenu;
+import org.oboedit.gui.menu.FileMenu;
+
+import org.simplericity.macify.eawt.*;  // Lets us control top-level menu on Macs (w/o affecting other platforms)
 
 public class OBOEdit {
 
@@ -169,5 +176,74 @@ public class OBOEdit {
 			}
 		};
 		SwingUtilities.invokeAndWait(r);
+		(new OBOEditWithMenu()).initTopLevelMenus();
 	}
+
+
+    /** 
+     *  This internal class fixes the issue on Macs where the MacOS adds a top-level menu for
+     *  each application (such as OBO-Edit) that includes menu items such as "About" and "Quit"
+     *  (shortcut command-Q).  If the Mac user selected Quit from that top-level menu (or typed
+     *  command-Q), OBO-Edit would instantly quit without going through the standard OBO-Edit
+     *  exit method (asking "Really quit?").
+     *  This class uses an Open Source library, org.simplericity.macify.eawt, that lets us
+     *  control that top-level menu on Macs without requiring any Mac-specific code that would
+     *  cause runtime or compile-time problems on non-Macs.
+     */
+    public static class OBOEditWithMenu implements ApplicationListener {
+	private AboutAction aboutAction;
+	private ExitAction exitAction;
+	private Application application = new DefaultApplication();
+	
+	public void initTopLevelMenus() {
+	    application.addApplicationListener(this);
+	    aboutAction = new AboutAction("About");
+	    exitAction = new ExitAction("Exit");
+	}
+
+	public void handleAbout(ApplicationEvent event) {
+	    aboutAction.actionPerformed(null);
+	    event.setHandled(true);
+	}
+
+	public void handleQuit(ApplicationEvent event) {
+	    exitAction.actionPerformed(null);
+	}
+
+	/* These have to be declared even if we're not going to do anything with them */
+	public void handleOpenApplication(ApplicationEvent event) {
+	}
+
+	public void handleOpenFile(ApplicationEvent event) {
+	}
+
+	public void handlePreferences(ApplicationEvent event) {
+	}
+
+	public void handlePrintFile(ApplicationEvent event) {
+	}
+
+	public void handleReopenApplication(ApplicationEvent event) {
+	}
+
+	class AboutAction extends AbstractAction {
+	    public AboutAction(String title) {
+		super(title);
+	    }
+
+	    public void actionPerformed(ActionEvent actionEvent) {
+		(new OEHelpMenu()).showAboutFrame();
+	    }
+	}
+
+	class ExitAction extends AbstractAction {
+	    public ExitAction(String title) {
+		super(title);
+	    }
+
+	    public void actionPerformed(ActionEvent actionEvent) {
+		(new FileMenu()).askQuit();
+	    }
+	}
+    }
 }
