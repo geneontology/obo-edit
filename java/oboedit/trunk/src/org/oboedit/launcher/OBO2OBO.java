8088210a6c11f915a97b6df8215646d1ea7f8822
nomi <nomi@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-01-03 22:51:32 +0000

    Improved formatting of usage message.  However, this class should switch to using the argument signature methods as in OBOMerge.

--- java/oboedit/trunk/src/org/oboedit/launcher/OBO2OBO.java
+++ java/oboedit/trunk/src/org/oboedit/launcher/OBO2OBO.java
@@ -636,35 +636,35 @@ public class OBO2OBO {
 	protected static void printUsage(int exitCode) {
 		System.err
 				.println("obo2obo [-?] [-formatversion <versionid>] <filename 1> ... <filename N> \\\n"
-						+ "    [-parsecomments] [-writecomments] \\\n"
-						+ "     [-script <scriptname> [arg1 arg2 ... argN] \\;] \\\n"
-						+ "   [-o [-f <filterfile1.xml>] <outputfile1>] ... \\\n"
-						+ "   [-o [-f <filterfileN.xml>] <outputfileN>]");
+					 + "             [-parsecomments] [-writecomments] \\\n"
+					 + "             [-script <scriptname> [arg1 arg2 ... argN] \\;] \\\n"
+					 + "             [-o [-f <filterfile1.xml>] <outputfile1>] ... \\\n"
+					 + "             [-o [-f <filterfileN.xml>] <outputfileN>]\n");
 		System.err
 				.println("  -?                         - Writes this page to stderr and exits.");
 		System.err
-				.println("  -parsecomments             - Parses comments in obsolete terms looking for "
-						+ "                               GO-style formatted comments containing parseable "
+				.println("  -parsecomments             - Parses comments in obsolete terms looking for\n"
+						+ "                               GO-style formatted comments containing parseable\n"
 						+ "                               replacement and consider terms.");
 		System.err
-				.println("  -writecomments             - Writes replaced_by and consider tags into parseable "
+				.println("  -writecomments             - Writes replaced_by and consider tags into parseable\n"
 						+ "                               GO-style formatted comments.");
 		System.err
 				.println("  -formatversion <versionid> - The version of OBO to write. Allowed values are\n"
 						+ "                               OBO_1_0 and OBO_1_2. The default is OBO_1_2.\n"
 						+ "                               Optional.");
 		System.err
-				.println("  -script <scriptname> <args> \\; - Runs an OSL script on the ontology. A script tag's "
-						+ "                                             arguments MUST be followed by a \\; sequence.");
+				.println("  -script <scriptname> <args> - Runs an OSL script on the ontology. A script tag's\n"
+					 + "                               arguments MUST be followed by a sequence.");
 
 		System.err
 				.println("  <filenameN>                - An obo file to load. Any number of OBO files may\n"
-						+ "                               be loaded");
+					 + "                               be loaded.");
 		System.err
-				.println("  -o [-f <objectfilterfile.xml>] [-lf <linkfilterfile.xml>] [-allowdangling] [-p <prefilter property id>] [-strictrootdetection] [-saveimpliedlinks|-saveallimpliedlinks] [-realizeimpliedlinks] <outputfile.obo> - \n"
+				.println("  -o [-f <objectfilterfile.xml>] [-lf <linkfilterfile.xml>] [-allowdangling] [-p <prefilter property id>] [-strictrootdetection] [-saveimpliedlinks|-saveallimpliedlinks] [-realizeimpliedlinks] <outputfile.obo>\n"
 						+ "        An output file to write. The optional -f and -lf flags may be used to specify a\n"
-						+ "        filter file or a link filter file to apply to the output file before writing. If the \n"
-						+ "        -allowdangling flag is specified, dangling links will not be written.\n"
+						+ "        filter file or a link filter file to apply to the output file before writing.\n"
+						+ "        If the -allowdangling flag is specified, dangling links will not be written.\n"
 						+ "        The optional -p flag specifies the id of a property to use for \n"
 						+ "        reasoner pre-filtering. The optional -strict-root-detection flag\n"
 						+ "        applies filters using strict root detection.");
