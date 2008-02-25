b9bbd480c1da7ce461d83d2058d5a3a3da8778aa
nomi <nomi@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-02-25 21:54:18 +0000

    Using initial capitals to be consistent with other window titles.

--- java/obo/trunk/src/org/obo/dataadapter/OBOAdapter.java
+++ java/obo/trunk/src/org/obo/dataadapter/OBOAdapter.java
@@ -8,16 +8,16 @@ import org.obo.history.HistoryList;
 
 public interface OBOAdapter extends DataAdapter {
 	public static final IOOperation<HistoryList, Void> WRITE_HISTORY = new DefaultIOOperation<HistoryList, Void>(
-			"WRITE_HISTORY", "write history", HistoryList.class, Void.class);
+			"WRITE_HISTORY", "Write history", HistoryList.class, Void.class);
 
 	@SuppressWarnings("unchecked")
 	public static final IOOperation<Void, List<HistoryList>> READ_HISTORY = (IOOperation<Void, List<HistoryList>>) (new DefaultIOOperation(
-			"READ_HISTORY", "read history", Void.class, List.class));
+			"READ_HISTORY", "Read history", Void.class, List.class));
 
 	public static final IOOperation<OBOSession, OBOSession> WRITE_ONTOLOGY = new DefaultIOOperation<OBOSession, OBOSession>(
-			"WRITE_ONTOLOGY", "write ontology", OBOSession.class,
+			"WRITE_ONTOLOGY", "Write ontology", OBOSession.class,
 			OBOSession.class);
 
 	public static final IOOperation<Void, OBOSession> READ_ONTOLOGY = new DefaultIOOperation<Void, OBOSession>(
-			"READ_ONTOLOGY", "read ontology", Void.class, OBOSession.class);
+			"READ_ONTOLOGY", "Read ontology", Void.class, OBOSession.class);
 }
