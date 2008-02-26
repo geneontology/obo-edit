652c6b9058153a123e26a8d34fd2a7729142e263
balhoff <balhoff@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-02-26 18:27:49 +0000

    Fixed problem with View menu when using Mac OS X top of screen menubar.  The menu merging code in AbstractDynamicMenuItem does not work for some reason in this situation.  I changed ViewMenu to avoid any merging of menu items.  This problem only appears in Phenote, since OBO-Edit does not use the Mac OS X menubar.

--- java/bbop/trunk/src/org/bbop/framework/ViewMenu.java
+++ java/bbop/trunk/src/org/bbop/framework/ViewMenu.java
@@ -13,10 +13,10 @@ import java.util.Map;
 
 import javax.swing.AbstractAction;
 import javax.swing.ButtonGroup;
-import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JSeparator;
 
 import org.bbop.framework.dock.Perspective;
 import org.bbop.swing.AbstractDynamicMenuItem;
@@ -30,117 +30,108 @@ public class ViewMenu extends AbstractDynamicMenuItem {
 	}
 
 	public List<Component> getItems() {
-		List<Component> out = new ArrayList<Component>();
-		DynamicMenu perspectivesMenu = new DynamicMenu("Layouts");
-		perspectivesMenu.add(new AbstractDynamicMenuItem("Layouts", true, true,
-				true) {
-			public List<Component> getItems() {
-				ButtonGroup group = new ButtonGroup();
-				List<Component> out = new LinkedList<Component>();
-				for (final Perspective perspective : ComponentManager
-						.getManager().getPerspectives()) {
-					JRadioButtonMenuItem item = new JRadioButtonMenuItem(
-							new AbstractAction(perspective.getName()) {
-								public void actionPerformed(ActionEvent e) {
-									ComponentManager.getManager()
-											.setPerspective(perspective);
-								}
-							});
-					group.add(item);
-					if (ObjectUtil.equals(ComponentManager.getManager()
-							.getCurrentPerspective(), perspective))
-						item.setSelected(true);
-					out.add(item);
-				}
-				return out;
-			}
-		});
-		AbstractDynamicMenuItem ioOps = new AbstractDynamicMenuItem("Show",
-				true, true, true) {
-			public List<Component> getItems() {
-				final Perspective current = ComponentManager.getManager()
-						.getDriver().getCurrentPerspective();
-				List<Component> out = new LinkedList<Component>();
-				JMenuItem savePerspectiveAsItem = new JMenuItem(
-						new AbstractAction("Save current perspective as...") {
-							public void actionPerformed(ActionEvent e) {
-								String name = JOptionPane
-										.showInputDialog("New perspective name");
-								if (name != null) {
-									ComponentManager.getManager()
-											.savePerspectiveAs(current, name);
-								}
-							}
-						});
-				JMenuItem deleteItem = new JMenuItem(new AbstractAction(
-						"Delete current perspective") {
-					public void actionPerformed(ActionEvent e) {
-						ComponentManager.getManager()
-								.deletePerspective(current);
-					}
-				});
-				boolean builtin = current == null ? false : current
-						.getBuiltIn();
-				deleteItem.setEnabled(!builtin);
-				out.add(savePerspectiveAsItem);
-				out.add(deleteItem);
-				return out;
-			}
-		};
+	    List<Component> out = new ArrayList<Component>();
+	    DynamicMenu perspectivesMenu = new DynamicMenu("Layouts") {
+	        public List<Component> getItems() {
+	            ButtonGroup group = new ButtonGroup();
+	            List<Component> out = new LinkedList<Component>();
+	            for (final Perspective perspective : ComponentManager
+	                    .getManager().getPerspectives()) {
+	                JRadioButtonMenuItem item = new JRadioButtonMenuItem(
+	                        new AbstractAction(perspective.getName()) {
+	                            public void actionPerformed(ActionEvent e) {
+	                                ComponentManager.getManager().setPerspective(perspective);
+	                            }
+	                        });
+	                group.add(item);
+	                if (ObjectUtil.equals(ComponentManager.getManager()
+	                        .getCurrentPerspective(), perspective))
+	                    item.setSelected(true);
+	                out.add(item);
+	            }
 
-		perspectivesMenu.add(ioOps);
-		DynamicMenu showMenu = new DynamicMenu("Show");
-		showMenu.add(new AbstractDynamicMenuItem("Show") {
-			public List<Component> getItems() {
-				List<Component> out = new LinkedList<Component>();
-				Map<GUIComponentFactory.FactoryCategory, List<GUIComponentFactory>> factoryMap = new HashMap<GUIComponentFactory.FactoryCategory, List<GUIComponentFactory>>();
-				Collection<GUIComponentFactory<?>> f = ComponentManager
-						.getManager().getFactories();
-				for (final GUIComponentFactory factory : f) {
-					if (!factory.showInMenus())
-						continue;
-					List<GUIComponentFactory> factories = factoryMap
-							.get(factory.getCategory());
-					if (factories == null) {
-						factories = new ArrayList<GUIComponentFactory>();
-						factoryMap.put(factory.getCategory(), factories);
-					}
-					factories.add(factory);
-				}
-				Comparator<GUIComponentFactory> factoryComparator = new Comparator<GUIComponentFactory>() {
+	            out.add(new JSeparator());
 
-					public int compare(GUIComponentFactory o1,
-							GUIComponentFactory o2) {
-						return o1.toString().compareTo(o2.toString());
-					}
+	            final Perspective current = ComponentManager.getManager()
+	            .getDriver().getCurrentPerspective();
+	            JMenuItem savePerspectiveAsItem = new JMenuItem(
+	                    new AbstractAction("Save current perspective as...") {
+	                        public void actionPerformed(ActionEvent e) {
+	                            String name = JOptionPane
+	                            .showInputDialog("New perspective name");
+	                            if (name != null) {
+	                                ComponentManager.getManager()
+	                                .savePerspectiveAs(current, name);
+	                            }
+	                        }
+	                    });
+	            JMenuItem deleteItem = new JMenuItem(new AbstractAction(
+	            "Delete current perspective") {
+	                public void actionPerformed(ActionEvent e) {
+	                    ComponentManager.getManager()
+	                    .deletePerspective(current);
+	                }
+	            });
+	            boolean builtin = current == null ? false : current
+	                    .getBuiltIn();
+	            deleteItem.setEnabled(!builtin);
+	            out.add(savePerspectiveAsItem);
+	            out.add(deleteItem);
+	            return out;
+	        }
+	    };
 
-				};
-				for (GUIComponentFactory.FactoryCategory category : GUIComponentFactory.FactoryCategory
-						.values()) {
-					List<GUIComponentFactory> factories = factoryMap
-							.get(category);
-					if (factories != null && factories.size() > 0) {
-						Collections.sort(factories, factoryComparator);
-						DynamicMenu subMenu = new DynamicMenu(category
-								.toString());
-						for (final GUIComponentFactory factory : factories) {
-							JMenuItem item = new JMenuItem(new AbstractAction(
-									factory.getName()) {
-								public void actionPerformed(ActionEvent e) {
-									ComponentManager.getManager()
-											.showComponent(factory, null);
-								}
-							});
-							subMenu.add(item);
-						}
-						out.add(subMenu);
-					}
-				}
-				return out;
-			}
-		});
-		out.add(perspectivesMenu);
-		out.add(showMenu);
-		return out;
+	    DynamicMenu showMenu = new DynamicMenu("Show") {
+	        public List<Component> getItems() {
+	            List<Component> out = new LinkedList<Component>();
+	            Map<GUIComponentFactory.FactoryCategory, List<GUIComponentFactory>> factoryMap = new HashMap<GUIComponentFactory.FactoryCategory, List<GUIComponentFactory>>();
+	            Collection<GUIComponentFactory<?>> f = ComponentManager
+	            .getManager().getFactories();
+	            for (final GUIComponentFactory factory : f) {
+	                if (!factory.showInMenus())
+	                    continue;
+	                List<GUIComponentFactory> factories = factoryMap
+	                .get(factory.getCategory());
+	                if (factories == null) {
+	                    factories = new ArrayList<GUIComponentFactory>();
+	                    factoryMap.put(factory.getCategory(), factories);
+	                }
+	                factories.add(factory);
+	            }
+	            Comparator<GUIComponentFactory> factoryComparator = new Comparator<GUIComponentFactory>() {
+
+	                public int compare(GUIComponentFactory o1,
+	                        GUIComponentFactory o2) {
+	                    return o1.toString().compareTo(o2.toString());
+	                }
+
+	            };
+	            for (GUIComponentFactory.FactoryCategory category : GUIComponentFactory.FactoryCategory
+	                    .values()) {
+	                List<GUIComponentFactory> factories = factoryMap.get(category);
+	                if (factories != null && factories.size() > 0) {
+	                    Collections.sort(factories, factoryComparator);
+	                    DynamicMenu subMenu = new DynamicMenu(category
+	                            .toString());
+	                    for (final GUIComponentFactory factory : factories) {
+	                        JMenuItem item = new JMenuItem(new AbstractAction(
+	                                factory.getName()) {
+	                            public void actionPerformed(ActionEvent e) {
+	                                ComponentManager.getManager()
+	                                .showComponent(factory, null);
+	                            }
+	                        });
+	                        subMenu.add(item);
+	                    }
+	                    out.add(subMenu);
+	                }
+	            }
+	            return out;
+	        }
+	    };
+
+	    out.add(perspectivesMenu);
+	    out.add(showMenu);
+	    return out;
 	}
 }
