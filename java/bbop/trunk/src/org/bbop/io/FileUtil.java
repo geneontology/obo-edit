737a2b2e8987735559a9e481628f71fbe46df2c7
mgibson <mgibson@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-03-03 20:18:13 +0000

    file util ensure exists now checks for files/resources (like perspectives.xml from idwdriver) from a few places - uses class loader dont know how that differs from the class loader that gets passed in also tries with / before file - this is crucial for webstart apparently

--- java/bbop/trunk/src/org/bbop/io/FileUtil.java
+++ java/bbop/trunk/src/org/bbop/io/FileUtil.java
@@ -2,6 +2,7 @@ package org.bbop.io;
 
 import java.io.*;
 import java.lang.reflect.*;
+import java.net.URL;
 
 public class FileUtil {
 
@@ -21,37 +22,66 @@ public class FileUtil {
      * by copying the specified resource from the specified class loader.
      * Based on code written by Matthew Pocock
      */
-    public static void ensureExists(File file, ClassLoader loader,
-				    String resource)
-	throws IOException {
+    public static void ensureExists(File file, ClassLoader loader, String resource)
+      throws IOException {
 
-	if(!file.exists()) {
-	    if (loader == null) {
-		throw new IOException("ClassLoader is null,"+
-				      " can't load resource.");
-	    }
+      // if file exists nothgin to - return
+      if (file.exists())
+        return;
 
-	    if (resource == null) {
-	    	throw new NullPointerException("Can't load from null resource");
-	    }
-	    InputStream is = loader.getResourceAsStream(resource);
-	    if(is == null) {
-		throw new FileNotFoundException("Couldn't find resource "+
-						resource);
-	    }
-	    (new File(file.getParent())).mkdirs();
-	    OutputStream os = new BufferedOutputStream(
-						  new FileOutputStream(file));
-	    
-	    System.err.println("ensureExists: creating " + file.getAbsolutePath() + " from resource " + resource); // DEL
-	    for(int next = is.read(); next != -1; next = is.read()) {
-		os.write(next);
-	    }
-	    
-	    os.flush();
-	    os.close();
-	}
+      //if(!file.exists()) {
+
+      // Get InputStream from resource
+      if (loader == null) {
+        throw new IOException("ClassLoader is null,"+
+                              " can't load resource.");
+      }
+        
+      if (resource == null) {
+        throw new NullPointerException("Can't load from null resource");
+      }
+      
+      InputStream is = loader.getResourceAsStream(resource);
+      if(is == null) {
+        System.err.println("ensureExists couldnt find resource "+resource+" gonna try "
+                           +"class.getResource");
+        try { is = findInputStream(resource); }
+        catch (FileNotFoundException e) {
+          System.err.println("ensureExists couldnt find resource "+resource+" gonna try "
+                             +"/"+resource);
+          is = findInputStream("/"+resource); //let ex fly
+        }
+
+        if (is == null)
+          throw new FileNotFoundException("Couldn't find resource "+resource);
+      }
+
+      // Got InputStream from resource copy to file
+      (new File(file.getParent())).mkdirs();
+      OutputStream os = new BufferedOutputStream(
+        new FileOutputStream(file));
+      
+      System.err.println("ensureExists: creating " + file.getAbsolutePath() + " from resource "
+                         + resource); // DEL
+      for(int next = is.read(); next != -1; next = is.read()) {
+        os.write(next);
+      }
+      
+      os.flush();
+      os.close();
+      //}
     }
+
+  private static InputStream findInputStream(String resource)
+    throws FileNotFoundException {
+    URL u = FileUtil.class.getResource(resource);
+    if (u == null) throw new FileNotFoundException("cant get resource "+resource);
+    InputStream is=null;
+    try { is = u.openStream(); }
+    catch (IOException e) {}
+    if (is == null) {throw new FileNotFoundException("failed to open stream "+resource);}
+    return is;
+  }
 
     public static ClassLoader getSystemClassLoader() {
 	try {
