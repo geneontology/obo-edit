1d2d809127e342d898480e8298007e49cd36a1e4
siddbasu <siddbasu@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-03-26 21:48:14 +0000

    Updated the async interface with those methods that are applicable now. There is a corresponding sleep class from where we are going to add the methods one by one.

--- go-dev/RefGenome/branches/RefGenomeSiddNew/src/org/bbop/client/RefGenomeServiceAsync.java
+++ go-dev/RefGenome/branches/RefGenomeSiddNew/src/org/bbop/client/RefGenomeServiceAsync.java
@@ -25,28 +25,8 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
  */
 public interface RefGenomeServiceAsync  {
 	
-	public void fetchIdsByName(String searchTerm, AsyncCallback callback);
-
-	public void fetchIdsByName(String searchTerm, String taxonId, AsyncCallback callback);
-
-	public void fetchLabelsById(String searchTerm, AsyncCallback callback);
-
-	public void fetchLabelMapsById(String searchTerm, AsyncCallback callback);
-
-	public void fetchReferenceTaxonIds( AsyncCallback callback);
 	
-	public void getTaxonIdPrefix( AsyncCallback callback);
 	
-	public void fetchReferenceTargetIds( AsyncCallback callback);
-	
-	public void fetchEntityIdsInHomologSet(String homologSetId, AsyncCallback callback);
-
-	public void assignGeneTargetStatus(String geneId, AsyncCallback callback) ;
-
-	public void retractGeneTargetStatus(String geneId, AsyncCallback callback) ;
-	
-	public void addUser(String userId, String fullName, String password, AsyncCallback callback); 
-	
-	public boolean checkUserPassword(String userId, String password, AsyncCallback callback); 
+	public void checkUserPassword(String userId, String password, AsyncCallback callback); 
 
 }
