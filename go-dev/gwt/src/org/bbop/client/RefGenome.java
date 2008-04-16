7eb4d32f10d1fa6289b252ee2c157d83befc0827
sjcarbon <kltm@users.noreply.github.com>
2008-04-16 23:29:56 +0000

    done retooling; TODO: get remaining mygwt.jar bug fixed (most visible during RefGenome-compile)

--- go-dev/gwt/src/org/bbop/client/RefGenome.java
+++ go-dev/gwt/src/org/bbop/client/RefGenome.java
@@ -1,12 +1,5 @@
 package org.bbop.client;
 
-
-
-
-
-
-
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 
@@ -20,9 +13,7 @@ public class RefGenome implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		RefGenomeServiceClientImpl refgservice = new RefGenomeServiceClientImpl(GWT.getModuleBaseURL() + "/RefGenome"); 
-       refgservice.getView().getViewPort().hideLoadingPanel("loading");
-       
-	   
+		RefGenomeServiceClientImpl refgservice = new RefGenomeServiceClientImpl(GWT.getModuleBaseURL() + "/RefGenomeService"); 
+		refgservice.getView().getViewPort().hideLoadingPanel("loading");
 	}	
 }
