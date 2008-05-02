7b91b5fc37ca76e740e1778cbcd06d44f7f7540a
rbruggne <rbruggne@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-05-02 22:32:56 +0000

    Cleanup

--- OBDUI/src/org/geneontology/web/NodeSearchResource.java
+++ OBDUI/src/org/geneontology/web/NodeSearchResource.java
@@ -9,13 +9,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-
 import org.obd.model.Graph;
 import org.obd.model.Node;
 import org.obd.model.bridge.OBDJSONBridge;
