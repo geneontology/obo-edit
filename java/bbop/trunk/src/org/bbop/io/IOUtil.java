bb85ec792ea9c50cb4575ae2015de8f3fa68af75
nomi <nomi@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-05-05 23:01:32 +0000

    Added copyFiles method to recursively copy files and/or directories (from http://www.dreamincode.net/code/snippet1443.htm).

--- java/bbop/trunk/src/org/bbop/io/IOUtil.java
+++ java/bbop/trunk/src/org/bbop/io/IOUtil.java
@@ -200,4 +200,53 @@ public final class IOUtil {
 			return false;
 		}
 	}
+
+/**
+ * This function will copy files or directories from one location to another.
+ * note that the source and the destination must be mutually exclusive. This 
+ * function can not be used to copy a directory to a sub directory of itself.
+ * The function will also have problems if the destination files already exist.
+ * From http://www.dreamincode.net/code/snippet1443.htm
+ * @param src -- A File object that represents the source for the copy
+ * @param dest -- A File object that represnts the destination for the copy.
+ * @throws IOException if unable to copy.
+ */
+	public static void copyFiles(File src, File dest) throws IOException {
+		//Check to ensure that the source is valid...
+		if (!src.exists()) {
+			throw new IOException("copyFiles: Can not find source: " + src.getAbsolutePath()+".");
+		} else if (!src.canRead()) { //check to ensure we have rights to the source...
+			throw new IOException("copyFiles: No right to source: " + src.getAbsolutePath()+".");
+		}
+		//is this a directory copy?
+		if (src.isDirectory()) {
+			if (!dest.exists()) { //does the destination already exist?
+				//if not we need to make it exist if possible (note this is mkdirs not mkdir)
+				if (!dest.mkdirs()) {
+					throw new IOException("copyFiles: Could not create direcotry: " + dest.getAbsolutePath() + ".");
+				}
+			}
+			//get a listing of files...
+			String list[] = src.list();
+			//copy all the files in the list.
+			for (int i = 0; i < list.length; i++)
+			{
+				File dest1 = new File(dest, list[i]);
+				File src1 = new File(src, list[i]);
+				copyFiles(src1 , dest1);
+			}
+		} else { 
+			//This was not a directory, so lets just copy the file
+			try {
+				System.out.println("copyFile " + src + " -> " + dest);  // DEL
+				copyFile(src, dest);
+			} catch (IOException e) { //Error copying file... 
+				IOException wrapper = new IOException("copyFiles: Unable to copy file: " + 
+								      src.getAbsolutePath() + "to" + dest.getAbsolutePath()+".");
+				wrapper.initCause(e);
+				wrapper.setStackTrace(e.getStackTrace());
+				throw wrapper;
+			}
+		}
+	}
 }
