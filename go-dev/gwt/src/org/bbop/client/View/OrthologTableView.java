e493f4deada23ad76da94fe84415c799f34e7a89
siddbasu <siddbasu@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-05-06 21:00:10 +0000

    Paging table first try

--- go-dev/gwt/src/org/bbop/client/View/OrthologTableView.java
+++ go-dev/gwt/src/org/bbop/client/View/OrthologTableView.java
@@ -1,5 +1,6 @@
 package org.bbop.client.View;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +11,12 @@ import net.mygwt.ui.client.data.DataCallback;
 import net.mygwt.ui.client.data.DataList;
 import net.mygwt.ui.client.data.LoadConfig;
 import net.mygwt.ui.client.data.LoadResult;
+import net.mygwt.ui.client.data.Model;
 import net.mygwt.ui.client.viewer.IAsyncContentCallback;
 import net.mygwt.ui.client.viewer.ModelCellLabelProvider;
 import net.mygwt.ui.client.viewer.RemoteContentProvider;
 import net.mygwt.ui.client.viewer.TableViewer;
+import net.mygwt.ui.client.viewer.ViewerCell;
 import net.mygwt.ui.client.widget.table.Table;
 import net.mygwt.ui.client.widget.table.TableColumn;
 import net.mygwt.ui.client.widget.table.TableColumnModel;
@@ -77,16 +80,21 @@ public class OrthologTableView extends GenericNodeListTableView {
 			
 			public void getData(LoadConfig config, DataCallback callback) {
 				// TODO Auto-generated method stub
-				DataList list = new DataList(getTestData());
-				LoadResult result = new LoadResult(list);	
-				callback.setResult(result);
+			    LoadResult result = getTestData();
+				System.err.println("From getData");
+				//System.err.println(list.toString());
 				
+				System.err.println(result.totalLength);
+				//System.err.println(result.data.toString());
+				//callback.setResult((LoadResult)result);
+				callback.setResult(result);
 			}
 
 			public void getElements(Object input, IAsyncContentCallback callback) {
 				// TODO Auto-generated method stub
 				if (input instanceof List) {
-					 List list = (List) input;
+					 List list = (List)input;
+					System.err.println(list.toArray());
 					callback.setElements(list.toArray());
 				}
 				else {
@@ -104,6 +112,7 @@ public class OrthologTableView extends GenericNodeListTableView {
 		viewer.setContentProvider(cp);
 		
 		ModelCellLabelProvider lp = new ModelCellLabelProvider();
+			
 		
 		for (int i = 0; i < cols.size(); i++) {
 			viewer.getViewerColumn(i).setLabelProvider(lp);
@@ -115,48 +124,58 @@ public class OrthologTableView extends GenericNodeListTableView {
 		return orthoTbl;
 	}
 	
-    private List getTestData() {
-    	List testData = new ArrayList();
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
-    	testData.add(new String[]{"ADHIA|PO7327","RGD:2044","WB:WP:CE23822","DictyBase:DDB0238276","TAIR:gene:1005715800","SGD:S000002327","GEneDB_Spombe:SPCC13B11.04C","MGI:1349472","FB:FBgn0011768","ECOGENE_G:adh1","ZFIN:ZDB-GENE-011003-1"});
+    private LoadResult getTestData() {
+    	DataList sublist = new DataList();
+    	for (int i = 0 ; i <30 ; i++) {
+    		OrthoData orthoData = getTestOrthoData();
+    		Model m = new Model();
+    		m.set("target", orthoData.target);
+    		m.set("r.norvegicus",orthoData.rat);
+    		m.set("c.elegans", orthoData.worm);
+    		m.set("d.discoideum", orthoData.dicty);
+    		m.set("a.thaliana", orthoData.tair);
+    		m.set("s.cerevisiae", orthoData.sgd);
+    		m.set("s.pombe",orthoData.pombe);
+    		m.set("m.musculus", orthoData.mouse);
+    		m.set("d.melanogaster", orthoData.fly);
+    		m.set("e.coli", orthoData.ecoli);
+    		m.set("d.rio",orthoData.zfin);
+    		sublist.add(m);
+    	}
+    	System.err.println("From getTestdata");
+    	System.err.println(sublist.size());
+    	LoadResult result = new LoadResult(sublist);
+    	System.err.println(result.totalLength);
+    	return result;
     	
-    	return testData;
     }
     
     public RemoteContentProvider getDataProvider() {
     	return cp;
     	
+    }
+    
+    private OrthoData getTestOrthoData() {
+    	OrthoData data = new OrthoData();
+    	data.target = "ADHIA|PO7327";
+    	data.rat = "RGD:2044";
+    	data.worm = "WB:WP:CE23822";
+    	data.dicty = "DictyBase:DDB0238276";
+    	data.tair = "TAIR:gene:1005715800";
+    	data.sgd = "SGD:S000002327";
+    	data.pombe = "GeneDB_Spombe:SPCC13B11.04C";
+    	data.mouse = "MGI:1349472";
+    	data.fly = "FB:FBgn0011768";
+    	data.ecoli = "ECOGENE_G:adh1";
+    	data.zfin = "ZFIN:ZDB-GENE-011003-1";
+    	
+    	return data;
+    	
+    }
+    
+    private class OrthoData implements Serializable {
+    	String target;
+    	String rat,worm,dicty,tair,sgd,pombe,mouse,fly,ecoli,zfin;
     }
 
 }
