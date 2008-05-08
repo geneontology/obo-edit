ee7e761b85c6c93d341dbf1f16872c0872e98b85
nomi <nomi@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-05-08 20:32:09 +0000

    Name is now initially set to null if it's not specified (this is the way Chris wants it). compareTo now checks whether the name is null before trying to do string comparisons on it.

--- java/obo/trunk/src/org/obo/datamodel/impl/OBOClassImpl.java
+++ java/obo/trunk/src/org/obo/datamodel/impl/OBOClassImpl.java
@@ -15,10 +15,13 @@ public class OBOClassImpl extends LinkedAnnotatedObjectImpl implements OBOClass 
 	    // Hey, this sets the name to the id!  I thought we didn't want that.  --NH, 4/2008
 	    // this(id, id);
 //	    this(null, id);
-	    // I'm putting this back the way it was, because making the name null broke cloning.
+	    // 5/1/2008: I'm putting this back the way it was, because making the name null broke cloning.
 	    // If you want to assign a different name to the object, you can
 	    // use the method below that takes a term name and an id.
-	    this(id, id);
+//	    this(id, id);
+	    // 5/7/2008: Chris says it's not ok to use the id as the name (even though this is
+	    // how it was before).
+	    this(null, id);
 	}
 
 	public OBOClassImpl(String term, String id) {
@@ -50,11 +53,20 @@ public class OBOClassImpl extends LinkedAnnotatedObjectImpl implements OBOClass 
 
 	public int compareTo(Object in) {
 		if (in instanceof OBOClass) {
+			String namea = getName();
+			String nameb = ((OBOClass) in).getName();
+			if (namea == null && nameb == null)
+			    return 0;
+			if (namea == null)
+			    return -1;
+			if (nameb == null)
+			    return 1;
+
 			int cmp = getName().toUpperCase().compareTo(
-					((OBOClass) in).getName().toUpperCase());
+				((OBOClass) in).getName().toUpperCase());
 			if (cmp == 0)
 				return getID().toUpperCase().compareTo(
-						((OBOClass) in).getID().toUpperCase());
+					((OBOClass) in).getID().toUpperCase());
 			else
 				return cmp;
 		} else {
