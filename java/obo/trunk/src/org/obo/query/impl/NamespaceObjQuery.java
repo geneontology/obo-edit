4af83fac13c94db43e024d0b1c180281209001be
mgibson <mgibson@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-05-14 20:09:32 +0000

    NamespaceObjQuery can now do properties/rels or terms but actually not both - currently no need for both - easy to add if need it

--- java/obo/trunk/src/org/obo/query/impl/NamespaceObjQuery.java
+++ java/obo/trunk/src/org/obo/query/impl/NamespaceObjQuery.java
@@ -17,7 +17,10 @@ import org.obo.util.TermUtil;
 /** this is identical to NamespaceQuery except that it works on OBOObjects instead
     of OBOClasses, so its more general as OBOClass is an OBOObject, and so is
     OBOProperty. NamespaceObjQuery allows for the querying of obo sessions
-    with namespaces. A Collection of OBOObjects are returned from the query */
+    with namespaces. A Collection of OBOObjects are returned from the query
+    The most important method here is the matches method as that is the really 
+    the heart of the query/filter.
+ */
 public class NamespaceObjQuery implements Query<OBOObject, OBOObject> {
 
 	protected Collection<String> namespaceIDs = new HashSet<String>();
@@ -25,6 +28,9 @@ public class NamespaceObjQuery implements Query<OBOObject, OBOObject> {
 	protected boolean allowObsoletes = true;
 
 	protected boolean allowNonObsoletes = true;
+
+  /** if justTerms true do just OBOClass, false just OBOProperties */
+  private boolean justTerms = true;
 
 	protected Comparator<OBOObject> comparator = new Comparator<OBOObject>() {
 		public int compare(OBOObject o1, OBOObject o2) {
@@ -36,7 +42,9 @@ public class NamespaceObjQuery implements Query<OBOObject, OBOObject> {
 
 	}
 
-	public NamespaceObjQuery(Namespace... namespaces) {
+  /** if just terms true do just OBOClass, false just OBOProperties */
+	public NamespaceObjQuery(boolean justTerms, Namespace... namespaces) {
+    this.justTerms = justTerms;
 		for (Namespace ns : namespaces) {
 			namespaceIDs.add(ns.getID());
 		}
@@ -94,17 +102,44 @@ public class NamespaceObjQuery implements Query<OBOObject, OBOObject> {
 		return OBOObject.class;
 	}
 
+  /** This is the method that is utlimately called by the Query machinery to say
+      whether an OBOObject passes or not, returns null on failure, returns the
+      OBOObject passed in on success */
 	public OBOObject matches(OBOObject a) {
-		boolean isObsolete = TermUtil.isObsolete(a);
-		if (!a.isBuiltIn()
-				&& TermUtil.isClass(a)
-				&& (namespaceIDs.size() == 0 || namespaceIDs.contains(a
-						.getNamespace().getID()))
-				&& ((!isObsolete && allowNonObsoletes) || (isObsolete && allowObsoletes))) {
-			return a;
-		} else
-			return null;
+    // a builtIn is one of the building block obo terms, like obo:term
+    if (a.isBuiltIn()) return null;
+    if (!namespaceMatch(a)) return null;
+    if (!obsoleteCheck(a)) return null;
+    if (!typeCheck(a)) return null;
+    return a;
+// 		//boolean isObsolete = TermUtil.isObsolete(a);
+// 		if (!a.isBuiltIn()
+// 				// && TermUtil.isClass(a) this can work for properties as well as classes!
+// 				&& (namespaceIDs.size() == 0 || namespaceIDs.contains(a
+// 						.getNamespace().getID()))
+// 				&& ((!isObsolete && allowNonObsoletes) || (isObsolete && allowObsoletes))) {
+// 			return a;
+// 		} else
+// 			return null;
 	}
+
+  /** return true if obj id in namespaceIDs, or if namespaceIDs are empty */
+  private boolean namespaceMatch(OBOObject obj) {
+    if (namespaceIDs.isEmpty()) return true; // no namespaces
+    return namespaceIDs.contains(obj.getNamespace().getID());
+  }
+  /** returns true if is not obsolete and allowNonObsolete, or vice versa */
+  private boolean obsoleteCheck(OBOObject obj) {
+		boolean isObsolete = TermUtil.isObsolete(obj);
+    if (!isObsolete && allowNonObsoletes) return true;
+    return isObsolete && allowObsoletes;
+  }
+  /** returns true if is term/OBOClass and justTerms is true 
+      OR if is OBOProperty and justTerms is false */
+  private boolean typeCheck(OBOObject obj) {
+    if (TermUtil.isClass(obj) && justTerms) return true;
+    return TermUtil.isProperty(obj) && !justTerms;
+  }
 
 	public boolean equals(Object o) {
 		if (o instanceof NamespaceObjQuery) {
