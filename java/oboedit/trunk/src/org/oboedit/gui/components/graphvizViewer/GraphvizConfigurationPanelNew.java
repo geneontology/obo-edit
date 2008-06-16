bd1cd9eac0e08593439d4fdea36a2f592a3e0559
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-06-16 14:05:27 +0000

    Added wrench icon and feature to remove disjoint relationships from display.

--- java/oboedit/trunk/src/org/oboedit/gui/components/graphvizViewer/GraphvizConfigurationPanelNew.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/graphvizViewer/GraphvizConfigurationPanelNew.java
@@ -1,36 +1,74 @@
 package org.oboedit.gui.components.graphvizViewer;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 
 import javax.swing.JColorChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import org.bbop.framework.ComponentConfiguration;
 import org.bbop.framework.ConfigurationPanel;
+import org.bbop.framework.GUIComponent;
 
-public class GraphvizConfigurationPanelNew  extends ConfigurationPanel {
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 
-		protected JPanel backgroundColorPanel = new JPanel();
-		protected JColorChooser backgroundColorChooser = new JColorChooser();
-		protected GraphvizCanvas graphvizCanvasInstance;
-		
-		public GraphvizConfigurationPanelNew(GraphvizCanvas graphvizCanvas) {
-			backgroundColorPanel.add(backgroundColorChooser);
-			this.graphvizCanvasInstance = graphvizCanvas;
-			
-		}
+//public class GraphvizConfigurationPanelNew  extends ConfigurationPanel {
 
-		@Override
-		public void commit() {
-			graphvizCanvasInstance.setBackground(backgroundColorChooser.getColor());
-		}
+//protected JPanel backgroundColorPanel = new JPanel();
+//protected JColorChooser backgroundColorChooser = new JColorChooser();
 
-		@Override
-		public void init() {
-		backgroundColorChooser.setColor(graphvizCanvasInstance.getBackground());	
-		}
+public class GraphvizConfigurationPanelNew extends ConfigurationPanel {
+
+	protected GraphvizCanvas graphvizCanvasInstance;
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextArea htmlArea = new JTextArea();
+//	private JColorChooser backgroundColorChooser = new JColorChooser();
+//	private JPanel backgroundColorPanel = new JPanel();
+
+	public GraphvizConfigurationPanelNew(GraphvizCanvas graphvizCanvasInstance) {
+		htmlArea.setWrapStyleWord(true);
+		htmlArea.setLineWrap(true);
+		setLayout(new BorderLayout());
+		add(new JLabel("HTML or plain text message"), BorderLayout.NORTH);
+
+		//backgroundColorPanel.add(backgroundColorChooser);
+		this.graphvizCanvasInstance = graphvizCanvasInstance;
 
 	}
+
+	@Override
+	public void commit() {
+		// TODO Auto-generated method stub
+		System.out.println("GraphvizConfigurationPanelNew: init() run.");
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		System.out.println("GraphvizConfigurationPanelNew: init() run.");
+		htmlArea.setLineWrap(false);
+//		backgroundColorChooser.setColor(graphvizCanvasInstance.getBackground());	
+
+	}
+
+
+	@Override
+	public GUIComponent getComponent() {
+		System.out.println("Config panel New : getComponent.");
+		return graphvizCanvasInstance;
+	}	
+	
+	@Override
+	public void setComponent(GUIComponent comp) {
+		if (comp instanceof GraphvizCanvas) {
+			graphvizCanvasInstance = (GraphvizCanvas)comp;
+			System.out.println("Config panel New : setComponent.");
+		}
+	}
+}
 
