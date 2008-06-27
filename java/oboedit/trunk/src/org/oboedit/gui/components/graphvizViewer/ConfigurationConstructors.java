e535ee99ccde965cc6cd87e8ab72dc42f52eb101
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-06-27 13:14:43 +0000

    Gradually copying swing components from old option window to new checkbox option window for Graphviz Viewer. Tidying code to make it more readable.

--- java/oboedit/trunk/src/org/oboedit/gui/components/graphvizViewer/ConfigurationConstructors.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/graphvizViewer/ConfigurationConstructors.java
@@ -14,10 +14,10 @@ import org.oboedit.controller.SessionManager;
 
 import org.apache.log4j.*;
 
-public class GraphvizConfigurationOld implements ComponentConfiguration {
+public class ConfigurationConstructors implements ComponentConfiguration {
 
 	//initialize logger
-	protected final static Logger logger = Logger.getLogger(GraphvizConfigurationOld.class);
+	protected final static Logger logger = Logger.getLogger(ConfigurationConstructors.class);
 	protected Map colorMap = new HashMap();
 	protected Color bgcolor = Color.black;
 
@@ -49,7 +49,7 @@ public class GraphvizConfigurationOld implements ComponentConfiguration {
 	
 	protected boolean doFiltering = false;
 
-	public GraphvizConfigurationOld() {
+	public ConfigurationConstructors() {
 	}
 
 	public Color getBGColor() {
@@ -78,40 +78,7 @@ public class GraphvizConfigurationOld implements ComponentConfiguration {
 		return labelFont;
 	}
 
-	public Vector getNamedColorList() {
-		Vector data = new Vector();
-		data.add(new NamedColor(GraphvizCanvas.BACKGROUND_COLOR, bgcolor));
-
-		data.add(new NamedColor(GraphvizCanvas.TERM_BACKGROUND_COLOR, termBoxColor));
-		data.add(new NamedColor(GraphvizCanvas.TERM_TEXT_COLOR, termFontColor));
-		data.add(new NamedColor(GraphvizCanvas.TERM_STROKE_COLOR, termStrokeColor));
-
-		data.add(new NamedColor(GraphvizCanvas.TYPE_BACKGROUND_COLOR, typeBoxColor));
-		data.add(new NamedColor(GraphvizCanvas.TYPE_STROKE_COLOR, typeStrokeColor));
-		data.add(new NamedColor(GraphvizCanvas.TYPE_TEXT_COLOR, typeFontColor));
-
-		data.add(new NamedColor(GraphvizCanvas.OBSOLETE_BACKGROUND_COLOR,
-				obsoleteBoxColor));
-		data.add(new NamedColor(GraphvizCanvas.OBSOLETE_STROKE_COLOR,
-				obsoleteStrokeColor));
-		data.add(new NamedColor(GraphvizCanvas.OBSOLETE_TEXT_COLOR, obsoleteFontColor));
-
-		Iterator it = TermUtil.getRelationshipTypes(SessionManager.getManager().getSession()).iterator();
-		while (it.hasNext()) {
-			OBOProperty type = (OBOProperty) it.next();
-			ColorPair pair;
-			// something wrong?
-			pair = (ColorPair) colorMap.get(type.getID());
-			if (pair == null)
-				pair = (ColorPair) GraphvizCanvas.defaultLabelColors.clone();
-
-			TypeColorPair tcp = new TypeColorPair(pair, type.getID());
-
-			data.add(tcp);
-		}
-		return data;
-	}
-
+	
 	public Font getNodeFont() {
 		return nodeFont;
 	}
