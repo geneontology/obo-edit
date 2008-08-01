1c987fd14eb8f1d7b5a1db7c52ca525cd40637d6
siddbasu <siddbasu@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-08-01 14:58:53 +0000

    removing old folder

--- go-dev/RefGenome/branches/RefGenomeSiddNew/src/org/bbop/client/Listener/RefGenomeViewListenerI.java
+++
@@ -1,9 +0,0 @@
-package org.bbop.client.Listener;
-
-public interface RefGenomeViewListenerI {
-
-	public void doLogin(String user, String pass);
-	public void fetchTargetIds();
-	public void fetchByName(String name);
-	public void fetchTaxonids();
-}
