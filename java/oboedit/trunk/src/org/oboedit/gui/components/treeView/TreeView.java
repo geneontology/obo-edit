b2752b5a8f1df29a0f257faa6b032db67efc2e21
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-08-21 13:27:41 +0000

    Added back main GUI.

--- java/oboedit/trunk/src/org/oboedit/gui/components/treeView/TreeView.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/treeView/TreeView.java
@@ -2,36 +2,49 @@ package org.oboedit.gui.components.treeView;
 
 /** This class used to be called DAGView */
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
-import org.bbop.framework.AbstractGUIComponent;
-import org.bbop.framework.ComponentConfiguration;
-import org.bbop.framework.GUIManager;
-import org.bbop.swing.*;
-import org.bbop.swing.plaf.DragFriendlyTreeUI;
-import org.bbop.util.*;
-import org.obo.datamodel.*;
-import org.oboedit.controller.SelectionManager;
-import org.oboedit.controller.SessionManager;
-import org.oboedit.gui.*;
-import org.oboedit.gui.components.graphvizViewer.GraphvizConfigPanel;
-import org.oboedit.gui.components.graphvizViewer.GraphvizSettings;
-import org.oboedit.gui.event.*;
-import org.oboedit.util.GUIUtil;
-import org.oboedit.util.PathUtil;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.ToolTipManager;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.tree.DefaultTreeSelectionModel;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
+
+import org.apache.log4j.Logger;
 import org.bbop.framework.AbstractGUIComponent;
 import org.bbop.framework.ComponentConfiguration;
 import org.bbop.framework.ConfigurationPanel;
-
-
-import java.util.*;
-import java.awt.*;
-import java.awt.event.*;
-
-import javax.swing.tree.*;
-import javax.swing.event.*;
-
-import org.apache.log4j.*;
+import org.bbop.swing.PathTreeModel;
+import org.bbop.swing.plaf.DragFriendlyTreeUI;
+import org.obo.datamodel.LinkedObject;
+import org.obo.datamodel.RootAlgorithm;
+import org.oboedit.controller.SelectionManager;
+import org.oboedit.controller.SessionManager;
+import org.oboedit.gui.OBOCellRenderer;
+import org.oboedit.gui.PathTask;
+import org.oboedit.gui.Preferences;
+import org.oboedit.gui.event.ReconfigEvent;
+import org.oboedit.gui.event.ReconfigListener;
+import org.oboedit.gui.event.ReloadEvent;
+import org.oboedit.gui.event.ReloadListener;
+import org.oboedit.gui.event.SelectionEvent;
+import org.oboedit.gui.event.SelectionListener;
+import org.oboedit.util.GUIUtil;
+import org.oboedit.util.PathUtil;
 
 public class TreeView extends AbstractGUIComponent {
 
@@ -53,25 +66,31 @@ public class TreeView extends AbstractGUIComponent {
 
 
 	protected boolean multiTerm() {
-		//logger.debug("TreeView: multiTerm method.");
+		logger.debug("TreeView: multiTerm method.");
 		return treeViewSettingsInstance.getMultiSelect();
 	}
 
-	protected SelectionListener selectionListener;
+	protected SelectionListener selectionListener = new SelectionListener() {
+
+		public void selectionChanged(SelectionEvent e) {
+			update();
+			logger.debug("TreeView: selectionListener method.");
+		}
+	};;
 	protected ReloadListener historyListener;
 	TreeModel model;
 
 	ReconfigListener reconfigListener = new ReconfigListener() {
 		public void configReloaded(ReconfigEvent e) {
 			setToolTips();
-			//logger.debug("TreeView: configReloaded method.");
+			logger.debug("TreeView: configReloaded method.");
 		}
 	};
 
 	MouseInputAdapter clickListener = new MouseInputAdapter() {
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			//logger.debug("TreeView: mouseClicked method.");
+			logger.debug("TreeView: mouseClicked method.");
 			if (SwingUtilities.isMiddleMouseButton(e)
 					|| SwingUtilities.isRightMouseButton(e)) {
 				TreePath path = tree.getSelectionPath();
@@ -94,11 +113,63 @@ public class TreeView extends AbstractGUIComponent {
 
 
 	
+	public void init() {
+		removeAll();
+		DefaultTreeSelectionModel selectionModel = new DefaultTreeSelectionModel();
+		selectionModel
+				.setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(200, 200));
+		setOpaque(true);
+		tree = new RestrictedJTree();
+		tree.setUI(getDefaultUI());
+		tree.setCellRenderer(new OBOCellRenderer());
+		tree.setSelectionModel(selectionModel);
+		tree.putClientProperty("JTree.lineStyle", "Angled");
+		pane = new JScrollPane(tree);
+
+		/*
+		 * String multiSelectStr = props.getProperty("multiselect"); boolean
+		 * multiSelect = false; if (multiSelectStr != null &&
+		 * multiSelectStr.equals("true")) multiSelect = true;
+		 * multiTermCheckbox.setSelected(multiSelect);
+		 */
+		JPanel controlPanel = new JPanel();
+		controlPanel.setOpaque(false);
+		controlPanel.setLayout(new BorderLayout());
+		// controlPanel.add(checkboxPanel, "East");
+		controlPanel.add(configButton, "East");
+		controlPanel.add(statusLabel, "Center");
+
+		add("Center", pane);
+		add("South", controlPanel);
+		emptyLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+		tree.setRootVisible(false);
+		tree.setShowsRootHandles(true);
+		update();
+		selectionListener = new SelectionListener() {
+			public void selectionChanged(SelectionEvent e) {
+				update();
+			}
+		};
+		historyListener = new ReloadListener() {
+			public void reload(ReloadEvent e) {
+				update();
+			}
+		};
+
+		SelectionManager.getManager().addSelectionListener(selectionListener);
+		Preferences.getPreferences().addReconfigListener(reconfigListener);
+		GUIUtil.addReloadListener(historyListener);
+		tree.addMouseListener(clickListener);
+		setToolTips();
+	}
+
 	
 	@Override
 	public String getName() {
-//		return "DAG Viewer";
-		//logger.debug("TreeView: getName method.");
+		logger.debug("TreeView: getName method.");
 
 		return "Tree Viewer";
 	}
@@ -106,7 +177,7 @@ public class TreeView extends AbstractGUIComponent {
 
 	@Override
 	public ConfigurationPanel getConfigurationPanel() {
-		//logger.debug("TreeView: getConfigurationPanel()");
+		logger.debug("TreeView: getConfigurationPanel()");
 
 		if (treeViewConfigPanelInstance == null) {
 			treeViewConfigPanelInstance = new TreeViewConfigPanel(this);
@@ -116,7 +187,7 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 	public void setToolTips() {
-		//logger.debug("TreeView: setToolTips method.");
+		logger.debug("TreeView: setToolTips method.");
 
 		if (Preferences.getPreferences().getShowToolTips())
 			ToolTipManager.sharedInstance().registerComponent(tree);
@@ -127,17 +198,17 @@ public class TreeView extends AbstractGUIComponent {
 	
 	@Override
 	public ComponentConfiguration getConfiguration() {
-		//logger.debug("TreeView: getConfiguration method.");
-//		treeViewSettingsInstance.setMultiSelect(treeViewSettingsInstance.getMultiSelect());
-//		treeViewSettingsInstance.setTrimPaths(trimPaths());
-//		treeViewSettingsInstance.setShowNonTransitive(showNonTransitive());
+		logger.debug("TreeView: getConfiguration method.");
+		treeViewSettingsInstance.setMultiSelect(treeViewSettingsInstance.getMultiSelect());
+		treeViewSettingsInstance.setTrimPaths(treeViewSettingsInstance.getTrimPaths());
+		treeViewSettingsInstance.setShowNonTransitive(treeViewSettingsInstance.getShowNonTransitive());
 		return treeViewSettingsInstance;
 	}
 
 
 
 	public void setConfiguration(TreeViewSettings treeViewSettingsInstance) {
-		//logger.debug("TreeView: setConfiguration method.");
+		logger.debug("TreeView: setConfiguration method.");
 
 		if (treeViewSettingsInstance != null && treeViewSettingsInstance instanceof TreeViewSettings)
 			this.treeViewSettingsInstance = (TreeViewSettings) this.treeViewSettingsInstance;
@@ -150,14 +221,14 @@ public class TreeView extends AbstractGUIComponent {
 
 	public TreeView(String id) {
 		super(id);
-		//logger.debug("TreeView: constructor.");
+		logger.debug("TreeView: constructor.");
 
 	}
 	
 
 	
 	public void cleanup() {
-		//logger.debug("TreeView: cleanup method.");
+		logger.debug("TreeView: cleanup method.");
 
 		SelectionManager.getManager()
 		.removeSelectionListener(selectionListener);
@@ -166,7 +237,7 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 	protected TreePath trimPathToNode(TreePath path, Object node) {
-		//logger.debug("TreeView: trimPathToNode method.");
+		logger.debug("TreeView: trimPathToNode method.");
 
 		Object[] objects = path.getPath();
 		int length = 0;
@@ -191,7 +262,7 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 	public void finishUpdate(TreePath[] paths) {
-		//logger.debug("TreeView: finishUpdate method.");
+		logger.debug("TreeView: finishUpdate method.");
 
 		if (paths.length == 0) {
 			if (isAncestorOf(progressBar)) {
@@ -229,13 +300,13 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 	protected void doUpdate() {
-		//logger.debug("TreeView: doUpdate method.");
+		logger.debug("TreeView: doUpdate method.");
 
 		final PathTask task = new PathTask();
 		task.addPostExecuteRunnable(new Runnable() {
 
 			public void run() {
-				//logger.debug("TreeView: run method.");
+				logger.debug("TreeView: run method.");
 
 				Collection<TreePath> pathc = task.getResults();
 				Iterator<TreePath> it = pathc.iterator();
@@ -271,7 +342,7 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 //	protected void showConfigurationWindow() {
-//		//logger.debug("TreeView: showConfigurationWindow method.");
+//		logger.debug("TreeView: showConfigurationWindow method.");
 //
 //		JButton closeButton = new JButton("Close");
 //
@@ -299,28 +370,17 @@ public class TreeView extends AbstractGUIComponent {
 //	}
 
 	public void update() {
-		//logger.debug("TreeView: update method.");
+		logger.debug("TreeView: update method.");
 
 		treeViewConfigPanelInstance.eventQueue.cancelAll();
 		if (SelectionManager.getGlobalSelection().isEmpty()) {
 			if (!isAncestorOf(treeViewConfigPanelInstance.emptyLabel)) {
-				remove(treeViewConfigPanelInstance.pane);
-				add(treeViewConfigPanelInstance.emptyLabel, "Center");
+				
 				validate();
 				repaint();
 			}
 		} else {
-			if (isAncestorOf(treeViewConfigPanelInstance.emptyLabel)) {
-				remove(treeViewConfigPanelInstance.emptyLabel);
-			}
 
-			if (isAncestorOf(treeViewConfigPanelInstance.pane)) {
-				remove(treeViewConfigPanelInstance.pane);
-			}
-
-			if (!isAncestorOf(treeViewConfigPanelInstance.progressBar)) {
-				add(treeViewConfigPanelInstance.progressBar, "Center");
-			}
 			validate();
 			repaint();
 			doUpdate();
@@ -328,7 +388,7 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 	protected DragFriendlyTreeUI getDefaultUI() {
-		//logger.debug("TreeView: getDefaultUI method.");
+		logger.debug("TreeView: getDefaultUI method.");
 
 		DragFriendlyTreeUI ui = new DragFriendlyTreeUI();
 		ui.setRightChildIndent(0);
@@ -336,26 +396,26 @@ public class TreeView extends AbstractGUIComponent {
 	}
 
 //	public boolean showNonTransitive() {
-//		//logger.debug("TreeView: showNonTransitive method.");
+//		logger.debug("TreeView: showNonTransitive method.");
 //		return showNonTransitiveCheckbox.isSelected();
 //	}
 //
 //	public boolean trimPaths() {
-//		//logger.debug("TreeView: trimPaths method.");
+//		logger.debug("TreeView: trimPaths method.");
 //		return trimPathsCheckbox.isSelected();
 //	}
 
 //	public JComponent getComponent() {
-//		//logger.debug("TreeView: getComponent method.");
+//		logger.debug("TreeView: getComponent method.");
 //		return this;
 //	}
 
 	public boolean isXMLSettable() {
-		//logger.debug("TreeView: isXMLSettable method.");
+		logger.debug("TreeView: isXMLSettable method.");
 		return false;
 	}
 
 	public void setXML(String xml) {
-		//logger.debug("TreeView: setXML method.");
+		logger.debug("TreeView: setXML method.");
 	}
 }
