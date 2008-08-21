5b79c61074dbbc2ef0f15c8108d0643e5dcb82fb
Suzanna Lewis <selewis@users.noreply.github.com>
2008-08-21 00:24:14 +0000

    Fixed bug in retrieving DBXref, namely the DB_name attribute cannot always depend on a corresponding entry in the DB table being there (ugh), so have to tolerate absent DBs (e.g. GenBank)
    Added mapping and retrieval of homology sets

--- GHOUL/src/org/geneontology/db/factory/GOobjectFactory.java
+++ GHOUL/src/org/geneontology/db/factory/GOobjectFactory.java
@@ -1,10 +1,8 @@
 package org.geneontology.db.factory;
 
+import org.geneontology.db.model.DB;
 import org.geneontology.db.model.DBXref;
-import org.geneontology.db.model.GOModel;
 import org.geneontology.db.model.GeneProduct;
-import org.geneontology.db.model.ProductSeq;
-import org.geneontology.db.model.Sequence;
 import org.geneontology.db.model.Species;
 import org.geneontology.db.model.Term;
 import org.hibernate.Session;
@@ -23,30 +21,66 @@ public class GOobjectFactory {
 	}
 	
 		
+	/** Term  factories 
+	 */
+	
 	/**
-	 * Fetches a {@link Transcript} from the database with the specified feature_id
-	 * @param feature_id the feature_id of the {@link Transcript} object to be retrieved from the database.
-	 * @return a {@link Transcript} object with the specified feature_id, if it exists. 
+	 * getTermByName: Fetches a Term from the database with the specified name
 	 */
 	public Term getTermByName(String name){
 		Session session = sf.getCurrentSession();
 		return (Term)session.createQuery("from Term where name = ?").setString(0, name).uniqueResult();
 	}
 	
-	public GeneProduct getGPByDBXref(int dbxref_id){
+	/** Gene product factories 
+	 */
+	
+	/** 
+	 * getGPByDBXref: Fetches a gene prduct using the unique identifier as the bait
+	 * @param int dbxref_id
+	 * @return GeneProduct
+	 */
+	public GeneProduct getGPByID(int dbxref_id){
 		Session session = sf.getCurrentSession();
 		return (GeneProduct)session.createQuery("from GeneProduct where dbxref_id = ?").setInteger(0, dbxref_id).uniqueResult();
 	}
 	
+	/** General DB xref utility factories
+	 */
+	
+	/**
+	 * getDBXrefByID
+	 * @param int id
+	 * @return DBXref
+	 */
+	public DBXref getDBXrefByID (int id) {
+		Session session = sf.getCurrentSession();
+		return (DBXref)session.createQuery("from DBXref where id = ?").setInteger(0, id).uniqueResult();
+	}
+
+	/**
+	 * getDBByName
+	 * @param String name
+	 * @return DB
+	 */
+	public DB getDBByName (String name) {
+		Session session = sf.getCurrentSession();
+		return (DB)session.createQuery("from DB where name = ?").setString(0, name).uniqueResult();
+	}
+
+	/** Association factories
+	 */
+	
+	/**
+	 * getSpeciesByTaxa
+	 * @param taxa
+	 * @return Species
+	 */
 	public Species getSpeciesByTaxa(int taxa){
 		Session session = sf.getCurrentSession();
 		return (Species)session.createQuery("from Species where ncbi_taxa_id = ?").setInteger(0, taxa).uniqueResult();
 	}
 	
-	public DBXref getDBXrefByID (int id) {
-		Session session = sf.getCurrentSession();
-		return (DBXref)session.createQuery("from DBXref where id = ?").setInteger(0, id).uniqueResult();
-	}
-	
+
 }
 
--- GHOUL/src/org/geneontology/db/test/TestGeneProductAPI.java
+++ GHOUL/src/org/geneontology/db/test/TestGeneProductAPI.java
@@ -5,24 +5,18 @@ import org.geneontology.db.model.GeneProduct;
 import org.geneontology.db.model.ProductSeq;
 import org.geneontology.db.model.Sequence;
 
-public class GOHibernateTestGeneProductAPI extends AbstractGOHibernateAPITest{
+public class TestGeneProductAPI extends AbstractGOHibernateAPITest{
 	
 	// These probably will need to be adjusted as the database changes. 
 	private final int test_val = 238988;
 
-	public GOHibernateTestGeneProductAPI () {
+	public TestGeneProductAPI () {
 		super();
 	}
 	 
 	public void testGeneProductQuery(){
-		/*
-		GOobjectFactory goFactory = initSessionFactory();
-		
-		GeneProduct gp = goFactory.getGPByDBXref(test_val);
-		prettyPrint (gp);
-		*/
 		this.getSessionFactory().getCurrentSession().beginTransaction();
-		GeneProduct gp = (GeneProduct) this.getSessionFactory().getCurrentSession().get(GeneProduct.class, 141899);
+		GeneProduct gp = (GeneProduct) this.getSessionFactory().getCurrentSession().get(GeneProduct.class, 250070);
 		prettyPrint (gp);
 	}
 	
