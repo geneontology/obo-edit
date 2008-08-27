8fd9d1f754f365942cb0c1c567c6381cd689b778
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-08-27 14:17:06 +0000

    Adding this file as a place for package level comments for the Tree Viewer.

---
+++ java/oboedit/trunk/src/org/oboedit/gui/components/treeView/package-info.java
@@ -0,0 +1,14 @@
+
+/** 
+ * The Tree Viewer package. The Tree Viewer displays a tree style representation of
+ * the graph running up to the ontology root, and down to the term or terms 
+ * selected in the Ontology Tree Editor. All possible paths from leaf node to root 
+ * are shown in this view, and the graph is not editable in this component.
+ * 
+ * This package is currently under construction. The version of the Tree Viewer 
+ * currently in use is in org.oboedit.gui.components. The old version is configured
+ * via an options button, whereas this version will be configured via the 
+ * wrench icon. 
+ * 
+ * */ 
+package org.oboedit.gui.components.treeView;
