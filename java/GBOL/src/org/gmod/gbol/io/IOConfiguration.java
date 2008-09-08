699e3cb8c991e639d71428d1994323af660c374c
rbruggne <rbruggne@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-09-08 18:29:47 +0000

    Renaming stuff again

--- java/GBOL/src/org/gmod/gbol/io/IOConfiguration.java
+++
@@ -1,8 +0,0 @@
-package org.gmod.gbol.io;
-
-public abstract class IOConfiguration{
-
-	
-	
-	
-}
--- java/GBOL/src/org/gmod/gbol/test/FileHandlerTest.java
+++ java/GBOL/src/org/gmod/gbol/test/FileHandlerTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.gmod.gbol.io.FileHandler;
 import org.gmod.gbol.io.impl.GFF3Handler;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class FileHandlerTest extends TestCase {
@@ -14,9 +15,11 @@ public class FileHandlerTest extends TestCase {
 	
 	public void testConstructor(){
 		try {
-			this.fileHandler = new GFF3Handler(filePath,null);
+			this.fileHandler = new GFF3Handler(filePath,null,null);
+			Assert.assertTrue("Opened file.",true);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			Assert.assertTrue("Could not open file: " + e.getMessage(),false);
 			e.printStackTrace();
 		}
 	}
