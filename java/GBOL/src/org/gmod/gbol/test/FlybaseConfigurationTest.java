c234ff1af2ec95fe41596106dcb44cc65c76abd1
rbruggne <rbruggne@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-09-13 01:43:41 +0000

    Small updates

--- java/GBOL/src/org/gmod/gbol/test/FlybaseConfigurationTest.java
+++ java/GBOL/src/org/gmod/gbol/test/FlybaseConfigurationTest.java
@@ -1,7 +1,13 @@
 package org.gmod.gbol.test;
 
+import java.util.Collection;
+
+import org.gmod.gbol.simpleObject.CV;
 import org.gmod.gbol.simpleObject.CVTermRelationship;
 import org.gmod.gbol.simpleObject.CVTerm;
+import org.gmod.gbol.simpleObject.Feature;
+import org.gmod.gbol.simpleObject.FeatureRelationship;
+import org.hibernate.Query;
 
 public class FlybaseConfigurationTest extends AbstractGBOLHibernateTest{
 
@@ -20,11 +26,30 @@ public class FlybaseConfigurationTest extends AbstractGBOLHibernateTest{
 	}
 	
 	public void testConfigure(){
+		/*
 		CVTerm cvterm = (CVTerm) this.sf.getCurrentSession().get(CVTerm.class, 30);
 		System.out.println("Parent: " + cvterm.getName());
 		for (CVTermRelationship cvtr : cvterm.getChildCVTermRelationships()){
 			System.out.println(cvtr.getSubjectCVTerm().getName() + " " + cvtr.getType().getName() + " " + cvtr.getObjectCVTerm().getName());
 		}
+		*/
+		
+		Query q = this.sf.getCurrentSession().createQuery("from CV where name=?");
+		q.setString(0, "SO");
+		
+		CV cv = (CV)q.uniqueResult();
+		System.out.println("CV: " + cv.getName());
+
+		cv.setName("sequence_ontology");
+		
+		Feature f = null;
+		
+		f.getType().setName("new_name");
+		
+		
+		
+		this.sf.getCurrentSession().save(f);
+		
 		
 	}
 	
