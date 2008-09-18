a0f0504928c239c43e68050827802ccf5c020aa1
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-09-18 15:27:50 +0000

    Added documentation and debugging print statements.

--- java/oboedit/trunk/src/org/oboedit/gui/components/treeView/TreeViewSettings.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/treeView/TreeViewSettings.java
@@ -3,6 +3,11 @@ package org.oboedit.gui.components.treeView;
 import org.apache.log4j.Logger;
 import org.bbop.framework.ComponentConfiguration;
 
+/**
+  * @author John Day-Richter, Jennifer Deegan, and Nicolas Rodriguez.<br>
+  * Docs by Jennifer Deegan and Nicolas Rodriguez.
+  *
+ */
 public class TreeViewSettings implements ComponentConfiguration {
 	protected boolean multiSelect = getMultiSelect();
 
@@ -26,26 +31,40 @@ public class TreeViewSettings implements ComponentConfiguration {
 	}
 
 	public void setShowNonTransitive(boolean showNonTransitive) {
+		System.out.println("TreeViewSettings: setShowNonTransitive");
 		this.showNonTransitive = showNonTransitive;
 	}
 
 	public boolean getShowNonTransitive() {
+		System.out.println("TreeViewSettings: getShowNonTransitive");
 		return showNonTransitive;
 	}
 
+	/**
+	 * @param multiSelect
+	 */
 	public void setMultiSelect(boolean multiSelect) {
+		System.out.println("TreeViewSettings: setMultiSelect");
 		this.multiSelect = multiSelect;
 	}
 
+	/**
+	 * Returns whether or not the component is set up to carry over multiple selection of terms from the Ontology Tree Editor.
+	 * 
+	 * @return whether or not the component is set up to carry over multiple selection of terms from the Ontology Tree Editor.
+	 */
 	public boolean getMultiSelect() {
+		System.out.println("TreeViewSettings: getMultiSelect");
 		return multiSelect;
 	}
 
 	public void setTrimPaths(boolean trimPaths) {
+		System.out.println("TreeViewSettings: setTrimPaths");
 		this.trimPaths = trimPaths;
 	}
 
 	public boolean getTrimPaths() {
+		System.out.println("TreeViewSettings: getTrimPaths");
 		return trimPaths;
 	}
 }
