e9f0d8224c070db2557472f66b3dbd3a3a42ac08
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-09-18 15:31:54 +0000

    Fixing documentation.

--- java/oboedit/trunk/src/org/oboedit/gui/components/treeView/package-info.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/treeView/package-info.java
@@ -1,5 +1,9 @@
 
 /** 
+ * @author John Day-Richter, Jennifer Deegan, and Nicolas Rodriguez.<br>
+ * Docs by Jennifer Deegan and Nicolas Rodriguez.
+ * 
+ * 
  * The Tree Viewer package. The Tree Viewer displays a tree style representation of
  * the graph running up to the ontology root, and down to the term or terms 
  * selected in the Ontology Tree Editor. All possible paths from leaf node to root 
