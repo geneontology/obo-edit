67f085a00b5f7fb54fb3edf71575b11d1a0cb9b4
gk_fan <gk_fan@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-10-10 21:57:16 +0000

    + Updated test suites

--- java/GBOL/branch/src/org/gmod/gbol/test/BioObjectHandlerTest.java
+++ java/GBOL/branch/src/org/gmod/gbol/test/BioObjectHandlerTest.java
@@ -3,7 +3,8 @@ package org.gmod.gbol.test;
 import java.util.Collection;
 
 import org.apache.log4j.PropertyConfigurator;
-import org.gmod.gbol.bioObject.AbstractSingleLocationBioObject;
+import org.gmod.gbol.bioObject.AbstractBioFeature;
+import org.gmod.gbol.bioObject.AbstractSingleLocationBioFeature;
 import org.gmod.gbol.bioObject.Exon;
 import org.gmod.gbol.bioObject.Gene;
 import org.gmod.gbol.bioObject.Transcript;
@@ -21,14 +22,37 @@ public class BioObjectHandlerTest extends TestCase {
 	public BioObjectHandlerTest() throws Exception
 	{
 		PropertyConfigurator.configure("testSupport/log4j.properties");
-		BioObjectConfiguration conf = new BioObjectConfiguration("testSupport/gbolOne.mapping.xml");
-		SimpleObjectIOInterface h = new HibernateHandler("testSupport/gbolOne.cfg.xml");
+		BioObjectConfiguration conf = new BioObjectConfiguration("testSupport/gbolTwo.mapping.xml");
+		SimpleObjectIOInterface h = new HibernateHandler("testSupport/gbolTwo.cfg.xml");
 		handler = new BioObjectHandler(conf, h);
+	}
+	
+	public void testGetAllFeatures() throws Exception
+	{
+		Collection<AbstractBioFeature> features = handler.getAllFeatures();
+		assertEquals(features.size(), 8);
+		int numGenes = 0;
+		int numTranscripts = 0;
+		int numExons = 0;
+		for (AbstractBioFeature f : features) {
+			if (f instanceof Gene) {
+				++numGenes;
+			}
+			if (f instanceof Transcript) {
+				++numTranscripts;
+			}
+			if (f instanceof Exon) {
+				++numExons;
+			}
+		}
+		assertEquals("Expected number of genes", 2, numGenes);
+		assertEquals("Expected number of transcripts", 2, numTranscripts);
+		assertEquals("Expected number of exons", 3, numExons);
 	}
 	
 	public void testGetGenes() throws Exception
 	{
-		Collection<Gene> genes = handler.getGenes();
+		Collection<Gene> genes = handler.getAllGenes();
 		assertEquals(genes.size(), 2);
 		for (Gene gene : genes) {
 			printFeatureInfo(gene, 0);
@@ -44,7 +68,16 @@ public class BioObjectHandlerTest extends TestCase {
 		}
 	}
 	
-	private void printFeatureInfo(AbstractSingleLocationBioObject feature, int indent)
+	public void testWrite() throws Exception
+	{
+		Collection<Gene> genes = handler.getAllGenes();
+		BioObjectConfiguration destConf = new BioObjectConfiguration("testSupport/gbolThree.mapping.xml");
+		SimpleObjectIOInterface h = new HibernateHandler("testSupport/gbolThree.cfg.xml");
+		BioObjectHandler destHandler = new BioObjectHandler(destConf, h);
+		destHandler.write(genes);
+	}
+	
+	private void printFeatureInfo(AbstractSingleLocationBioFeature feature, int indent)
 	{
 		for (int i = 0; i < indent; ++i) {
 			System.out.print("\t");
