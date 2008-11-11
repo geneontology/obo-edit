fa0fa4a981318e244c6231d1d99b49103562161d
yostinso <yostinso@6f0e8829-b336-0410-acfb-cb9b228023ad>
2008-11-11 23:34:44 +0000

    Restore the trunk folder to what it was before the branching

---
+++ java/GBOL/trunk/GBOL/src/org/gmod/gbol/util/HibernateUtil.java
@@ -0,0 +1,47 @@
+package org.gmod.gbol.util;
+
+import java.io.File;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+/**
+ * HibernateUtil is a class that creates a new Hibernate {@link SessionFactory}
+ * based on the users configuration file.
+ * 
+ * @see SessionFactory
+ * @see HibernateException
+ * @see Configuration
+ * @author Robert Bruggner
+ * 
+ */
+public class HibernateUtil {
+	
+
+	/**
+	 * Returns a new Hibernate {@link SessionFactory} given a hibernate configuration file.
+	 * 
+	 * @param filename
+	 *            Hibernate configuration filename
+	 * @return a {@link SessionFactory}
+	 * @throws Exception
+	 *             when configuration of the Hibernate {@link SessionFactory} fails.  
+	 */
+    public static SessionFactory buildSessionFactory(String filename) throws Exception{
+    	try {
+    		 
+    		 System.out.println("Trying to configure using " + filename);
+    		 File f = new File(filename);
+    		 
+    		 Configuration c = new Configuration().configure(f);
+    		 SessionFactory sessionFactory = c.buildSessionFactory();
+    		 return sessionFactory;
+    	} catch (HibernateException he){
+    		System.out.println("Couldn't build session!");
+    		System.out.println(he.getMessage());
+    		System.err.println("Couldn't build session. " + he.getMessage());
+    		he.printStackTrace();
+    		throw he;
+    	}
+    }    
+}
