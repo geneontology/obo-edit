e64f51b5d79835179ccb9a75e421d6760d91177a
Chris Mungall <cjm@berkeleybop.org>
2008-12-22 06:35:24 +0000

    changed target search to use anonymous asynccallback classes

--- go-dev/RefGenome/branches/gwt1.5/src/org/bbop/client/Listener/RefGenomeViewListenerI.java
+++ go-dev/RefGenome/branches/gwt1.5/src/org/bbop/client/Listener/RefGenomeViewListenerI.java
@@ -1,17 +1,22 @@
 package org.bbop.client.Listener;
 
+import org.bbop.client.RefGenomeService;
+import org.bbop.client.RefGenomeServiceAsync;
 import org.bbop.client.model.DateDTO;
 
+// implemented by: RefGenomeServiceClientImpl
 public interface RefGenomeViewListenerI {
 
+	public RefGenomeServiceAsync getRefGenomeService();
+	
 	public void doLogin(String user, String pass);
 	public void fetchTargetIds();
 	public void fetchByName(String name);
 	public void fetchByNameAndTaxon(String name, String taxonId);
 	public void fetchTargetNodesByName(String name);
 	public void fetchTaxonNodes();
-	public void uploadFile(String userId, String filePath, String fileType);
 	public void fetchTargets();
+	public void uploadFile(String userId, String filePath, String fileType);
 	public void assignEntityTargetStatus(String userId, String id, DateDTO date);
 	public void retractEntityTargetStatus(String userId, String id);
 	public void cancelFetch();
