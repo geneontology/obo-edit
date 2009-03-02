1f2b3efb14cddc0b4c11806f43e6c8e818683ed2
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-03-02 12:55:54 +0000

    Added documentation.

--- java/obo/trunk/src/org/obo/filters/LinkFilter.java
+++ java/obo/trunk/src/org/obo/filters/LinkFilter.java
@@ -2,6 +2,43 @@ package org.obo.filters;
 
 import org.obo.datamodel.Link;
 
+/*
+ * Here is an example of how this interface can be used along with an ObjectFilter object.
+ * 
+ * //Call the method:
+ *
+ *		LinkFilter lfilter1 = getLinkFilter("CARO:0000003", LinkFilter.CHILD);
+ *		
+ * //Method definition:
+ *
+ *	private LinkFilter getLinkFilter(String id, int aspect) {
+ *
+ *	//Initialize lfilter = Link type has Any text field contains ""
+ *	LinkFilter lfilter = (LinkFilter)lff.createNewFilter();
+ *	
+ *	//Initialize ofilter = Any text field contains ""
+ *	ObjectFilter ofilter = (ObjectFilter)off.createNewFilter();
+ *		
+ *	//Initialize EqualsComparison c = equals.
+ *	EqualsComparison c = new EqualsComparison();
+ *
+ *	//now ofilter = Any text field equals ""
+ *	ofilter.setComparison(c);
+ *
+ *	//Now ofilter = Any text field equals "CARO:0000003"
+ *	ofilter.setValue(id);
+ *
+ *	//Now lfilter = Link type has Any text field equals "CARO:0000003"
+ *	lfilter.setFilter(ofilter);
+ *
+ *	//Now lfilter = Link child has Any text field equals "CARO:0000003"
+ *	lfilter.setAspect(aspect);
+ *
+ *	return lfilter;
+ *	}
+ *
+ */
+
 public interface LinkFilter extends PathCapableFilter<Link> {
 
 	public static final int CHILD = 1;
