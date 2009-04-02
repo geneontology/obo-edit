705ca55bf073281efa9df9cb47876e43a2ca83d9
thomaswaechter <thomaswaechter@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-04-02 13:21:08 +0000

    Fixed completeness of definitions problem (does not affect OBOEdit components other then Ontology Generation Tool

--- java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/DefinitionsPopup.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/DefinitionsPopup.java
@@ -6,7 +6,8 @@ import java.awt.Container;
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.IOException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +26,10 @@ import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 
 /**
- * A popup showing the different kinds of the definition and its respective
- * URLs.
+ * A popup showing the different kinds of the definition and its respective URLs.
  * 
  * @author Goetz Fabian
  * @author Marcel Hanke
@@ -44,139 +40,239 @@ public class DefinitionsPopup extends JDialog
 	private static final long serialVersionUID = -9159196480683973991L;
 	protected final static Logger logger = Logger.getLogger(DefinitionsPopup.class);
 
-	private OntologyGenerationComponent parent;
-	
+	private Component parent;
+
+	private CandidateDefinition originalDefinition;
 	private List<DefPosPair> candidateDefinitionList;
 	private Box box;
 	private JLabel label;
 	private JScrollPane scrollPane;
 	private DefinitionPopupTable table;
 	private JButton closeButton;
-	
-	public DefinitionsPopup(OntologyGenerationComponent parent)
+	private int numberOfCallsToSummarizeDefinition;
+
+	public DefinitionsPopup(final Component parent)
 	{
 		this.parent = parent;
+		this.setModal(true);
 	}
 
 	public void initPopup(CandidateDefinition definition)
 	{
-		candidateDefinitionList = new ArrayList<DefPosPair>();
-		
-		/* Fill the List:
-		 * The first entry (position=0) is always for the definition itself (html-formatted).
-		 * The rest of the entries (position=1 ... position=def.getUrl().size()) are the URLs.
-		 * 
-		 * In the first loop, the original definition is inserted, in the second loop the 
-		 * alternative definitions are inserted. 
-		 */
-		for (int i = 0; i < (definition.getUrl().size() + 1); i++) {
-			candidateDefinitionList.add(new DefPosPair(definition, i));
-		}
-		if (definition.getAlternativeDefinitions() != null) {
-			for (CandidateDefinition alternativeDef : definition.getAlternativeDefinitions()) {
-				for (int i = 0; i < (alternativeDef.getUrl().size() + 1); i++) {
-					candidateDefinitionList.add(new DefPosPair(alternativeDef, i));
-				}
-			}
-		}
-		
+		this.originalDefinition = definition;
+
+		candidateDefinitionList = buildCandidateDefinitionList(definition);
+
 		/*
 		 * Construct objects
 		 */
 		Container contentPane = getContentPane();
 		box = new Box(BoxLayout.Y_AXIS);
-		label = new JLabel("Soure URLs for available definitions:");
+		label = new JLabel("Available Definitions:");
 		table = new DefinitionPopupTable();
 		scrollPane = new JScrollPane(table);
 		closeButton = new JButton("Close");
-		
+
 		/*
 		 * Adjust objects
 		 */
-		closeButton.addMouseListener(new MouseAdapter() {
+		closeButton.addMouseListener(new MouseAdapter()
+		{
 			@Override
 			public void mouseClicked(MouseEvent e)
 			{
 				setVisible(false);
+				parent.repaint();
 			}
 		});
-		
+
 		label.setAlignmentX(Component.LEFT_ALIGNMENT);
 		scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
 		closeButton.setAlignmentX(Component.LEFT_ALIGNMENT);
-		
+
 		contentPane.add(box);
-	
+
 		box.add(label);
 		box.add(scrollPane);
 		box.add(closeButton);
-		
+
 		box.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		
-		//fullDefinition(definition);
-		
+
 		setSize(500, 400);
-		setVisible(true);		
-	}
-	
-	private boolean fullDefinition(CandidateDefinition definition) 
-	{
-		
-		if(definition==null || definition.getCachedURL()==null || definition.getCachedURL().isEmpty()) {
-			return false;	
-		}
-		
-		HttpClient client;
-		GetMethod method = new GetMethod(definition.getCachedURL().get(0));
-		
-		client = new HttpClient();
-		try {
-			int status = client.executeMethod(method);
-			
-			if(status==HttpStatus.SC_OK) {
-				byte[] responseBody = method.getResponseBody();
-				System.out.println(new String(responseBody));
+
+		// center
+		Point parentAnchor = parent.getLocation();
+		SwingUtilities.convertPointToScreen(parentAnchor, parent);
+		int x = parentAnchor.x + (parent.getWidth() - this.getWidth()) / 2;
+		int y = parentAnchor.y + (parent.getHeight() - this.getHeight()) / 2;
+		setLocation(x, y);
+
+		/*
+		 * Add an listener capturing the done() in the worker
+		 */
+		getTable().addPropertyChangeListener(new PropertyChangeListener()
+		{
+
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				if (evt.getPropertyName().equals(DefinitionExtensionWorker.WAS_EXTENDED_PROPERTY)) {
+					summarizeDefinitions();
+				}
 			}
-		} catch (HttpException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		});
+
+		/*
+		 * Try to extend the existing definitions.
+		 */
+		for (DefPosPair defPosPair : candidateDefinitionList) {
+			if (defPosPair.isDef()) {
+				DefinitionExtensionWorker extensionWorker = new DefinitionExtensionWorker(defPosPair.def, getTable());
+				extensionWorker.execute();
+			}
 		}
-		
-		return false;
+		numberOfCallsToSummarizeDefinition = 0;
 	}
-	
-	public JTable getTable()
+
+	private List<DefPosPair> buildCandidateDefinitionList(CandidateDefinition def)
+	{
+
+		List<DefPosPair> candDefList = new ArrayList<DefPosPair>();
+		/*
+		 * Fill the List: The first entry (position=0) is always for the definition itself (html-formatted). The rest of
+		 * the entries (position=1 ... position=def.getUrl().size()) are the URLs.
+		 * 
+		 * In the first loop, the original definition is inserted, in the second loop the alternative definitions are
+		 * inserted.
+		 */
+		for (int i = 0; i < (def.getUrl().size() + 1); i++) {
+			candDefList.add(new DefPosPair(def, i));
+		}
+		if (def.getAlternativeDefinitions() != null) {
+			for (CandidateDefinition alternativeDef : def.getAlternativeDefinitions()) {
+				for (int i = 0; i < (alternativeDef.getUrl().size() + 1); i++) {
+					candDefList.add(new DefPosPair(alternativeDef, i));
+				}
+			}
+		}
+
+		return candDefList;
+	}
+
+	public DefinitionPopupTable getTable()
 	{
 		return table;
 	}
-	
-	private class DefinitionPopupTable extends JTable 
+
+	/**
+	 * Tries to summarize the existing definitions after extension as good as possible.
+	 */
+	private void summarizeDefinitions()
+	{
+		boolean updateTable = false;
+
+		numberOfCallsToSummarizeDefinition++;
+		if (originalDefinition.getAlternativeDefinitions() == null) {
+			return;
+		}
+		if (numberOfCallsToSummarizeDefinition < originalDefinition.getAlternativeDefinitions().size() + 1) {
+			return;
+		}
+		for (int i = 0; i < originalDefinition.getAlternativeDefinitions().size(); i++) {
+			CandidateDefinition def = originalDefinition.getAlternativeDefinitions().get(i);
+			if (def.getDefinition().equals(originalDefinition.getDefinition())) {
+				for (String url : def.getUrl()) {
+					boolean skipURL = false;
+					for (String alreadyExistingURL : originalDefinition.getUrl()) {
+						if (url.equals(alreadyExistingURL)) {
+							skipURL = true;
+						}
+					}
+					if (!skipURL) {
+						// add URL
+						originalDefinition.addURL(url);
+						// add the corresponding cachedURL
+						originalDefinition.addCachedURL(def.getCachedURL().get(def.getUrl().indexOf(url)));
+						// remove alternative definition
+					}
+				}
+				originalDefinition.getAlternativeDefinitions().remove(def);
+
+				updateTable = true;
+			}
+			else {
+				for (int j = originalDefinition.getAlternativeDefinitions().indexOf(def) + 1; j < originalDefinition
+				    .getAlternativeDefinitions().size(); j++) {
+					CandidateDefinition altDef = originalDefinition.getAlternativeDefinitions().get(j);
+					if (def.getDefinition().equals(altDef.getDefinition())) {
+						for (String url : altDef.getUrl()) {
+							boolean skipURL = false;
+							for (String alreadyExistingURL : def.getUrl()) {
+								if (url.equals(alreadyExistingURL)) {
+									skipURL = true;
+								}
+							}
+							if (!skipURL) {
+								// add URL
+								def.addURL(url);
+								// add the corresponding cachedURL
+								def.addCachedURL(def.getCachedURL().get(def.getUrl().indexOf(url)));
+								// remove alternative definition
+							}
+						}
+						originalDefinition.getAlternativeDefinitions().remove(altDef);
+
+						updateTable = true;
+					}
+				}
+			}
+		}
+
+		if (updateTable) {
+			candidateDefinitionList = buildCandidateDefinitionList(originalDefinition);
+
+			// reset the row heights etc.
+			getTable().initPopupTable();
+
+			getTable().repaint();
+		}
+
+	}
+
+	private class DefinitionPopupTable extends JTable
 	{
 		private static final long serialVersionUID = 108519703957872291L;
-		
+
 		public DefinitionPopupTable()
 		{
 			super(new DefinitionPopupTableModel());
-			
+
 			this.setGridColor(Color.LIGHT_GRAY);
-			
+
+			initPopupTable();
+
+		}
+
+		public void initPopupTable()
+		{
 			int cnt = 0;
 			for (DefPosPair defPosPair : candidateDefinitionList) {
-				if (defPosPair.pos == 0) {
+				if (defPosPair.isDef()) {
 					this.setRowHeight(cnt, this.getRowHeight() + 30);
-				} else {
+				}
+				else {
 					this.setRowHeight(cnt, this.getRowHeight() + 4);
 				}
 				cnt++;
 			}
-			
-			//this.setRowHeight(getRowHeight() + 30);
+
 			getColumnModel().getColumn(0).setMaxWidth(50);
 			getColumnModel().getColumn(0).setResizable(false);
 			getColumnModel().getColumn(2).setMaxWidth(30);
 			this.tableHeader.setReorderingAllowed(false);
-			
+
+			if (this.getMouseListeners().length > 0)
+				this.removeMouseListener(this.getMouseListeners()[0]);
+
 			this.addMouseListener(new MouseAdapter()
 			{
 				@Override
@@ -194,47 +290,51 @@ public class DefinitionsPopup extends JDialog
 						return;
 					}
 					// CheckBox clicked
-					else if (columnIndex == 0 && candidateDefinitionList.get(rowIndex).pos == 0) {
+					else if (columnIndex == 0 && candidateDefinitionList.get(rowIndex).isDef()) {
 						onClickAddDefinition(rowIndex, columnIndex);
 					}
 					// Only open web page on clicks on the (I)-Button
-					else if (columnIndex == 2 && candidateDefinitionList.get(rowIndex).pos > 0) {
+					else if (columnIndex == 2 && candidateDefinitionList.get(rowIndex).isURL()) {
 						onClickOpenExternalDefinitionWebPage();
 					}
 				}
 			});
+
 		}
-		
+
 		@Override
 		public DefinitionPopupTableModel getModel()
 		{
 			return (DefinitionPopupTableModel) super.getModel();
 		}
 
-
 		@Override
-		public TableCellRenderer getCellRenderer(int row, int column) 
+		public TableCellRenderer getCellRenderer(int row, int column)
 		{
-			/* 
-			 * Select the appropriate table cell renderer:
-			 * If row contains a URL (position > 0), put (I)-Button in last column,
-			 * Otherwise (row contains definition), use standard cell renderer.
+			/*
+			 * Select the appropriate table cell renderer: If row contains a URL (position > 0), put (I)-Button in last
+			 * column, Otherwise (row contains definition), use standard cell renderer.
 			 */
-			if (candidateDefinitionList.get(row).pos > 0 && column == 2) {
+			if (column == 2 && candidateDefinitionList.get(row).isURL()) {
 				return new TableCellImageRenderer("resources/aboutIcon.png");
 			}
 			else if (column == 0) {
-				return new DefaultTableCellRenderer() {
+				return new DefaultTableCellRenderer()
+				{
 					private static final long serialVersionUID = -4293679914935943300L;
+
 					public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-							boolean hasFocus, int row, int column) {
+					    boolean hasFocus, int row, int column)
+					{
 						// Check if the row belongs to a URL (=> don't display a checkBox)
-						if (candidateDefinitionList.get(row).pos > 0) {
-							JLabel comp = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+						if (candidateDefinitionList.get(row).isURL()) {
+							JLabel comp = (JLabel) super.getTableCellRendererComponent(table, value, isSelected,
+							    hasFocus, row, column);
 							comp.setText(null);
 							return comp;
 						}
-						return table.getDefaultRenderer(table.getColumnClass(column)).getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+						return table.getDefaultRenderer(table.getColumnClass(column)).getTableCellRendererComponent(
+						    table, value, isSelected, hasFocus, row, column);
 					}
 				};
 			}
@@ -242,11 +342,11 @@ public class DefinitionsPopup extends JDialog
 				return super.getCellRenderer(row, column);
 			}
 		}
-		
+
 		/*
 		 * CLICKLISTENER
 		 */
-		
+
 		/**
 		 * Launches web browser to show the webpage definition is fetched from
 		 */
@@ -255,7 +355,7 @@ public class DefinitionsPopup extends JDialog
 		{
 			int colNumber = getSelectedColumn();
 			int rowNumber = getSelectedRow();
-			
+
 			String errMsg = "Error attempting to launch web browser";
 			if (colNumber == 2 && rowNumber >= 0) {
 				String osName = System.getProperty("os.name");
@@ -287,27 +387,28 @@ public class DefinitionsPopup extends JDialog
 				}
 			}
 		}
-		
+
 		/**
 		 * Adds the definition in the given row to the editDefArea in the main plugin component.
 		 * 
-		 * @param rowIndex 
+		 * @param rowIndex
 		 * @param columnIndex
 		 */
-		private void onClickAddDefinition(int rowIndex, int columnIndex) {
+		private void onClickAddDefinition(int rowIndex, int columnIndex)
+		{
 			if (candidateDefinitionList.get(rowIndex).def.isTicked()) {
 				String definition = candidateDefinitionList.get(rowIndex).def.getDefinition();
-			
-				parent.updateEditDefArea(definition);
+
+				((OntologyGenerationComponent) (parent)).updateEditDefArea(definition);
 			}
 		}
 
 	}
-	
+
 	private class DefinitionPopupTableModel extends AbstractTableModel
 	{
 		private static final long serialVersionUID = 7816649265967843550L;
-				
+
 		public int getColumnCount()
 		{
 			return 3;
@@ -322,37 +423,37 @@ public class DefinitionsPopup extends JDialog
 		{
 			DefPosPair defPosPair = candidateDefinitionList.get(rowIndex);
 			switch (columnIndex) {
-				case 0:
-					if (defPosPair.pos == 0) {
-						return defPosPair.def.isTicked();
-					}
-					break;
-				case 1:
-					// Row contains definition
-					if (defPosPair.pos == 0) {
-						return defPosPair.def.getDefinitionHTMLFormatted();
-					}
-					// row contains URL
-					else {
-						return defPosPair.def.getUrl().get(defPosPair.pos - 1);
-					}
+			case 0:
+				if (defPosPair.isDef()) {
+					return defPosPair.def.isTicked();
 				}
+				break;
+			case 1:
+				// Row contains definition
+				if (defPosPair.isDef()) {
+					return defPosPair.def.getDefinitionHTMLFormatted();
+				}
+				// row contains URL
+				else {
+					return defPosPair.def.getUrl().get(defPosPair.pos - 1);
+				}
+			}
 			return null;
 		}
-		
+
 		@Override
 		public boolean isCellEditable(int rowIndex, int columnIndex)
 		{
-			// Only rows displaying a definition are editable. 
-			return (columnIndex == 0 && candidateDefinitionList.get(rowIndex).pos == 0);
+			// Only rows displaying a definition are editable.
+			return (columnIndex == 0 && candidateDefinitionList.get(rowIndex).isDef());
 		}
-		
+
 		@Override
 		public String getColumnName(int columnIndex)
 		{
 			return "";
 		}
-		
+
 		@Override
 		public Class<?> getColumnClass(int columnIndex)
 		{
@@ -365,44 +466,59 @@ public class DefinitionsPopup extends JDialog
 			}
 
 		}
-		
+
 		@Override
 		public void setValueAt(Object aValue, int rowIndex, int columnIndex)
 		{
-			CandidateDefinition def = candidateDefinitionList.get(rowIndex).def;
-			if (((Boolean) aValue) == true) {
-				def.setTicked(true);
-			}
-			else {
-				def.setTicked(false);
-			}
-		}
+			if (columnIndex == 0) {
+				CandidateDefinition def = candidateDefinitionList.get(rowIndex).def;
+				if (((Boolean) aValue) == true) {
+					def.setTicked(true);
+				}
+				else {
+					def.setTicked(false);
+					super.setValueAt(aValue, rowIndex, columnIndex);
+				}
 
+			}
+			if ((columnIndex == 1) && (aValue != null) && (aValue instanceof CandidateDefinition)) {
+				candidateDefinitionList.set(rowIndex, new DefPosPair((CandidateDefinition) aValue, 0));
+			}
+
+		}
 
 		public DefPosPair getDefinitionForRow(int rowIndex)
 		{
 			return candidateDefinitionList.get(rowIndex);
 		}
 	}
-	
+
 	/**
-	 * DefPosPair describes a pair of a definition and the position of the needed (cached-)URL in the 
-	 * definition (cached-)URL list. This is needed for finding the appropriate cachedURL for the 
-	 * displayed URL in the DefinitionPopupTable.
+	 * DefPosPair describes a pair of a definition and the position of the needed (cached-)URL in the definition
+	 * (cached-)URL list. This is needed for finding the appropriate cachedURL for the displayed URL in the
+	 * DefinitionPopupTable.
 	 * 
 	 * @author Goetz Fabian
-	 *
 	 */
 	private class DefPosPair
 	{
 		public int pos;
 		public CandidateDefinition def;
-		
+
 		public DefPosPair(CandidateDefinition def, int position)
 		{
 			this.def = def;
 			this.pos = position;
 		}
+
+		public boolean isDef()
+		{
+			return (pos == 0);
+		}
+
+		public boolean isURL()
+		{
+			return (pos > 0);
+		}
 	}
 }
-
