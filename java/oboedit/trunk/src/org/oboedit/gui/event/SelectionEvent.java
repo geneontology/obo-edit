11711d01dfc0ad346432cb76f874c997c03bc382
jenclark <jenclark@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-06-03 09:39:03 +0000

    Altered so that Graphviz component does not reload UI every time a new term is selected in a bulk dbxref addition. The change involves making a second setGlobalSelection method that has an additional parameter to indicate whether UI reload is required.

--- java/oboedit/trunk/src/org/oboedit/gui/event/SelectionEvent.java
+++ java/oboedit/trunk/src/org/oboedit/gui/event/SelectionEvent.java
@@ -11,7 +11,14 @@ public class SelectionEvent extends EventObject {
 	//initialize logger
 	protected final static Logger logger = Logger.getLogger(SelectionEvent.class);
 	protected Selection selection;
+	protected String option;
 	
+	
+	
+	public String getOption() {
+		return option;
+	}
+
 	public SelectionEvent(Object o, Selection selection) {
 		super(o);
 		this.selection = selection;
@@ -21,6 +28,20 @@ public class SelectionEvent extends EventObject {
 		this(selection.getComponent(), selection);
 	}
 	
+	/**
+	 * Enables the passing of a selection event, but with an instruction not to reload the UI of other listening components.
+	 *  
+	 * @param source
+	 * @param current
+	 * @param option determines whether the UI of listening components will be reloaded. 
+	 */
+	public SelectionEvent(Object source, Selection current, String option) {
+		super(source);
+		this.selection = current;
+		this.option = option;
+		
+	}
+
 	public Selection getSelection() {
 		return selection;
 	}
