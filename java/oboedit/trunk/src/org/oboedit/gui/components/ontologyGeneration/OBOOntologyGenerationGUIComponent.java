a82f32a5f8a6369602c297d3e234b27f7f2c357f
thomaswaechter <thomaswaechter@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-06-25 11:20:44 +0000

    New version of the ontology generation tool with term generation from  PubMed, Web, PDF or plain text. Definition generation from web pages and prediction of suitable parent terms for selected ontology terms.

--- java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/OBOOntologyGenerationGUIComponent.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/OBOOntologyGenerationGUIComponent.java
@@ -4,13 +4,24 @@ import java.awt.Cursor;
 
 import org.bbop.framework.AbstractGUIComponent;
 
-public class OBOOntologyGenerationGUIComponent extends AbstractGUIComponent {
+/**
+ * Ontology Generation Plugin for OBO-Edit 2 which supports the automatic generation of candidate terms from text
+ * retrieved from PubMed, Web, PDF or Text sources. It includes definition creation support for terms.
+ * <p>
+ * Developed at the Bioinformatics Group, BIOTEC, TU Dresden, Dresden, Germany
+ * </p>
+ * 
+ * @author Thomas Waechter (<href>waechter@biotec.tu-dresden.de</href>), 2009
+ */
+public class OBOOntologyGenerationGUIComponent extends AbstractGUIComponent
+{
 
 	private static final long serialVersionUID = 6424353594539158432L;
 	final OntologyGenerationComponent innerComponent;
 	final OntologyModelAdapterInterface adapter;
 
-	public OBOOntologyGenerationGUIComponent(String id) {
+	public OBOOntologyGenerationGUIComponent(String id)
+	{
 		super(id);
 		adapter = OBOOntologyModelAdapter.getInstance();
 		innerComponent = new OntologyGenerationComponent(adapter, this);
@@ -19,7 +30,8 @@ public class OBOOntologyGenerationGUIComponent extends AbstractGUIComponent {
 	}
 
 	@Override
-	public void init() {
+	public void init()
+	{
 		add(innerComponent.buildGUI());
 		innerComponent.initListener();
 		validate();
@@ -29,20 +41,24 @@ public class OBOOntologyGenerationGUIComponent extends AbstractGUIComponent {
 	}
 
 	@Override
-	public void cleanup() {
+	public void cleanup()
+	{
 		adapter.removeListeners();
 	}
 
 	@Override
-	public void setCursor(Cursor cursor) {
+	public void setCursor(Cursor cursor)
+	{
 		super.setCursor(cursor);
 	}
 
-	public void setUpdateUI() {
+	public void setUpdateUI()
+	{
 		super.updateUI();
 	}
 
-	public void updateEditDefArea(String definition) {
+	public void updateEditDefArea(String definition)
+	{
 		innerComponent.updateEditDefArea(definition);
 	}
 }
