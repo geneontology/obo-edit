a46f187e3f8e9b3955c58a5ad3b1e252a7b74946
Chris Mungall <cjm@berkeleybop.org>
2009-09-11 18:11:06 +0000

    json/ouch

---
+++ go-moose/GOBO/Writers/JSONWriter.pm
@@ -0,0 +1,16 @@
+package GOBO::Writers::JSONWriter;
+use Moose;
+use strict;
+extends 'GOBO::Writers::PerlHashWriter';
+use GOBO::Node;
+use GOBO::LinkStatement;
+use JSON::PP;
+
+sub write {
+    my $self = shift;
+    $self->SUPER::write();
+    my $json = encode_json $self->hashref;
+    print $json;
+}
+
+1;
