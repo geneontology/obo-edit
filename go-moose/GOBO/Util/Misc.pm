85eb0d2fa0c8ab12cf648c6838a3ce987a18a6db
girlwithglasses <girlwithglasses@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-11-07 00:07:46 +0000

    adding utilities

--- go-moose/GOBO/Util/Misc.pm
+++ go-moose/GOBO/Util/Misc.pm
@@ -2,7 +2,7 @@ package GOBO::Util::Misc;
 
 use Moose;
 use Data::Dumper;
-
+use Class::MOP;
 
 
 =head2 read_term_file
@@ -66,5 +66,60 @@ sub read_term_file {
 	return $sub_h;
 
 }
+
+
+=head2 compare_objects
+
+Simplistic comparison of objects
+
+input:  obj 1, obj 2
+output: 1 if they are the same; 0 if they are not
+
+=cut
+
+sub compare_objects {
+	my ($obj_1, $obj_2) = (@_);
+	return 0 unless ref $obj_1 eq ref $obj_2;
+
+	# if they both have IDs, do a quick comparison...
+	if ($obj_1->isa('GOBO::Base') && $obj_1->can('id') && defined $obj_1->id && defined $obj_2->id)
+	{	if ($obj_1->id eq $obj_2->id)
+		{	return 1;
+		}
+		else
+		{	return 0;
+		}
+	}
+
+	## get the object attributes and compare them
+	my $meta = Class::MOP::Class->initialize( ref($obj_1) );
+	foreach my $a ($meta->get_all_attributes) {
+	#	print STDERR "$a = " . $a->name . "; accessor = " . $a->accessor . "\n";
+		my $acc = $a->accessor;
+		my $obj_1_val = $obj_1->$acc;
+		my $obj_2_val = $obj_2->$acc;
+
+		## go on to the next characteristic if neither has this one
+		next if ! defined $obj_1_val && ! defined $obj_2_val;
+
+		## fail if obj_1 has the attribute but obj_2 doesn't, or vice versa
+		return 0 if ( (defined $obj_1_val && ! defined $obj_2_val) || (! defined $obj_1_val && defined $obj_2_val) );
+
+		## ok, both have this attribute
+		## if both are scalars and they're not identical, fail
+		if (! ref $obj_1_val && ! ref $obj_2_val)
+		{	return 0 if $obj_1_val ne $obj_2_val;
+			next;
+		}
+
+		if ($obj_1_val->isa('GOBO::Base') && $obj_1_val->can('id') && defined $obj_1_val->id && defined $obj_2_val->id)
+		{	return 0 if $obj_1_val->id ne $obj_2_val->id;
+		}
+		## more tests...?
+	}
+	## maybe they are the same...!
+	return 1;
+}
+
 
 1;
