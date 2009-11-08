101ba981e11ceec099d365d6be32b828cfc087a9
thomaswaechter <thomaswaechter@6f0e8829-b336-0410-acfb-cb9b228023ad>
2009-11-08 14:58:38 +0000

    Ontology Generation Tool, allow selection of relation types when adding a term

--- java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/oboAdapter/OBOTermsTableModel.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/oboAdapter/OBOTermsTableModel.java
@@ -4,18 +4,18 @@ import org.obo.datamodel.LinkedObject;
 import org.obo.datamodel.OBOProperty;
 import org.oboedit.gui.components.ontologyGeneration.interfaces.AbstractOntologyTermsTableModel;
 
-public class OBOTermsTableModel extends AbstractOntologyTermsTableModel<LinkedObject>
+public class OBOTermsTableModel extends AbstractOntologyTermsTableModel<LinkedObject, OBOProperty>
 {
 	private static final long serialVersionUID = 2327784875383421301L;
 
 	@Override
-	public Object[] getDefaultRelationTypes()
+	public OBOProperty[] getDefaultRelationTypes()
 	{
 		return OBOProperty.BUILTIN_TYPES;
 	}
 
 	@Override
-	public Object getPreselectedRelationType()
+	public OBOProperty getDefaultRelationType()
 	{
 		return OBOProperty.IS_A;
 	}
