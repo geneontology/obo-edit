4f194626881c7552b863af3ca7775ae89273fe55
sjcarbon <kltm@users.noreply.github.com>
2009-12-09 01:20:34 +0000

    recent additions from AmiGO::Model; TODO: probably some that were missed, but since the transition is done, I'll pick them up as I catch them

---
+++ gobo-dbic/GOBO/DBIC/GODBModel/Schema/TermSubset.pm
@@ -0,0 +1,47 @@
+=head1 GOBO::DBIC::GODBModel::Schema::TermSubset
+
+
+=cut
+
+
+package GOBO::DBIC::GODBModel::Schema::TermSubset;
+
+use base qw/DBIx::Class/;
+
+##
+__PACKAGE__->load_components(qw/ PK::Auto Core /);
+
+__PACKAGE__->table('term_subset');
+
+__PACKAGE__->add_columns(
+			 term_id =>
+			 {
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => undef,
+			 },
+			 subset_id =>
+			 {
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => undef,
+			 },
+			);
+
+##
+#__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to('term' =>
+			'GOBO::DBIC::GODBModel::Schema::Term', 'term_id');
+__PACKAGE__->belongs_to('subset' =>
+			'GOBO::DBIC::GODBModel::Schema::Term', 'subset_id');
+
+##
+
+
+
+1;
