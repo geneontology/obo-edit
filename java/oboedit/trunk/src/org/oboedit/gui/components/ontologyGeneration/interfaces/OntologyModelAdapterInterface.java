dc136176fe9b956ca5db6d9a2e895d91d63a50e9
thomaswaechter <thomaswaechter@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-01-20 10:54:06 +0000

    Ontology Generation Tool:  Bugfix synonym handling for filtering ontology terms and the prediction of subclass relationships; Bugfix: visible terms in OntologyTermsTable; Update SplashScreen;

--- java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/interfaces/OntologyModelAdapterInterface.java
+++ java/oboedit/trunk/src/org/oboedit/gui/components/ontologyGeneration/interfaces/OntologyModelAdapterInterface.java
@@ -4,16 +4,17 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+import org.obo.datamodel.LinkedObject;
 import org.oboedit.gui.components.ontologyGeneration.CandidateTerm;
 import org.oboedit.gui.components.ontologyGeneration.oboAdapter.ParentRelationEntry;
 
-public interface OntologyModelAdapterInterface<T,R>
+public interface OntologyModelAdapterInterface<T, R>
 {
 
 	/*
 	 * Needed interface methods
 	 */
-	
+
 	public abstract void refillOBOTermsTableWithExistingTerms();
 
 	public abstract String findTermId(CandidateTerm candidateTerm);
@@ -24,24 +25,27 @@ public interface OntologyModelAdapterInterface<T,R>
 
 	public abstract String getLabelForExistingTerm(CandidateTerm candidateTerm);
 
+	public abstract List<String> getSynonymsForOntologyTerm(LinkedObject linkedObject);
+
 	public abstract String getDefinitionForExistingTerm(CandidateTerm candidateTerm);
 
 	public abstract Map<String, String> getParentsForExistingTerm(CandidateTerm candidateTerm);
-	
+
 	public abstract void selectOntologyTerm(String id);
 
 	public abstract void commitLabel(CandidateTerm candidateTerm);
 
 	public abstract void commitDefinition(CandidateTerm candidateTerm);
 
-	public abstract void commitAddToOntologyAsChildOfLinkedObject(CandidateTerm selectedCandidateTerm,
-			Collection<ParentRelationEntry<R>> parentRelations, boolean includeChildren, boolean includeBranch);
+	public abstract void commitAddToOntologyAsChildOfLinkedObject(CandidateTerm selectedCandidateTerm, Collection<ParentRelationEntry<R>> parentRelations, boolean includeChildren,
+	    boolean includeBranch);
 
 	/*
 	 * Wrapper organizational methods
 	 */
-	
-	public abstract void setService(OntologyGenerationComponentServiceInterface<T,R> service);
-	
+
+	public abstract void setService(OntologyGenerationComponentServiceInterface<T, R> service);
+
 	public abstract void cleanup();
+
 }
