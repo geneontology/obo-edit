bac0ade8e9ccf8d250641ff4788019e024ac273c
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-02-08 04:28:25 +0000

    switching things over to use populated graph_path; adding some phylotree-related tables to model

---
+++ gobo-dbic/GOBO/DBIC/GODBModel/Schema/GeneProductPhylotree.pm
@@ -0,0 +1,57 @@
+=head1 GOBO::DBIC::GODBModel::Schema::GeneProductPhylotree
+
+
+=cut
+
+use utf8;
+use strict;
+
+package GOBO::DBIC::GODBModel::Schema::GeneProductPhylotree;
+
+##
+use base qw/DBIx::Class/;
+
+##
+__PACKAGE__->load_components(qw/ PK::Auto Core /);
+
+__PACKAGE__->table('gene_product_phylotree');
+
+__PACKAGE__->add_columns(
+			 id =>
+			 {
+			  accessor  => 'id', #overrides default of 'id' (irony)
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 1,
+			  default_value => undef,
+			 },
+			 gene_product_id =>
+			 {
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => '',
+			 },
+			 phylotree_id =>
+			 {
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => '',
+			 },
+			);
+
+##
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to('gene_product' =>
+                        'GOBO::DBIC::GODBModel::Schema::GeneProduct', 'gene_product_id');
+__PACKAGE__->belongs_to('phylotree' =>
+                        'GOBO::DBIC::GODBModel::Schema::Phylotree', 'phylotree_id');
+
+
+
+1;
---
+++ gobo-dbic/GOBO/DBIC/GODBModel/Schema/Phylotree.pm
@@ -0,0 +1,65 @@
+=head1 GOBO::DBIC::GODBModel::Schema::Phylotree
+
+
+=cut
+
+use utf8;
+use strict;
+
+package GOBO::DBIC::GODBModel::Schema::Phylotree;
+
+##
+use base qw/DBIx::Class/;
+
+##
+__PACKAGE__->load_components(qw/ PK::Auto Core
+				 +GOBO::DBIC::GODBModel::Extension /);
+
+__PACKAGE__->table('phylotree');
+
+__PACKAGE__->add_columns(
+			 id =>
+			 {
+			  accessor  => 'id', #overrides default of 'id' (irony)
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 1,
+			  default_value => undef,
+			 },
+			 name =>
+			 {
+			  data_type => 'varchar',
+			  size      => 255,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => '',
+			 },
+			 dbxref_id =>
+			 {
+			  data_type => 'integer',
+			  size      => 11,
+			  is_nullable => 0,
+			  is_auto_increment => 0,
+			  default_value => '',
+			 },
+			);
+
+##
+__PACKAGE__->set_primary_key('id');
+
+##
+__PACKAGE__->might_have('gene_product_phylotree' =>
+			'GOBO::DBIC::GODBModel::Schema::GeneProductPhylotree',
+			'gene_product_id');
+
+##
+__PACKAGE__->belongs_to('dbxref' =>
+			'GOBO::DBIC::GODBModel::Schema::DBXRef', 'dbxref_id');
+
+##
+__PACKAGE__->add_unique_constraint("i0", ["id"]);
+
+
+
+1;
