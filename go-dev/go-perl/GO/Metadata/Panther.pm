c7605a17f38ee46b58d627d16fdfb78276705a44
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-05-14 17:59:05 +0000

    added, likely deprecated ecoli id

--- go-dev/go-perl/GO/Metadata/Panther.pm
+++ go-dev/go-perl/GO/Metadata/Panther.pm
@@ -163,7 +163,8 @@ our @species =
 
    { #  Escherichia coli str. K-12 substr. MG1655
     key => 'ECOLI',
-    ncbi_taxa_id => [ 83333 ],
+    # I think the 511145 is more for reverse compatibility
+    ncbi_taxa_id => [ 83333, 511145 ],
    },
 
    #
