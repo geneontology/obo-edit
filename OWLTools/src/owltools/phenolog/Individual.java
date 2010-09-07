4d3e9a8ee31eaa6d0a6ffcbaf49a3e56f1c56fa9
beladia <beladia@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-09-07 03:10:23 +0000

    Commented code for owlObject(future work) Added Constructor

--- OWLTools/src/owltools/phenolog/Individual.java
+++ OWLTools/src/owltools/phenolog/Individual.java
@@ -3,18 +3,24 @@ package owltools.phenolog;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.semanticweb.owlapi.model.OWLObject;
+//import org.semanticweb.owlapi.model.OWLObject;
 
 /**
- * represents an attribute-bearing entity; for example, a gene. 
+ * represents an attribute-bearing entity; for example, a gene.
  */
 public class Individual {
-	
+
 	private String id;
 	private String label;
-	private OWLObject owlObject; // for future use
+	//private OWLObject owlObject; // for future use
 	private Set<Attribute> attributes = new HashSet<Attribute>();
-	
+
+        public Individual(String id, String label, Set<Attribute> attributes){
+            this.id = id;
+            this.label = label;
+            this.attributes = attributes;
+        }
+
 	public String getId() {
 		return id;
 	}
@@ -27,13 +33,13 @@ public class Individual {
 	public void setLabel(String label) {
 		this.label = label;
 	}
-	public OWLObject getOwlObject() {
-		return owlObject;
-	}
-	public void setOwlObject(OWLObject owlObject) {
-		this.owlObject = owlObject;
-	}
-	
+    /*public OWLObject getOwlObject() {
+    return owlObject;
+    }
+    public void setOwlObject(OWLObject owlObject) {
+    this.owlObject = owlObject;
+    }*/
+
 
 	public Set<Attribute> getAttributes() {
 		return attributes;
@@ -42,7 +48,4 @@ public class Individual {
 	public void setAttributes(Set<Attribute> attributes) {
 		this.attributes = attributes;
 	}
-	
-	
-
 }
