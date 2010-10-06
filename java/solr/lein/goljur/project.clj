3dbaf68cbab3553ea438883e9c7b8345098623aa
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-10-06 01:20:16 +0000

    rude testing for go solr; basic http testing probes; raised server limitiations to help testing; need to look at filtering to get speed up--see note in core

---
+++ java/solr/lein/goljur/project.clj
@@ -0,0 +1,11 @@
+(defproject goljur "0.0.2-SNAPSHOT"
+  :description "Interaction and test suite for GO Solr."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 ;;[org.clojars.kjw/lucene-core "2.9.1"]
+		 ;;[org.clojars.kjw/lucene-query "2.9.1"]
+		 ;;[org.clojars.lancepantz/clojure-solr "0.2.0"]
+		 [clj-http "0.1.1"]
+		 [org.danlarkin/clojure-json "1.1"]
+		 [org.clojars.kjw/mysql-connector "5.1.11"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
