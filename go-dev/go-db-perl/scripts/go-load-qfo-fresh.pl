4f0d5f15bbe206e94e5bc9fe4427bd93a4dfb710
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-10-14 18:57:07 +0000

    added --fetch option

--- go-dev/go-db-perl/scripts/go-load-qfo-fresh.pl
+++ go-dev/go-db-perl/scripts/go-load-qfo-fresh.pl
@@ -8,6 +8,7 @@ use Getopt::Long;
 use IO::Uncompress::AnyUncompress qw/$AnyUncompressError/;
 use List::Util qw/first/;
 use Text::Wrap;
+use Net::FTP;
 
 use Bio::SeqIO;
 
@@ -19,9 +20,17 @@ use GO::MatchID;
 # option to only load reference genome species, but that data seems to
 # be burried in GO::AmiGO::Aid someplace.
 my $panther = 0;
-my $verbose = 1;
+my $verbose_p = 1;
 my $dry_run = 1000;
 my $match_only;
+my $fetch_p;
+
+my $download_dir = "$ENV{HOME}/tmp"; # change to tempdir
+my %ftp_opt = ( Passive => 1 ); # In doubt use passive, it's what
+                                # wget(1) does
+my $ftp_host = 'ftp.ebi.ac.uk';
+my $ftp_dir = '/pub/databases/reference_proteomes/';
+
 
 my $apph = GO::AppHandle->connect(\@ARGV);
 
@@ -33,22 +42,8 @@ GetOptions
    'debug!'           => \$GO::MatchID::debug,
    'match-only!'      => \$match_only,
    'dry-run!'         => \$dry_run,
+   'fetch!'           => \$fetch_p,
   ) or die;
-
-
-
-my @directory;
-my @file;
-while (@ARGV) {
-    my $file = shift @ARGV;
-
-    if (-d $file) {
-	push @directory, $file;
-    } elsif (-f $file) {
-	push @file, $file;
-    }
-}
-
 
 my $fasta_match;
 if ($panther) {
@@ -60,15 +55,77 @@ if ($panther) {
     $fasta_match = qr/\.fasta(\.gz)?$/;
 }
 
-sub wanted{
-    if (m/$fasta_match/) {
-	push @file, $File::Find::name;
-    }
+
+sub ftp_connect{
+    my $out = okp(Net::FTP->new($ftp_host, %ftp_opt), "[FTP] connecting to $ftp_host:", $@);
+    okp($out->login('anonymous', 'anonymous'), '[FTP] loggig in:', $out->message);
+    okp($out->binary(), '[FTP] binary mode:', $out->message);
+    okp($out->cwd($ftp_dir), "[FTP] changing to directory '$ftp_dir':", $out->message);
+    return $out;
 }
 
-find(\&wanted, @directory) if (scalar @directory);
+my @file;
 
-if ($verbose) {
+if ($fetch_p) {
+    sub okp{
+	my $check = shift;
+	chomp $_[-1];
+	my $message = join(' ', @_) . "\n";
+	if (!$check) {
+	    die "Failed: $message";
+	} elsif ($verbose_p) {
+	    warn $message
+	}
+	return $check;
+    }
+
+    my $ftp = ftp_connect();
+
+    @file = map {
+	if (m/$fasta_match/) {
+	    my $file = File::Spec->catfile($download_dir, $_);
+	    my @stat = stat($file);
+
+	    if (-e $file) { # Just check if it exists
+		okp(1, '[FTP] Already have ', $file);
+	    } else {
+		warn "[FTP] fetching $file\n";
+		if (!$ftp->get($_, $file)) {
+		    unlink $file;
+		    die "Failed [FTP] Got $file:", $ftp->message;
+		}
+	    }
+
+	    $file;
+	} else {
+	    ();
+	}
+
+    } $ftp->ls();
+    $ftp->quit();
+} else {
+    my @directory;
+
+    while (@ARGV) {
+	my $file = shift @ARGV;
+
+	if (-d $file) {
+	    push @directory, $file;
+	} elsif (-f $file) {
+	    push @file, $file;
+	}
+    }
+
+    sub wanted{
+	if (m/$fasta_match/) {
+	    push @file, $File::Find::name;
+	}
+    }
+
+    find(\&wanted, @directory) if (scalar @directory);
+}
+
+if ($verbose_p) {
     if (1 == scalar @file) {
 	warn "Going to load one fasta file: $file[0]\n";
     } else {
@@ -153,7 +210,11 @@ while (@file) {
 	    $stats{$basename}->{missing}++;
 	}
 
-	next if ($match_only || $gene_product_id);
+	next if ($match_only);
+
+	##########
+	# Here is the bit that inserts new dbxref and gene_product
+	# entries.
 
 	if (!$dbxref_id) {
 
@@ -173,6 +234,11 @@ while (@file) {
 	  ($sth{insert_gene_product}, ($guesser->{tagval}->{GN} || ''),
 	   $guesser->{guessed}->{dbxref_id}, $guesser->species_id,
 	   $type_id) or die Dumper $guesser;
+
+	# replace stuff in sequence table here.  Use
+	# go-load-qfo-seqs.pl's store_seq() subroutine.
+	##########
+
     }
 
 }
