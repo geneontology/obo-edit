91fddca86244e73ab5054f7cbeeca8236a236acb
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-10-25 17:45:25 +0000

    Bulk load into postgresql is implemented

---
+++ java/gold/src/org/geneontology/gold/io/postgres/test/CvsFilelLoaderTest.java
@@ -0,0 +1,33 @@
+package org.geneontology.gold.io.postgres.test;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.geneontology.gold.io.postgres.CvsFilelLoader;
+import org.postgresql.core.BaseConnection;
+
+import junit.framework.TestCase;
+
+public class CvsFilelLoaderTest extends TestCase {
+
+	
+	public static void testLoadTabes() throws ClassNotFoundException, SQLException, IOException{
+		BaseConnection bc = (BaseConnection)getConnection();
+		
+		CvsFilelLoader loader = new CvsFilelLoader(bc);
+		
+		loader.loadTables();
+	}
+	
+	private static Connection getConnection() throws ClassNotFoundException, SQLException{
+		Class.forName("org.postgresql.Driver");
+		
+		return  DriverManager.getConnection("jdbc:postgresql://localhost/gold", "postgres", "postgres");
+		
+		
+	}
+	
+	
+}
