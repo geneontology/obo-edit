72ffb7de21cb6bfc446e862b9c5eb84140ee34ee
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-10-26 22:06:15 +0000

    Web contents are added for the admin site

--- java/gold/src/org/geneontology/web/AdminServlet.java
+++ java/gold/src/org/geneontology/web/AdminServlet.java
@@ -1,6 +1,9 @@
 package org.geneontology.web;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.Writer;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +29,28 @@ public class AdminServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		response.getWriter().write("<h1>Hello World</h1>");
+
+		Writer writer = response.getWriter();
+
+		writer.write("<h1>Hello World</h1>");
+		
+		String command = request.getParameter("command");
+
+		if(command != null){
+		
+			if("bulk-load".equals(command)){
+				writer.write("<h1>Bulk load is command is provided. This ia temporary message. This message will be replaced as soon as this service is implemented.</h1>");
+				return;
+			}
+		}
+		
+			
+			writer.write("<h1>No valid parameters are provided. Please call this page with valid parameters</h1>");
+			
+			
+		
+		
+
 	}
 
 	/**
