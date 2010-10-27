36fcbfa8cc500ed41e9b423f9dbbe7e41ccaf323
sjcarbon <kltm@users.noreply.github.com>
2010-10-27 00:59:42 +0000

    possible solution to in and out on the same child

--- go-dev/go-perl/scripts/go2owl_chunked.pl
+++ go-dev/go-perl/scripts/go2owl_chunked.pl
@@ -1,16 +1,21 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+####
+#### Whackiness with I/O: http://perldoc.perl.org/perlipc.html#Bidirectional-Communication-with-Another-Process
+####
 
 use FileHandle;
+use IPC::Open2;
 $/= "\n\n";
 
 my $n=0;
 while (<>) {
     my $fh = new FileHandle;
-    open(F,"|go2owl -p obo -|") || die;
-    print F $_;
+    #open(F,"|go2owl -p obo -|") || die;
+    my $pid = open2(*READ, *WRITE, "go2owl -p obo" ) || die;
+    print WRITE $_;
     my $skip_me = $n > 0;
     print "**N= $n ;; $skip_me\n";
-    while(<F>) {
+    while(<READ>) {
         if ($skip_me) {
             print "SKIP: $_\n";
             if (m@</owl:Ontology>@) {
@@ -25,6 +30,7 @@ while (<>) {
             print "X:$_";
         }
     }
-    close(F);
+    close(READ);
+    close(WRITE);
     $n++;
 }
