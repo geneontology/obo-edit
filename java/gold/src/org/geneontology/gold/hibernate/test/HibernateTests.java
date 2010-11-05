1b7084f7e44f7ef853e89c930c75317f04dfbbb1
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-05 02:02:07 +0000

    Improvements in the object model of the hibernate layer

--- java/gold/src/org/geneontology/gold/hibernate/test/HibernateTests.java
+++ java/gold/src/org/geneontology/gold/hibernate/test/HibernateTests.java
@@ -3,8 +3,12 @@ package org.geneontology.gold.hibernate.test;
 import java.util.List;
 
 import org.geneontology.gold.hibernate.factory.GoldObjectFactory;
+import org.geneontology.gold.hibernate.model.AllSomeRelationship;
 import org.geneontology.gold.hibernate.model.Cls;
+import org.geneontology.gold.hibernate.model.ObjAlternateLabel;
+import org.geneontology.gold.hibernate.model.Relation;
 import org.geneontology.gold.hibernate.model.SubclassOf;
+
 import junit.framework.TestCase;
 
 public class HibernateTests extends TestCase {
@@ -30,6 +34,57 @@ public class HibernateTests extends TestCase {
 		
 		List<SubclassOf> list =  f.getSubClassOfAssertions(cls);
 		
-		System.out.println(list);
+		//System.out.println(list);
+		
+		for(SubclassOf sub: list){
+			System.out.println(sub.getCls().getId() + ", " + sub.getSuperCls().getId() + ", " + sub.getOntology().getId());
+		}
 	}
+	
+	
+	public static void testGetObjAlternateLabel(){
+		GoldObjectFactory f = new GoldObjectFactory();
+
+		
+		String obj = "CARO:0000045";
+		
+		List<ObjAlternateLabel> list =  f.getObjAlternateLabel(obj);
+		
+		//System.out.println(list);
+		
+		for(ObjAlternateLabel label: list){
+			System.out.println(label.getObj() + ", " + label.getLabel());
+		}
+		
+	}
+
+	public static void testGetAllSomeRelationship(){
+		GoldObjectFactory f = new GoldObjectFactory();
+
+		
+		String cls = "CARO:0000014";
+		
+		List<AllSomeRelationship> list =  f.getAllSomeRelationship(cls);
+		
+		//System.out.println(list);
+		
+		for(AllSomeRelationship rel: list){
+			System.out.println(rel.getCls().getId() + ", " + rel.getRelation().getId() + ", " + rel.getTargetCls().getId());
+		}
+		
+	}
+
+	
+	public static void testgetRelation(){
+		GoldObjectFactory f = new GoldObjectFactory();
+		
+		String id = "TODO_part:of";
+		
+		Relation r = f.getRelation(id);
+
+		System.out.println(r.getId());
+	}
+	
+	
+	
 }
