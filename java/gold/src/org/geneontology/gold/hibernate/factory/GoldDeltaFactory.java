94bdec0ddca5bd6c4c080b91ba5e43b412e1fcac
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-06 00:02:14 +0000

    work in progress in the delta update of GOLD

---
+++ java/gold/src/org/geneontology/gold/hibernate/factory/GoldDeltaFactory.java
@@ -0,0 +1,24 @@
+package org.geneontology.gold.hibernate.factory;
+
+import java.util.List;
+
+import org.geneontology.gold.hibernate.model.Cls;
+
+/**
+ * This class builds delta from two databases of GOLD
+ * ,i.e. gold.dbname and gold.deltadb (see gold.properties file). 
+ * It returns in memory hibernate objects. The are need be commited in the
+ * gold.dbname database.
+ * @author Shahid Manzoor
+ *
+ */
+public class GoldDeltaFactory {
+
+	public List<Cls> buildClsDelta(){
+		
+		
+		
+		return null;
+	}
+	
+}
