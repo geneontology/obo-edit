2e40509d4ad126683bef4ee1a260c8bb974fe753
sjcarbon <kltm@users.noreply.github.com>
2010-11-11 19:26:43 +0000

    wrap fresh

--- go-dev/go-db-perl/scripts/go_db_install_wrapper.pl
+++ go-dev/go-db-perl/scripts/go_db_install_wrapper.pl
@@ -9,4 +9,4 @@ export GO_ROOT=/home/sjcarbon/local/src/svn/geneontology/go-dev
 export PANTHER_NEWICK_PATH=/srv/www/htdocs/amigo/panther
 export GO_DBNAME=go_latest_lite
 export GO_DBHOST=localhost
-$GO_ROOT/go-db-perl/scripts/go_db_install.pl -i -n -j -e $GO_DBNAME -v -d $GO_DBHOST; cd $GO_CVS/gene-associations/submission/paint; cvs update; find . -name "*.gaf" | xargs -I {} perl -I $GO_ROOT/go-db-perl -I $GO_ROOT/go-perl $GO_ROOT/go-db-perl/scripts/load-go-into-db.pl -datatype go_assoc -h $GO_DBHOST -d $GO_DBNAME {}
+$GO_ROOT/go-db-perl/scripts/go_db_install.pl -F -i -n -j -e $GO_DBNAME -v -d $GO_DBHOST; cd $GO_CVS/gene-associations/submission/paint; cvs update; find . -name "*.gaf" | xargs -I {} perl -I $GO_ROOT/go-db-perl -I $GO_ROOT/go-perl $GO_ROOT/go-db-perl/scripts/load-go-into-db.pl -datatype go_assoc -h $GO_DBHOST -d $GO_DBNAME {}
