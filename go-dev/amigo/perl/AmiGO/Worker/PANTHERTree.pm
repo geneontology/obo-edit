b33eb4f92196719c36c50724b6fbca34ac76a7cd
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-22 18:30:54 +0000

    only think it's a tree if property_key=newick

--- go-dev/amigo/perl/AmiGO/Worker/PANTHERTree.pm
+++ go-dev/amigo/perl/AmiGO/Worker/PANTHERTree.pm
@@ -59,11 +59,10 @@ sub get_tree {
 
     $ret = [];
     foreach my $ptree (@$query_results){
-      my $props = $ptree->phylotree_property;
+      my $props = $ptree->phylotree_property({'property_key' => 'newick'});
       if( defined $props ){
 	foreach my $prop ($props->all){
 	  my $name = $prop->property_key;
-	  next if ($name eq 'missing');
 	  my $tree = $prop->property_val;
 	  # $self->kvetch("name: " . $name);
 	  # $self->kvetch("tree: " . $tree);
