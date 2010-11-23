c809f898e27ec4f9314c1bee7bbaadd906794165
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-23 18:10:12 +0000

    display concurrent annotion date

--- go-dev/amigo/perl/AmiGO/WebApp/Phylotree.pm
+++ go-dev/amigo/perl/AmiGO/WebApp/Phylotree.pm
@@ -188,6 +188,9 @@ sub mode_cluster{
     $c->set_template_parameter(cluster => $o);
     $c->set_template_parameter(gene_products => [ $o->gene_products() ]);
     $c->set_template_parameter(page_title => $pid);
+    $c->set_template_parameter(concurrent => join(', ', map {
+	$_->property_val;
+    } $o->properties('concurrent')));
     $c->set_template_parameter(missing => [ sort {
 	$a->property_val cmp $b->property_val;
     } $o->properties('missing') ]);
