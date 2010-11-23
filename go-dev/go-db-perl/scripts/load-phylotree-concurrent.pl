256a44d565e84f948074d68c71bf73a787e2adcb
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-23 20:14:01 +0000

    if no command line file given it will try to fetch the file from geneontology's ftp site

--- go-dev/go-db-perl/scripts/load-phylotree-concurrent.pl
+++ go-dev/go-db-perl/scripts/load-phylotree-concurrent.pl
@@ -2,13 +2,31 @@
 use warnings;
 use strict;
 use IO::Uncompress::AnyUncompress qw/$AnyUncompressError/;
+use Net::FTP;
+use File::Temp qw/tempfile/;
 
 use GO::AppHandle;
 
 my $apph = GO::AppHandle->connect(\@ARGV);
 my $dbh = $apph->dbh();
 
-if (1 != scalar(@ARGV)) {
+my $file;
+if (1 == scalar(@ARGV)) {
+    $file = shift @ARGV;
+} elsif (0 == scalar(@ARGV)) {
+    (undef, $file) = tempfile(undef, UNLINK=>1);
+    my $host = 'ftp.geneontology.org';
+    my $path = '/go/doc/paint';
+    my $get  = 'panther-target-lits-all.csv';
+    my $user = 'anonymous';
+    my $pass = '';
+    my $ftp  = Net::FTP->new($host, Passive => 1) or die $@;
+    $ftp->login($user, $pass) or die $ftp->message;
+    #$ftp->binary()            or die $ftp->message;
+    $ftp->cwd($path)          or die $ftp->message;
+    $ftp->get($get, $file)    or die $ftp->message;
+    $ftp->close()
+} else {
     die;
 }
 
@@ -19,8 +37,8 @@ my $property_key = 'concurrent';
 my $xref_key_head = '"family"';
 my $property_val_head = '';
 
-my $csv =  IO::Uncompress::AnyUncompress->new($ARGV[0])
-  or die "AnyUncompress failed opening '$ARGV[0]': $AnyUncompressError\n";
+my $csv =  IO::Uncompress::AnyUncompress->new($file)
+  or die "AnyUncompress failed opening 'file': $AnyUncompressError\n";
 my @head = split(m/$split/, readline($csv));
 chomp $head[-1];
 
@@ -55,3 +73,4 @@ while (<$csv>) {
     $insert_phylotree_property->execute
       ($phylotree_id, $property_key,  $line{$property_val_head});
 }
+$csv->close();
