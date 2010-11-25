28681c2d726c36bb54f4784f280e5d98abad6e60
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-11-25 00:01:49 +0000

    more tables are added in the bulk load

--- java/gold/src/org/geneontology/gold/io/OntologyBulkLoader.java
+++ java/gold/src/org/geneontology/gold/io/OntologyBulkLoader.java
@@ -7,12 +7,11 @@ import java.util.Set;
 import java.util.Vector;
 import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDisjointClassesAxiom;
-import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLDisjointObjectPropertiesAxiom;
+import org.semanticweb.owlapi.model.OWLEquivalentObjectPropertiesAxiom;
 import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLNamedObject;
 import org.semanticweb.owlapi.model.OWLNaryBooleanClassExpression;
@@ -23,11 +22,11 @@ import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom;
 import org.semanticweb.owlapi.model.OWLObjectUnionOf;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-
+import org.semanticweb.owlapi.model.OWLSubObjectPropertyOfAxiom;
+import org.semanticweb.owlapi.model.OWLSubPropertyChainOfAxiom;
 import owltools.graph.OWLGraphEdge;
 import owltools.graph.OWLGraphWrapper;
 import owltools.graph.OWLQuantifiedProperty;
-import owltools.graph.OWLQuantifiedProperty.Quantifier;
 
 /**
  * Loads ontology into Gold database.
@@ -41,12 +40,56 @@ import owltools.graph.OWLQuantifiedProperty.Quantifier;
  *
  */
 public class OntologyBulkLoader extends AbstractBulkLoader{
+
+	
+	private void initTables() throws IOException{
+		tables.put("ontology",  new TableDumper(this.dumpFilePrefix + "ontology", this.path));
+		tables.put("ontology_annotation",  new TableDumper(this.dumpFilePrefix + "ontology_annotation", this.path));
+		
+		tables.put("obj_alternate_label",  new TableDumper(this.dumpFilePrefix + "obj_alternate_label", this.path));
+
+		
+		tables.put("obj_definition_xref",   new TableDumper(this.dumpFilePrefix + "obj_definition_xref", this.path));
+
+		tables.put("obj_xref",   new TableDumper(this.dumpFilePrefix + "obj_xref", this.path));
+
+		
+		tables.put("cls_intersection_of",   new TableDumper(this.dumpFilePrefix + "cls_intersection_of", this.path));
+
+		tables.put("cls_union_of",   new TableDumper(this.dumpFilePrefix + "cls_union_of", this.path));
+		
+		tables.put("equivalent_to",   new TableDumper(this.dumpFilePrefix + "equivalent_to", this.path));
+		
+		tables.put("disjoint_with",   new TableDumper(this.dumpFilePrefix + "disjoint_with", this.path));
+		
+
+		tables.put("inferred_relationship",   new TableDumper(this.dumpFilePrefix + "inferred_relationship", this.path));
+		
+
+		tables.put("annotation_assertion",   new TableDumper(this.dumpFilePrefix + "annotation_assertion", this.path));
+		
+		tables.put("obj_alternate_id",   new TableDumper(this.dumpFilePrefix + "obj_alternate_id", this.path));
+		
+		tables.put("obj_subset",   new TableDumper(this.dumpFilePrefix + "obj_subset", this.path));
+		
+		tables.put("subrelation_of",   new TableDumper(this.dumpFilePrefix + "subrelation_of", this.path));
+		
+		tables.put("relation_disjoint_with",   new TableDumper(this.dumpFilePrefix + "relation_disjoint_with", this.path));
+		
+		tables.put("relation_equivalent_to",   new TableDumper(this.dumpFilePrefix + "relation_equivalent_to", this.path));
+		
+		tables.put("cls",   new TableDumper(this.dumpFilePrefix + "cls", this.path));
+		
+		tables.put("relation",   new TableDumper(this.dumpFilePrefix + "relation", this.path));
+	
+		tables.put("annotation_property",   new TableDumper(this.dumpFilePrefix + "annotation_property", this.path));
+		
+		
+	}
 	
 	
 	public OntologyBulkLoader(OWLGraphWrapper wrapper, String path, String dumpFilePrefix) {
 		super(wrapper, path, dumpFilePrefix);
-		
-		
 	}
 
 	public OntologyBulkLoader(OWLGraphWrapper wrapper) {
@@ -57,20 +100,24 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 	 * 
 	 */
 	public List<String> dumpBulkLoadTables() throws IOException{
+
+		initTables();
+		
 		List<String> list = new Vector<String>();
+		list.addAll(tables.keySet());
 		
-		list.addAll(dumpOntologyTable());
-		list.addAll(dumpDeclarationsAndMetadata());
-		list.addAll(dumpLogicalAxioms());
 		
+		dumpOntologyTable();
+		dumpTermFrameAndMetaData();
+		dumpTypeDefFrameAndMetaData();
+		dumpLogicalAxioms();
+		flushTables();
 		return list;
 	}
 	
-	public List<String> dumpOntologyTable() throws IOException{
-		List<String> arrayList = new ArrayList<String>();
-		
-		TableDumper ontologyDumper = new TableDumper(this.dumpFilePrefix + "ontology", this.path);
-		TableDumper ontology_annotationDumper = new TableDumper(this.dumpFilePrefix + "ontology_annotation", this.path);
+	public void dumpOntologyTable() throws IOException{
+		TableDumper ontologyDumper = tables.get("ontology");
+		TableDumper ontology_annotationDumper = tables.get("ontology_annotation");
 	
 		String id = graphWrapper.getOntologyId();
 		Set<OWLAnnotationAssertionAxiom> anns = graphWrapper.getOntology().getAnnotationAssertionAxioms(graphWrapper.getOntology().getOntologyID().getOntologyIRI());
@@ -97,43 +144,211 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 		String versionString = null;
 		if(version != null)
 			versionString = version.toString();
+		
+		
 		ontologyDumper.dumpRow(id, id,  versionString,
 				dt);
-
 		
-		ontologyDumper.close();
-		ontology_annotationDumper.close();
-		
-		
-		arrayList.add(ontologyDumper.getTable());
-		arrayList.add(ontology_annotationDumper.getTable());
-		
-		return arrayList;
 	}
 
-	public List<String> dumpDeclarationsAndMetadata() throws IOException {
-		TableDumper clsDumper = new TableDumper(this.dumpFilePrefix + "cls", this.path);
-		
-		
-		TableDumper obj_alternate_labelDumper = new TableDumper(this.dumpFilePrefix + "obj_alternate_label", this.path);
+	private void dumpAnnotations(OWLObject obj,String objId, String ontology)
+	throws IOException{
 
 		
-		TableDumper obj_definition_xref = new TableDumper(this.dumpFilePrefix + "obj_definition_xref", this.path);
+		
+		boolean builtin = graphWrapper.getBuiltin(obj);
+		if(builtin){
+			dumpAnnotation("builtin",  objId, builtin + "", ontology);
+		}
 
-		TableDumper obj_xref = new TableDumper(this.dumpFilePrefix + "obj_xref", this.path);
+		String createdBy = graphWrapper.getCreatedBy(obj);
+		if(createdBy != null)
+			dumpAnnotation("created_by",  objId, createdBy, ontology);
+		
+		
+		boolean isObselete = graphWrapper.getIsObsolete(obj);
+		if(isObselete)
+			dumpAnnotation("is-obsolete",  objId, isObselete + "", ontology);
 
-		
-		TableDumper cls_intersection_ofDumper = new TableDumper(this.dumpFilePrefix + "cls_intersection_of", this.path);
+			
+		String replacedBy = graphWrapper.getReplacedBy(obj);
+		if(replacedBy != null)
+			dumpAnnotation("replaced_by",  objId, replacedBy, ontology);
+			
 
-		TableDumper cls_union_ofDumper = new TableDumper(this.dumpFilePrefix + "cls_union_of", this.path);
-		
-		TableDumper equivalent_toDumper = new TableDumper(this.dumpFilePrefix + "equivalent_to", this.path);
-		
-		TableDumper disjoint_withDumper = new TableDumper(this.dumpFilePrefix + "disjoint_with", this.path);
+		String consider = graphWrapper.getConsider(obj);
+		if(consider != null)
+			dumpAnnotation("consider",  objId, consider, ontology);
 		
 
-		TableDumper inferred_relationshipDumper = new TableDumper(this.dumpFilePrefix + "inferred_relationship", this.path);
+		boolean isAnonymous = graphWrapper.getIsAnonymous(obj);
+		if(isAnonymous)
+			dumpAnnotation("is_anonymous",  objId, isAnonymous + "", ontology);
 		
+		if(obj instanceof OWLObjectProperty){
+			OWLObjectProperty prop = (OWLObjectProperty)obj;
+		
+			String domain = graphWrapper.getDomain(prop);
+			if(domain != null)
+				dumpAnnotation("domain",  objId, domain, ontology);
+	
+			String range = graphWrapper.getRange(prop);
+			if(range != null)
+				dumpAnnotation("range",  objId, range, ontology);
+			
+
+			boolean isAntiSymmetric = graphWrapper.getIsAntiSymmetric(obj);
+			if(isAntiSymmetric)
+				dumpAnnotation("is_anti_symmetric",  objId, isAntiSymmetric + "", ontology);
+				
+			boolean isCyclic = graphWrapper.getIsCyclic(obj);
+			if(isCyclic)
+				dumpAnnotation("is_cyclic",  objId, isCyclic + "", ontology);
+
+			boolean isFunctional = graphWrapper.getIsFunctional(prop);
+			if(isFunctional)
+				dumpAnnotation("is_functional",  objId, isFunctional + "", ontology);
+
+			boolean isInverseFunctional = graphWrapper.getIsInverseFunctional(prop);
+			if(isInverseFunctional)
+				dumpAnnotation("is_inverse_functional",  objId, isInverseFunctional + "", ontology);
+			
+			
+		}
+		
+	}
+	
+	private void dumpAlternateId(OWLObject obj, String id) 
+			throws IOException{
+		
+		TableDumper table = tables.get("obj_alternate_id");
+		String alt_id = graphWrapper.getAltId(obj);
+		if(alt_id != null){
+			table.dumpRow(id,alt_id);
+		}
+		
+	}
+	
+	
+	private void dumpAnnotation(String tag,String objId, String target_obj,  String ontology)
+		throws IOException{
+		String prop = graphWrapper.getIdentifier(  
+			graphWrapper.getAnnotationProperty(tag)
+			);
+
+		TableDumper table = tables.get("annotation_assertion");
+		table.dumpRow(prop, objId, target_obj, ontology );
+	}
+
+	private void dumpObjAlternateLabel(OWLObject obj, String id)
+			throws IOException{
+		
+		TableDumper obj_alternate_labelDumper = tables.get("obj_alternate_label");
+		
+		for(String l: graphWrapper.getSynonymStrings(obj)){
+			obj_alternate_labelDumper.dumpRow(id, l, null, null, null);
+		}
+	}
+
+	
+	private void dumpObjDefinitionXref(OWLObject obj, String id)
+		throws IOException{
+		
+		TableDumper obj_definition_xref = tables.get("obj_definition_xref");
+		
+		for(String xref: graphWrapper.getDefXref(obj)){
+			obj_definition_xref.dumpRow(id, xref);
+		}
+	
+		
+	}
+	
+	
+	private void dumpObjXref(OWLObject obj, String id)
+		throws IOException{
+		
+		TableDumper obj_xref = tables.get("obj_xref");
+		
+		for(String xref: graphWrapper.getXref(obj)){
+			obj_xref.dumpRow(id, xref, null);
+		}
+		
+		
+	}
+	
+	
+	private void dumpObjSubset(OWLObject obj, String id) throws IOException{
+		String subset = graphWrapper.getSubset(obj);
+		
+		if(subset != null){
+			TableDumper obj_subsetDumper = tables.get("obj_subset");
+			obj_subsetDumper.dumpRow(id, subset);
+		}
+		
+		
+	}
+	
+	private void dumpTypeDefFrameAndMetaData() throws IOException{
+		
+		TableDumper relDumper = tables.get("relation");
+		TableDumper annotation_propertyDumper =  tables.get("annotation_property");
+		
+		String ontologyId = graphWrapper.getOntologyId();
+				
+		for (OWLObjectProperty op : getOwlOntology().getObjectPropertiesInSignature()) {
+			String label = graphWrapper.getLabel(op);
+			String def = graphWrapper.getDef(op);
+			String id = graphWrapper.getIdentifier(op);
+			String comment = graphWrapper.getComment(op);
+			String namespace = graphWrapper.getNamespace(op);
+
+			boolean isTransitive = graphWrapper.getIsTransitive(op);
+			boolean isReflexive = graphWrapper.getIsReflexive(op);
+			boolean isSymmetric = graphWrapper.getIsSymmetric(op);
+			
+			boolean isMetaTag = graphWrapper.getIsMetaTag(op);
+			
+			if(isMetaTag){
+				annotation_propertyDumper.dumpRow(id, label, ontologyId , namespace, comment, def, null);
+			}else{
+				relDumper.dumpRow(id, label, ontologyId , namespace, comment, def, isTransitive + "", 
+					isSymmetric + "", isReflexive + "", null);
+			}
+			
+			dumpObjAlternateLabel(op, id);
+			
+			dumpObjDefinitionXref(op, id);
+			
+			dumpAlternateId(op, id);
+	
+			dumpObjXref(op, id);
+			
+			dumpObjSubset(op, id);
+			
+			dumpAnnotations(op, id, ontologyId);
+			
+		}
+	
+	}
+	
+	private void dumpTermFrameAndMetaData() throws IOException {
+		TableDumper clsDumper = tables.get("cls");
+			//new TableDumper(this.dumpFilePrefix + "cls", this.path);
+		
+		
+		TableDumper cls_intersection_ofDumper = tables.get("cls_intersection_of");
+
+		TableDumper cls_union_ofDumper = tables.get("cls_union_of");
+		
+			//new TableDumper(this.dumpFilePrefix + "cls_union_of", this.path);
+		
+		TableDumper equivalent_toDumper = tables.get("equivalent_to");
+			//new TableDumper(this.dumpFilePrefix + "equivalent_to", this.path);
+		
+		TableDumper disjoint_withDumper = tables.get("disjoint_with");
+			//new TableDumper(this.dumpFilePrefix + "disjoint_with", this.path);
+		
+		TableDumper inferred_relationshipDumper = tables.get("inferred_relationship");
 		
 		//	TableDumper subclass_ofDumper = new TableDumper("subclass_of");
 		//TableDumper allSomeRelationship = new TableDumper("all_some_relationship");
@@ -149,18 +364,22 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 			clsDumper.dumpRow(id, label, ontologyId, namespace, comment, def, null);
 			
 			//dump synonms
-			for(String l: graphWrapper.getSynonymStrings(cls)){
-				obj_alternate_labelDumper.dumpRow(id, l, null, null, null);
-			}
+			dumpObjAlternateLabel(cls, id);
+
+
+			//dump alt_id tag
+			dumpAlternateId(cls, id);
+			
+
+			dumpObjDefinitionXref(cls, id);
+
+			dumpObjXref(cls, id);
 
 			
-			for(String xref: graphWrapper.getDefXref(cls)){
-				obj_definition_xref.dumpRow(id, xref);
-			}
+			dumpObjSubset(cls, id);
 			
-			for(String xref: graphWrapper.getXref(cls)){
-				obj_xref.dumpRow(id, xref, null);
-			}
+			
+			dumpAnnotations(cls, id, ontologyId);
 			
 			//dump intersection of and union_of	
 			for(OWLObject ec: cls.getEquivalentClasses(graphWrapper.getOntology())){
@@ -186,8 +405,9 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 			}
 			
 			
+			/*
+			//TODO: Inferred relationship
 			Set<OWLGraphEdge> outgoing = graphWrapper.getOutgoingEdges(cls);
-			System.out.println("Outputoing: " + outgoing); 
 			for(OWLGraphEdge edge: outgoing){
 				String targetId = graphWrapper.getIdentifier( edge.getTarget() );
 				for(OWLQuantifiedProperty prop: edge.getQuantifiedPropertyList()){
@@ -198,92 +418,20 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 			}
 			
 			
-			Set<OWLGraphEdge> reflexsive = graphWrapper.getOutgoingEdgesClosureReflexive(cls);
-			System.out.println("Reflesive: " + reflexsive); 
+			Set<OWLGraphEdge> reflexive = graphWrapper.getOutgoingEdgesClosureReflexive(cls);
 			
-			reflexsive.removeAll(outgoing);
+			reflexive.removeAll(outgoing);
 
-			for(OWLGraphEdge edge: reflexsive){
+			for(OWLGraphEdge edge: reflexive){
 				String targetId = graphWrapper.getIdentifier( edge.getTarget() );
 				for(OWLQuantifiedProperty prop: edge.getQuantifiedPropertyList()){
 					String propId = graphWrapper.getDef(prop.getProperty());
 					inferred_relationshipDumper.dumpRow(id, targetId, propId, "false" , "true", ontologyId);
 				}
-			}
+			}*/
 			
 		}
 		
-		
-		TableDumper relDumper = new TableDumper(this.dumpFilePrefix + "relation", path);
-		TableDumper annotation_propertyDumper = new TableDumper(this.dumpFilePrefix + "annotation_property", path);
-		
-		
-		for (OWLObjectProperty op : getOwlOntology().getObjectPropertiesInSignature()) {
-			String label = graphWrapper.getLabel(op);
-			String def = graphWrapper.getDef(op);
-			String id = graphWrapper.getIdentifier(op);
-			String comment = graphWrapper.getComment(op);
-			String namespace = graphWrapper.getNamespace(op);
-
-			boolean isTransitive = graphWrapper.getIsTransitive(op);
-			boolean isReflexive = graphWrapper.getIsReflexive(op);
-			boolean isSymmetric = graphWrapper.getIsSymmetric(op);
-			
-			
-			// textdef TODO
-		
-			
-			relDumper.dumpRow(id, label, ontologyId , namespace, comment, def, isTransitive + "", 
-					isSymmetric + "", isReflexive + "", null);
-			
-			for(String l: graphWrapper.getSynonymStrings(op)){
-				obj_alternate_labelDumper.dumpRow(id, l, null, null, null);
-			}
-			
-			
-			for(String xref: graphWrapper.getDefXref(op)){
-				obj_definition_xref.dumpRow(id, xref);
-			}
-			
-			for(String xref: graphWrapper.getXref(op)){
-				obj_xref.dumpRow(id, xref, null);
-			}
-			
-			
-			
-		}
-		
-		clsDumper.close();
-		relDumper.close();
-		obj_alternate_labelDumper.close();
-		obj_definition_xref.close();
-		obj_xref.close();
-		
-		equivalent_toDumper.close();
-		cls_intersection_ofDumper.close();
-		cls_union_ofDumper.close();
-		disjoint_withDumper.close();
-		
-		inferred_relationshipDumper.close();
-	
-		
-		List<String> list = new ArrayList<String>();
-		list.add(clsDumper.getTable());
-		list.add(relDumper.getTable());
-		list.add(obj_alternate_labelDumper.getTable());
-		list.add(obj_alternate_labelDumper.getTable());
-		list.add(obj_xref.getTable());
-		list.add(obj_definition_xref.getTable());
-
-		list.add(cls_intersection_ofDumper.getTable());
-		list.add(cls_union_ofDumper.getTable());
-		list.add(equivalent_toDumper.getTable());
-		list.add(disjoint_withDumper.getTable());
-		
-		list.add(inferred_relationshipDumper.getTable());
-		
-		
-		return list;
 		
 	}
 	
@@ -342,6 +490,73 @@ public class OntologyBulkLoader extends AbstractBulkLoader{
 			}
 			else {
 				// TODO
+			}
+		}
+		
+		TableDumper subrelation_ofDumper = tables.get("subrelation_of");
+		for(OWLSubObjectPropertyOfAxiom ax: getOwlOntology().getAxioms(AxiomType.SUB_OBJECT_PROPERTY)){
+			String subid = graphWrapper.getIdentifier(ax.getSubProperty());
+			String supid = graphWrapper.getIdentifier(ax.getSuperProperty());
+			
+			if(subid != null & supid != null){
+				subrelation_ofDumper.dumpRow(subid, supid, ontologyId);
+			}
+		}
+		
+
+		TableDumper relation_disjoint_withDumper = tables.get("relation_disjoint_with");
+		for(OWLDisjointObjectPropertiesAxiom ax: getOwlOntology().getAxioms(AxiomType.DISJOINT_OBJECT_PROPERTIES)){
+			boolean first = false;
+
+			String prop1 = null;
+			for(OWLObjectPropertyExpression prop :   ax.getProperties()){
+				if(!first)
+					prop1 = graphWrapper.getIdentifier(prop);
+				else{
+					if(prop1 != null){
+						String prop2 = graphWrapper.getIdentifier(prop);
+						relation_disjoint_withDumper.dumpRow(prop1, prop2, ontologyId);
+					}
+				}
+			}
+			
+			
+		}
+		
+
+		TableDumper relation_equivalent_toDumper = tables.get("relation_equivalent_to");
+		for(OWLEquivalentObjectPropertiesAxiom ax: getOwlOntology().getAxioms(AxiomType.EQUIVALENT_OBJECT_PROPERTIES)){
+			boolean first = false;
+
+			String prop1 = null;
+			for(OWLObjectPropertyExpression prop :   ax.getProperties()){
+				if(!first)
+					prop1 = graphWrapper.getIdentifier(prop);
+				else{
+					if(prop1 != null){
+						String prop2 = graphWrapper.getIdentifier(prop);
+						relation_equivalent_toDumper.dumpRow(prop1, prop2, ontologyId);
+					}
+				}
+			}
+		}
+		
+		
+		TableDumper relation_chainDumper = tables.get("relation_chain");
+		for(OWLSubPropertyChainOfAxiom ax: getOwlOntology().getAxioms(AxiomType.SUB_PROPERTY_CHAIN_OF)){
+			boolean first = false;
+
+			String supr = graphWrapper.getIdentifier(ax.getSuperProperty());
+			String prop1 = null;
+			for(OWLObjectPropertyExpression prop :   ax.getPropertyChain()){
+				if(!first)
+					prop1 = graphWrapper.getIdentifier(prop);
+				else{
+					if(prop1 != null){
+						String prop2 = graphWrapper.getIdentifier(prop);
+						relation_chainDumper.dumpRow(supr, prop1, prop2, null);
+					}
+				}
 			}
 		}
 		
