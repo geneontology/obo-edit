d7ac7d38ed76787b32fae9bf1b950f22e72b419f
sjcarbon <kltm@users.noreply.github.com>
2010-12-06 20:12:11 +0000

    working commit

--- javascript/graph/render/test1.js
+++ javascript/graph/render/test1.js
@@ -7,43 +7,33 @@ bbop.core.require('bbop', 'model');
 bbop.core.require('bbop', 'model', 'tree');
 bbop.core.require('bbop', 'render', 'phylo');
 
-// Example graph hand loaded through tree.
-var t = new bbop.model.tree.graph();
-
-var an0 = new bbop.model.tree.node('AN0', 'AN0');
-var an1 = new bbop.model.tree.node('AN1', 'AN1');
-var an2 = new bbop.model.tree.node('AN2', 'AN2');
-var an6 = new bbop.model.tree.node('AN6', 'AN6');
-var xp9 = new bbop.model.tree.node('XP_800359', 'XP_800359');
-var xp2 = new bbop.model.tree.node('XP_790652', 'XP_790652');
-var xp0 = new bbop.model.tree.node('XP_800360', 'XP_800360');
-var q7rk = new bbop.model.tree.node('Q7RKB3', 'Q7RKB3');
-var q7rb = new bbop.model.tree.node('Q7RBF2', 'Q7RBF2');
-var q747 = new bbop.model.tree.node('Q747I8', 'Q747I8');
-t.add_node(an0);
-t.add_node(an1);
-t.add_node(an2);
-t.add_node(an6);
-t.add_node(xp9);
-t.add_node(xp2);
-t.add_node(xp0);
-t.add_node(q7rk);
-t.add_node(q7rb);
-t.add_node(q747);
-
-t.add_edge(new bbop.model.tree.edge(an0, an1, 1.0));
-t.add_edge(new bbop.model.tree.edge(an0, q747, 1.0));
-t.add_edge(new bbop.model.tree.edge(an1, an2, 0.473));
-t.add_edge(new bbop.model.tree.edge(an1, an6, 0.223));
-t.add_edge(new bbop.model.tree.edge(an2, xp9, 0.687));
-t.add_edge(new bbop.model.tree.edge(an2, xp2, 0.774));
-t.add_edge(new bbop.model.tree.edge(an2, xp0, 0.695));
-t.add_edge(new bbop.model.tree.edge(an6, q7rk, 1.366));
-t.add_edge(new bbop.model.tree.edge(an6, q7rb, 1.208));
-var layout = t.layout();
-
 // Run the layout after everything is loaded.
 window.onload = function(){
+
+    // Set settable rendering properties.
     bbop.render.phylo.use_animation = true;
-    bbop.render.phylo.display('test1', layout);
+
+    // Example graph hand loaded through tree.
+    var r = new bbop.render.phylo.renderer('test1');
+    r.add_node('AN0');
+    r.add_node('AN1');
+    r.add_node('AN2');
+    r.add_node('AN6');
+    r.add_node('XP_800359');
+    r.add_node('XP_790652');
+    r.add_node('XP_800360');
+    r.add_node('Q7RKB3');
+    r.add_node('Q7RBF2');
+    r.add_node('Q747I8');
+    r.add_edge('AN0', 'AN1', 1.0);
+    r.add_edge('AN0', 'Q747I8', 1.0);
+    r.add_edge('AN1', 'AN2', 0.473);
+    r.add_edge('AN1', 'AN6', 0.223);
+    r.add_edge('AN2', 'XP_800359', 0.687);
+    r.add_edge('AN2', 'XP_790652', 0.774);
+    r.add_edge('AN2', 'XP_800360', 0.695);
+    r.add_edge('AN6', 'Q7RKB3', 1.366);
+    r.add_edge('AN6', 'Q7RBF2', 1.208);
+
+    r.display();
 };
