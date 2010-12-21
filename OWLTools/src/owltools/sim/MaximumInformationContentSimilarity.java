170de1d048672e0c3934704df08bdad071be8644
Chris Mungall <cjm@berkeleybop.org>
2010-12-21 19:58:16 +0000

    new semantic sim code

---
+++ OWLTools/src/owltools/sim/MaximumInformationContentSimilarity.java
@@ -0,0 +1,42 @@
+package owltools.sim;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLObject;
+
+public class MaximumInformationContentSimilarity extends Similarity {
+
+	public Set<OWLObject> bestSubsumers = new HashSet<OWLObject>();
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (OWLObject obj : bestSubsumers) {
+			sb.append(obj+"; ");
+		}
+		return score + " "+sb.toString();
+	}
+
+	@Override
+	public void calculate(SimEngine simEngine, OWLObject a, OWLObject b) {
+		Set<OWLObject> objs = simEngine.getCommonSubsumers(a, b);
+		double maxIC = 0;
+		Set<OWLObject> bestSubsumers = new HashSet<OWLObject>();
+		for (OWLObject obj : objs) {
+			if (!simEngine.hasInformationContent(obj))
+				continue;
+			double ic = simEngine.getInformationContent(obj);
+			if (ic > maxIC) {
+				// warning: FP arithmetic
+				bestSubsumers = new HashSet<OWLObject>();
+				maxIC = ic;
+			}
+			if (ic >= maxIC) {
+				bestSubsumers.add(obj);
+			}
+		}
+		setScore(maxIC);
+		this.bestSubsumers = bestSubsumers;
+
+	}
+}
