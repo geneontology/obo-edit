75c7d68e39542a3ae2c7f7f96d73569d80b0c00d
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2010-12-21 22:42:48 +0000

    implementation revised for the bulkload of multiple ontologies.

--- java/gold/src/org/geneontology/web/AdminServlet.java
+++ java/gold/src/org/geneontology/web/AdminServlet.java
@@ -30,21 +30,20 @@ import owltools.graph.OWLGraphWrapper;
 /**
  * Servlet implementation class AdminServlet
  */
-public class AdminServlet extends HttpServlet{
+public class AdminServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static final Logger LOG = Logger.getLogger(AdminServlet.class);
 	private static final boolean DEBUG = LOG.isDebugEnabled();
-	
-	//only task can be running at one time
-	//task is not null then it means there 
-	//is a task running;
-	private DbOperationsTask task;
-	
 
-	//global reference of the OWLGraphWrapper
+	// only task can be running at one time
+	// task is not null then it means there
+	// is a task running;
+	private DbOperationsTask task;
+
+	// global reference of the OWLGraphWrapper
 	private Hashtable<String, OWLGraphWrapper> graphs;
-	
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -54,68 +53,86 @@ public class AdminServlet extends HttpServlet{
 		graphs = new Hashtable<String, OWLGraphWrapper>();
 	}
 
-	
-	private void printTaskStatus(Writer writer, boolean addReload) 
-		throws IOException{
-		
-		if(DEBUG){
+	private void printTaskStatus(Writer writer, boolean addReload)
+			throws IOException {
+
+		if (DEBUG) {
 			LOG.debug("addReload variable: " + addReload);
-			LOG.debug("Is Tasking Running: " + task != null ? task.isRunning() : null);
+			LOG.debug("Is Tasking Running: " + task != null ? task.isRunning()
+					: null);
 		}
-		
-		
-		if(task.isRunning() || addReload){
+
+		if (task.isRunning() || addReload) {
 			writer.write("<script type='text/javascript'>");
-				writer.write("setTimeout(\"location.reload(true)\", 9000)");
+			writer.write("setTimeout(\"location.reload(true)\", 9000)");
 			writer.write("</script>");
-			
+
 			writer.write("<center><img src=\"../images/progress-indicator.gif\" alt=\"Request is in Progress\" /></center>");
 			writer.write("<p align=\"center\">Your Request is in Progress</p>");
 		}
 
-		
-		writer.write("<h2>Status report for the running task '" + task.getOperationName() + "' :</h2>");
+		writer.write("<h2>Status report for the running task '"
+				+ task.getOperationName() + "' :</h2>");
 		writer.write("<table><tr><th>Operation Name</th><th>Status/Completion Time</th></tr>");
 		Exception ex = task.getException();
-		for(String opName: task.getCompletedOperations()){
-			 long stTime = task.getStartTime(opName);
-			 long endTime = task.getEndTime(opName);
-			 
-			 String status = "In progress";
-			 boolean isCompleted = false;
-			 if(endTime>0){
-				 status = (float)(endTime - stTime)/1000 + "";
-				 isCompleted = true;
-			 }else if (ex != null){
-				 status = "failed";
-			 }
-			 
-			 writer.write("<tr><td>" + opName + "</td><td" + (isCompleted ? " bgcolor='green'" : "") + ">" + status + "</td></tr>");
-		 }
-		 
-		 writer.write("</table>");
-		
-		 
-		 if(task.getException() != null){
+		String ontology = "";
+		for (String opName : task.getCompletedOperations()) {
+			String[] st = opName.split("--");
+			String opLocalName = opName;
+			if(st.length>1){
+				if(!ontology.equals(st[1])){
+					ontology = st[1];
+					writer.write("<tr><td colspan='2'><h4>Loading " + ontology + "</h4></td></tr>");
+				}
+				
+				opLocalName = st[0];
+			}
+			
+			long stTime = task.getStartTime(opName);
+			long endTime = task.getEndTime(opName);
+
+			String status = "In progress";
+			boolean isCompleted = false;
+			if (endTime > 0) {
+				status = (float) (endTime - stTime) / 1000 + "";
+				isCompleted = true;
+			} else if (ex != null) {
+				status = "failed";
+			}
+
+			writer.write("<tr><td>" + opLocalName + "</td><td"
+					+ (isCompleted ? " bgcolor='green'" : "") + ">" + status
+					+ "</td></tr>");
+		}
+
+		writer.write("</table>");
+
+		if (task.getException() != null) {
 			LOG.error("", task.getException());
 			PrintWriter pw = new PrintWriter(writer);
 			task.getException().printStackTrace(pw);
-		 }
-		 
-		 if(!task.isRunning() && !addReload){
-			 this.task = null;
-		 }
-		
-	}
-	
-	private OWLGraphWrapper buildOWLGraphWrapper(String ontologylocation) throws IOException{
-		try{
-			return new DbOperations().buildOWLGraphWrapper(ontologylocation);
-		}catch(Exception ex){
-			throw new IOException("An error ocurred while building OWLGraphWrapper object reference", ex);
 		}
+
+		if (!task.isRunning() && !addReload) {
+			for(OWLGraphWrapper graph: task.getGraphs()){
+				graphs.put(graph.getOntologyId(), graph);
+			}
+			this.task = null;
+		}
+
 	}
-	
+
+	/*private OWLGraphWrapper buildOWLGraphWrapper(String ontologylocation)
+			throws IOException {
+		try {
+			return new DbOperations().buildOWLGraphWrapper(ontologylocation);
+		} catch (Exception ex) {
+			throw new IOException(
+					"An error ocurred while building OWLGraphWrapper object reference",
+					ex);
+		}
+	}*/
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -123,91 +140,105 @@ public class AdminServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		
 		Writer writer = response.getWriter();
 		writer.write("<html>");
 		writer.write("<head><title>");
 		writer.write("GOLD Admin Operation Status");
 		writer.write("</title></head>");
-		
+
 		String command = request.getParameter("command");
-		
-		if(DEBUG){
+
+		if (DEBUG) {
 			LOG.debug("Command parameter = " + command);
 		}
-		
+
 		boolean addReload = false;
 		if (command != null && task == null) {
-			if ("bulkload".equals(command) || "update".equals(command)) {
-				
-				String ontologylocation = request.getParameter("ontologylocation");
-				
-				if(ontologylocation==null){
+			if ("update".equals(command)) {
+
+				String ontologylocation = request
+						.getParameter("ontologylocation");
+
+				if (ontologylocation == null) {
 					printOntologySelectionForm(writer, command);
-				}else{
-				
-					String force = request.getParameter("force");
-					OWLGraphWrapper graph = buildOWLGraphWrapper(ontologylocation);
-					
-					if(graph == null){
-						writer.write("<h1>OWLGraphWrapper cannot be built from the ontologylocation at '" + ontologylocation + "'");
-					}else{
-						graphs.put(graph.getOntologyId(), graph);
+				} else {
+
+					/*OWLGraphWrapper graph = buildOWLGraphWrapper(ontologylocation);
+
+					if (graph == null) {
+						writer.write("<h1>OWLGraphWrapper cannot be built from the ontologylocation at '"
+								+ ontologylocation + "'");
+					} else {
+						graphs.put(graph.getOntologyId(), graph);*/
 						addReload = true;
-						task = new DbOperationsTask(command, graph, 
-								"true".equals(force), "", "");
+						task = new DbOperationsTask(command,
+								new String[] { ontologylocation }, false, "", "");
 						task.start();
-					}
-					}
-				 
-			}else if ("getlastupdate".equals(command)){
+					//}
+				}
+
+			}else if ("bulkload".equals(command)){ 
+				List list = GeneOntologyManager.getInstance().getDefaultOntologyLocations();
+				String locations[] = new String[list.size()];
+				list.toArray(locations);
+				addReload = true;
+				String force = request.getParameter("force");
+				task = new DbOperationsTask(command,
+						locations,  "true".equals(force), "", "");
+				task.start();
+			
+			}else if ("getlastupdate".equals(command)) {
 				DbOperations db = new DbOperations();
 
 				writer.write("<h1>Ontology update history</h1>");
-				for(Ontology ont: db.getLastUpdateStatus()){
+				for (Ontology ont : db.getLastUpdateStatus()) {
 
 					writer.write("<hr />");
-					
+
 					writer.write("Ontology          : " + ont.getLabel());
-					writer.write("<br >Version           : " + ont.getVersioniri());
-					writer.write("<br />Laste Update Date : " + ont.getCreationDate());
-					
+					writer.write("<br >Version           : "
+							+ ont.getVersioniri());
+					writer.write("<br />Laste Update Date : "
+							+ ont.getCreationDate());
+
 				}
 			}
-			
-			
-			try{
-				Thread.sleep(1000);
-			}catch(Exception ex){
-				
-			}
-			
 
-		}else if(task == null){
+			try {
+				Thread.sleep(1000);
+			} catch (Exception ex) {
+
+			}
+
+		} else if (task == null) {
 			writer.write("<h1>No valid parameters are provided. Please call this page with valid parameters</h1>");
 			return;
 		}
-		
-	//	DbOperationsTask task = (DbOperationsTask) obj;
-		
-		if(task != null){
+
+		// DbOperationsTask task = (DbOperationsTask) obj;
+
+		if (task != null) {
 			printTaskStatus(writer, addReload);
 		}
-		
+
 		writer.write("</body></html>");
 	}
 
-	private void printOntologySelectionForm(Writer writer, String command) throws IOException{
+	private void printOntologySelectionForm(Writer writer, String command)
+			throws IOException {
 		writer.write("<h1>Gene Ontology Admin</h1>");
 		writer.write("<form action='.'>");
-		writer.write("<input type='hidden' name='command' value='" + command + "' />");
+		writer.write("<input type='hidden' name='command' value='" + command
+				+ "' />");
 		writer.write("<label>Please select ontology</label><br />");
-		for(Object obj: GeneOntologyManager.getInstance().getDefaultOntologyLocations() ){
-			writer.write("<input type='radio' name='ontologylocation' value='" + obj + "' />"+ obj +"<br />");
+		for (Object obj : GeneOntologyManager.getInstance()
+				.getDefaultOntologyLocations()) {
+			writer.write("<input type='radio' name='ontologylocation' value='"
+					+ obj + "' />" + obj + "<br />");
 		}
-		writer.write("<br /><label>Force to recreate the database</label>");
-		writer.write("<input type='checkbox' name='force' value='true' /><br /><br />");
-		writer.write("<input type='submit' /><br />");
+//		writer.write("<br /><label>Force to recreate the database</label>");
+//		writer.write("<input type='checkbox' name='force' value='true' /><br /><br />");
+		writer.write("<input type='submit' value='update' /><br />");
 		writer.write("</form>");
 	}
 
