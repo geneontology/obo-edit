9782aa8e66486d6545224457342bf82879573683
svenmh <svenmh@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-01-17 15:53:58 +0000

    it now uses the fresh script for qfo files

--- go-dev/go-db-perl/scripts/go_db_install.pl
+++ go-dev/go-db-perl/scripts/go_db_install.pl
@@ -61,9 +61,9 @@ use vars qw(
 	    $opt_z
 	    $opt_D
 	    $opt_M
-	    $opt_F
 	   );
 # Remove opt_F after go-load-qfo-fresh.pl is default
+my $opt_F=1;
 
 ## Sane and easy to modify defaults.
 my %local = (
@@ -96,7 +96,8 @@ my %local = (
 	    );
 
 # Remove F after go-load-qfo-fresh.pl is default
-getopts('Fhvzijxng:m:a:s:f:l:d:u:p:P:t:e:D:M:');
+#getopts('Fhvzijxng:m:a:s:f:l:d:u:p:P:t:e:D:M:');
+getopts('hvzijxng:m:a:s:f:l:d:u:p:P:t:e:D:M:');
 
 ## Hunt down the paths and let's try and get all of the likely
 ## libraries in. Trying "use lib" didn't work so well, so got hacky
