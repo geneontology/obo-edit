3b96ce1e66540a5c067fe92eabafcb3eee795fe3
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-01-25 00:58:19 +0000

    re-structuring of admin servlet is in progress

---
+++ java/gold/src/org/geneontology/web/services/ServiceHandlerAbstract.java
@@ -0,0 +1,14 @@
+package org.geneontology.web.services;
+
+import org.apache.log4j.Logger;
+
+
+public abstract class ServiceHandlerAbstract implements ServiceHandler  {
+
+	private static Logger LOG = Logger.getLogger(ServiceHandlerAbstract.class);
+	
+	protected ServiceHandlerAbstract(){
+	
+	}
+	
+}
