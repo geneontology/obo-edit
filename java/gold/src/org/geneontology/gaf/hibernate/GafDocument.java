3224ddc04dd1ff27f1e08745de5610fa8baef203
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-02-03 20:58:22 +0000

    gaf hibernate

--- java/gold/src/org/geneontology/gaf/hibernate/GafDocument.java
+++ java/gold/src/org/geneontology/gaf/hibernate/GafDocument.java
@@ -1,6 +1,9 @@
 package org.geneontology.gaf.hibernate;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Set;
 
 import org.geneontology.gold.hibernate.model.GOModel;
@@ -10,10 +13,20 @@ public class GafDocument extends GOModel implements Serializable {
 	private String id;
 	private String documentPath;
 	
+	private transient Hashtable<String, Bioentity> bioentities;
+	private transient Hashtable<String, Set<WithInfo>> withInfos;
+	private transient Hashtable<String, Set<ExtensionExpression>> extensionExpressions;
+	private transient Hashtable<String, Set<CompositeQualifier>> compositeQualifiers; 
+	private transient HashSet<GeneAnnotation> annotations;
+	
 	public GafDocument(){
 		String keys [] = {"id"};
 		this.initUniqueConstraintFields(GafDocument.class, keys);
-		
+		bioentities = new Hashtable<String, Bioentity>();
+		withInfos = new Hashtable<String, Set<WithInfo>>();
+		extensionExpressions = new Hashtable<String, Set<ExtensionExpression>>();
+		compositeQualifiers = new Hashtable<String, Set<CompositeQualifier>>();
+		annotations = new HashSet<GeneAnnotation>();
 	}
 	
 	public GafDocument(String id, String documentPath) {
@@ -39,16 +52,16 @@ public class GafDocument extends GOModel implements Serializable {
 	}
 	
 	public Bioentity getBioentity(String id){
-		return null;
+		return bioentities.get(id);
 	}
 
-	public Set<Bioentity> getBioentities(){
-		return null;
+	public Collection<Bioentity> getBioentities(){
+		return bioentities.values();
 	}
 	
 
 	public Set<GeneAnnotation> getGeneAnnotations(){
-		return null;
+		return annotations;
 	}
 	
 	public Set<GeneAnnotation> getGeneAnnotations(String bioentity){
@@ -60,25 +73,71 @@ public class GafDocument extends GOModel implements Serializable {
 	}
 	
 	public void addBioentity(Bioentity bioentity){
+		if(bioentities.contains(bioentity.getId()))
+				return;
 		
+		bioentities.put(bioentity.getId(), bioentity);
 	}
 	
 	public void addCompositeQualifier(CompositeQualifier compositeQualifier){
+		Set<CompositeQualifier> set = compositeQualifiers.get(compositeQualifier.getId());
+		if(set == null){
+			set = new HashSet<CompositeQualifier>();
+			compositeQualifiers.put(compositeQualifier.getId(), set);
+		}
+		
+		set.add(compositeQualifier);
+	}
+	
+	public Set<String> getCompositeQualifiersIds(){
+		return compositeQualifiers.keySet();
+	}
+	
+	public Set<CompositeQualifier> getCompositeQualifiers(String id){
+		return compositeQualifiers.get(id);
+	}
+	
+	public void addWithInfo(WithInfo withInfo){
+		
+		Set<WithInfo> set = withInfos.get(withInfo.getId());
+		if(set == null){
+			set = new HashSet<WithInfo>();
+			withInfos.put(withInfo.getId(), set);
+		}
+		
+		set.add(withInfo);
 		
 	}
 	
+	public Set<String> getWithInfosIds(){
+		return withInfos.keySet();
+	}
 	
-	//only add if another instance of the same information already does not exists
-	public void addWithInfo(WithInfo withInfo){
-		
+	public Set<WithInfo> getWithInfos(String id){
+		return withInfos.get(id);
+	}
+	
+	
+	public Set<ExtensionExpression> getExpressions(String id){
+		return extensionExpressions.get(id);
+	}
+	
+	public Set<String> getExtensionExpressionIds(){
+		return extensionExpressions.keySet();
 	}
 	
 	public void addExtensionExpression(ExtensionExpression extensionExpression){
+		Set<ExtensionExpression> set = extensionExpressions.get(extensionExpression.getId());
+		if(set == null){
+			set = new HashSet<ExtensionExpression>();
+			extensionExpressions.put(extensionExpression.getId(), set);
+		}
 		
+		set.add(extensionExpression);
 	}
 	
 	public void addGeneAnnotation(GeneAnnotation ga){
-		
+		annotations.add(ga);
 	}
 	
 }
