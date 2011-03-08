3260ebe302beb719fa10e37faaa143e6ce9970c4
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-03-08 00:10:43 +0000

    updates in javadoc documentation

--- java/gold/src/org/geneontology/web/AdminServlet.java
+++ java/gold/src/org/geneontology/web/AdminServlet.java
@@ -1,70 +1,59 @@
 package org.geneontology.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.util.Hashtable;
-import java.util.List;
-
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.log4j.Logger;
-import org.geneontology.conf.GeneOntologyManager;
-import org.geneontology.gold.hibernate.model.Ontology;
-import org.geneontology.gold.io.DbOperations;
-import org.geneontology.gold.io.DbOperationsListener;
-import org.geneontology.gold.io.OntologyBulkLoader;
-import org.geneontology.gold.io.postgres.SchemaManager;
-import org.geneontology.gold.io.postgres.TsvFileLoader;
+import org.geneontology.web.services.GafDbOperationsService;
+import org.geneontology.web.services.GoldDbOperationsService;
+import org.geneontology.web.services.ReasoningService;
 import org.geneontology.web.services.ServiceHandler;
 import org.geneontology.web.services.ServicesConfig;
-import org.obolibrary.obo2owl.Obo2Owl;
-import org.obolibrary.oboformat.model.OBODoc;
-import org.obolibrary.oboformat.parser.OBOFormatParser;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import owltools.graph.OWLGraphWrapper;
 
 /**
- * Servlet implementation class AdminServlet
+ * This servlet is the main entry point of the web interface of the 
+ * Gold project.
+ * All the different operations are implemented as Services
+ * (see {@link ServiceHandler}. 
+ * This serlvet expects the 'service-name'  required
+ * request parameter. It finds the service object via service name in the
+ *  {@link ServicesConfig} class object. It calls the handle service method of the service object.
+ *  The service object is supposed to do 
+ * computations and store the results in the request object via 
+ *  setAttribute method.
+ * The service also sets the view name (a jsp file) which renders the results.
+ *  In the last step the servlet forwards the request to the jsp file.
+ * 
+ * See also {@link GoldDbOperationsService}, {@link ReasoningService} and {@link GafDbOperationsService}
+ * @author Shahid Manzoor
+ *
  */
 public class AdminServlet extends HttpServlet {
-	
-	/*private void printError(String message, PrintWriter writer){
-		writer.println("<html><body>");
-		writer.println("<center><h1>************"+ message +"************</h1></center>");
-		writer.println("</body></html>");
-		
-	}*/
-	
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
 
-	
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response)
+			throws ServletException, IOException {
+
+		//get the service name from the parameter
 		String servicename= request.getParameter("servicename");
 		
 		String error = null;
 		
 		if(servicename == null){
 			error = "servicename parameter is missing in the parameter";
-		//	printError("servicename parameter is missing in the parameter", response.getWriter());
-	//		return;
 		}
 		
+		//find the service object
 		ServiceHandler handler= ServicesConfig.getService(servicename);	
 		String view = null;
 		if(handler == null){
 			error = "The service '"+ servicename + "' is not supproted by the server";
-			//printError("The service '"+ servicename + "' is not supproted by the server", response.getWriter());
-		//	return;
 		}else{
+			
 			handler.handleService(request, response);
 			view = handler.getViewPath();
 		}
@@ -74,13 +63,14 @@ public class AdminServlet extends HttpServlet {
 			request.setAttribute("error", error);
 		}
 		
+		//forwarding the request to a jsp file reffered in the 'view' variable
 		if(view != null){
-			//RequestDispatcher dispatcher = getServletContext().getContext(handler.getViewPath()). .getRequestDispatcher(handler.getViewPath());
 			ServletContext context = getServletContext().getContext("/");
 			RequestDispatcher dispatcher = context.getRequestDispatcher(view);
 			dispatcher.forward(request, response);
 		}
-		
+	
 	}
+
 
 }
