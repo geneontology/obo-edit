4d43c93a35069e0fe0396b0be03cc795530790a9
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-03-29 21:37:08 +0000

    functionality of upload gaf is implemented

--- java/gold/src/org/geneontology/web/AdminServlet.java
+++ java/gold/src/org/geneontology/web/AdminServlet.java
@@ -1,17 +1,30 @@
 package org.geneontology.web;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+import org.geneontology.conf.GeneOntologyManager;
 import org.geneontology.web.services.GafDbOperationsService;
 import org.geneontology.web.services.GoldDbOperationsService;
 import org.geneontology.web.services.ReasoningService;
 import org.geneontology.web.services.ServiceHandler;
 import org.geneontology.web.services.ServicesConfig;
+
 
 /**
  * This servlet is the main entry point of the web interface of the 
@@ -34,11 +47,68 @@ import org.geneontology.web.services.ServicesConfig;
 public class AdminServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		
+		System.out.println("Post method.........." + request.getRequestURI());
 
-		resp.getWriter().println("Post method");
+		if(ServletFileUpload.isMultipartContent(request)){
+			response.getWriter().println("File uload");
+			
+			try{
+				ServletFileUpload servletFileUpload = new ServletFileUpload(new DiskFileItemFactory());
+				List fileItemsList = servletFileUpload.parseRequest(request);		
+				
+				Iterator it = fileItemsList.iterator();
+				
+				FileItem uploadFile = null;
+				Hashtable<String, String> parameters = new Hashtable<String, String>();
+				String query = "";
+				  while (it.hasNext()){
+				    FileItem fileItemTemp = (FileItem)it.next();
+				    if (fileItemTemp.isFormField()){
+				    	response.getWriter().println(fileItemTemp.getFieldName());
+				    	//query += fileItemTemp.getFieldName() + "=" + URLEncoder.encode(fileItemTemp.getString(), "UTF-8") + "&";
+				    	parameters.put(fileItemTemp.getFieldName(), fileItemTemp.getString());
+				    }else{
+				    	uploadFile = fileItemTemp;
+				    }
+				  }
+				  
+				  if(uploadFile != null){
+					  String fileName = uploadFile.getName();
+					  fileName = FilenameUtils.getName(fileName);
+					  
+					  String dirName = GeneOntologyManager.getInstance().getGafUploadDir();
+					  
+					  File file = new File(dirName, fileName);
+					  
+					//  query += "filepath=" + URLEncoder.encode(file.getAbsolutePath(), "UTF-8");
+					  parameters.put("filelocation", file.getCanonicalPath());
+					  
+					  
+					  uploadFile.write(file);
+					  
+					  
+				  }
+				  
+				  request.setAttribute("parameters", parameters);
+				
+					ServletContext context = getServletContext().getContext("/");
+					RequestDispatcher dispatcher = context.getRequestDispatcher("/servicesui/uploadfile.jsp");
+					dispatcher.forward(request, response);
+				  
+				  
+			}catch(Exception ex){
+				throw new ServletException(ex);
+			}
+			
+			
+		}else{
+			doGet(request, response);
+		}
+		
 	}
 
 	
