407a8f18322639696d63f07a655f9006ace0719d
Chris Mungall <cjm@berkeleybop.org>
2011-04-01 19:55:29 +0000

    new

--- java/gold/sql/gaf_ix.sql
+++ java/gold/sql/gaf_ix.sql
@@ -1,3 +1,11 @@
 CREATE INDEX gene_annotation_ix_cls ON gene_annotation(cls);
 CREATE INDEX gene_annotation_ix_bioentity ON gene_annotation(bioentity);
 CREATE INDEX gene_annotation_ix_cls_bioentity ON gene_annotation(cls,bioentity);
+
+CREATE INDEX composite_qualifier_ix_id ON composite_qualifier(id);
+CREATE INDEX composite_qualifier_ix_obj ON composite_qualifier(qualifier_obj);
+CREATE INDEX composite_qualifier_ix_id_obj ON composite_qualifier(id,qualifier_obj);
+
+CREATE INDEX with_info_ix_id ON with_info(id);
+CREATE INDEX with_info_ix_xref ON with_info(with_xref);
+CREATE INDEX with_info_ix_id_xref ON with_info(id,with_xref);
