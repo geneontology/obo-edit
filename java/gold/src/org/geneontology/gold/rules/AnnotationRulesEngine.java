776b7f7d0a183f0e46fa6f057e747503193eb94e
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-04-12 21:13:23 +0000

    implement of rule engine and the rules encoded in xml

--- java/gold/src/org/geneontology/gold/rules/AnnotationRulesEngine.java
+++ java/gold/src/org/geneontology/gold/rules/AnnotationRulesEngine.java
@@ -1,9 +1,13 @@
 package org.geneontology.gold.rules;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.geneontology.gaf.hibernate.GafDocument;
+import org.geneontology.gaf.hibernate.GeneAnnotation;
 
 public class AnnotationRulesEngine {
 
@@ -19,7 +23,7 @@ public class AnnotationRulesEngine {
 		rules.add(rule);
 		
 		
-		try{
+		/*try{
 			rule  = new AnnotationTaxonRule();
 			rules.add(rule);
 		}catch(Exception ex){
@@ -33,7 +37,7 @@ public class AnnotationRulesEngine {
 		rules.add(rule);
 		
 		rule = new GoClassReferenceAnnotationRule();
-		rules.add(rule);
+		rules.add(rule);*/
 	}
 	
 	public static AnnotationRulesEngine getInstance(){
@@ -43,4 +47,27 @@ public class AnnotationRulesEngine {
 		
 		return engine;
 	}
+	
+	public List<AbstractAnnotatioRule> getRules(){
+		return rules;
+	}
+	
+	
+	public Set<AnnotationRuleViolation> validateAnnotations(GafDocument doc){
+		HashSet<AnnotationRuleViolation> set = new HashSet<AnnotationRuleViolation>();
+		
+		try{
+		
+			for(GeneAnnotation annotation: doc.getGeneAnnotations()){
+				for(AbstractAnnotatioRule rule: rules){
+					set.addAll( rule.getRuleViolations(annotation) );
+				}
+			}
+		}catch(Exception ex){
+			LOG.error(ex.getMessage(), ex);
+		}
+		
+		return set;
+	}
+	
 }
