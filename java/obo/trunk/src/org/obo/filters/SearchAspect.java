cdd9550204dc0f26e2bd0f135420cdc9d9fb5795
nomi <nomi@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-05-10 20:52:03 +0000

    comments

--- java/obo/trunk/src/org/obo/filters/SearchAspect.java
+++ java/obo/trunk/src/org/obo/filters/SearchAspect.java
@@ -10,5 +10,8 @@ public interface SearchAspect extends Cloneable {
 	public Collection<LinkedObject> getObjects(Collection<LinkedObject> c,
 			ReasonedLinkDatabase reasoner, Filter traversalFilter, Object o);
 	public String getID();
+
+        // 5/9/11: This method was used for an experiment where search aspects that required the reasoner were
+        // left off the aspect menu if the reasoner was off.  It is not currently used and could probably be removed.
 	public boolean requiresReasoner();
   }
