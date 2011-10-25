207e2989474ca96846222e881b8a0edd5e3f3db3
shahidmanzoor <shahidmanzoor@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-10-25 21:11:14 +0000

    security policy is implemented.

--- java/gold/src/org/geneontology/web/services/LoginService.java
+++ java/gold/src/org/geneontology/web/services/LoginService.java
@@ -1,17 +1,22 @@
 package org.geneontology.web.services;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.log4j.Logger;
 import org.geneontology.jetty.JettyStarter;
 
 import com.google.gson.Gson;
@@ -24,12 +29,76 @@ public class LoginService extends ServiceHandlerAbstract {
 	
 	private String viewPath;
 	
+	private static Logger LOG = Logger.getLogger(LoginService.class);
+	
+	private static boolean DEBUG = LOG.isDebugEnabled();
+	
 	private boolean isLoggedIn;
+	
+	private static Hashtable<String, List<String>> servicesWithSecurity;
+	
+	static{
+
+		try{
+			File f = new File("conf/security.properties");
+
+			servicesWithSecurity = new Hashtable<String, List<String>>();
+			
+		//	PropertyListConfiguration root = new PropertyListConfiguration(f);
+			
+		//	Configuration config = root.subset("geneontology.gold.security.service");
+			PropertiesConfiguration config = new PropertiesConfiguration(f);
+			
+			String prefix = "geneontology.gold.security.service";
+			HashSet<String> servicesIndexes = new HashSet<String>();
+			Iterator itr = config.getKeys(prefix);
+			while(itr.hasNext()){
+				String key = (String)itr.next();
+				int index = key.indexOf(".", prefix.length()+1);
+				String servicePrefix = key.substring(prefix.length()+1, index);
+				
+				if(DEBUG)
+					LOG.debug(servicePrefix);
+				
+				servicesIndexes.add(servicePrefix);
+			}
+			
+			for(String index: servicesIndexes){
+				String key = prefix + "." + index + ".name";
+				String serviceName = config.getString(key);
+				
+				if(serviceName != null){
+					List<String> list = new ArrayList<String>();
+					servicesWithSecurity.put(serviceName.trim(), list);
+					for(Object restriction: config.getList(prefix + "." + index + ".restriction")){
+						list.add(((String)restriction).trim());
+					}
+				}
+			}
+		
+			if(DEBUG){
+				LOG.debug(servicesWithSecurity.keySet());
+				LOG.debug(servicesWithSecurity.values());
+			}
+			
+			
+		}catch(Exception ex){
+			LOG.error("Error in reading conf/security.xml file", ex);
+		}
+		
+		
+	}
+	
+	public LoginService(){
+	}
+	
 	
 	@Override
 	public void handleService(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
 	
+	
+		HttpSession session = request.getSession(true);
 		
 		this.viewPath = "/servicesui/login.jsp";
 
@@ -40,8 +109,27 @@ public class LoginService extends ServiceHandlerAbstract {
 			return;
 		}
 		
-		HttpSession session = request.getSession(true);
+		String servicName = request.getParameter("servicename");
 
+		if(servicName == null){
+			this.isLoggedIn = true;
+			return;
+		}
+		
+		List<String> commands= servicesWithSecurity.get(servicName);
+		
+		if(commands == null){
+			this.isLoggedIn = true;
+			return;
+		}
+		
+		String command = request.getParameter("command");
+		
+		if(!commands.contains("*") && (command == null || !commands.contains(command))){
+			this.isLoggedIn = true;
+			return;
+		}
+		
 		Object userId = session.getAttribute("userid");
 		String assertion = request.getParameter("assertion");
 		//user is not logged-in uet
@@ -56,6 +144,7 @@ public class LoginService extends ServiceHandlerAbstract {
 		}
 		
 	}
+	
 
 	private BrowserIdVerificationResponse authenticateUser(String assertion, String audienceValue) throws HttpException, IOException{
 		PostMethod post = new PostMethod("https://browserid.org/verify");
