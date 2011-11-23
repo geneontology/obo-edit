4bd5581d3fdade45b02bf5c91c77b19bf7cf576b
beladia <beladia@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-11-23 07:06:42 +0000

    Renamed ptc to performTransitiveClosure and made it static. Removed class attributes and getters and setters corresponding to the same.

--- OWLTools/src/owltools/phenolog/PhenoTransitiveClosure.java
+++ OWLTools/src/owltools/phenolog/PhenoTransitiveClosure.java
@@ -10,50 +10,10 @@ import owltools.graph.OWLGraphWrapper;
 import owltools.io.ParserWrapper;
 import org.semanticweb.owlapi.model.OWLObject;
 
-/**
- *
- * @author beladia
- */
 public class PhenoTransitiveClosure {
-    private String obolink;
-    private String prefix;
-    private HashSet<Pheno> hsp;
-    private HashMap<String, Pheno> hmp;
-    private HashMap<String, IndividualPair> hmpair;
 
-    public String getobolink(){
-        return this.obolink;
-    }
-    public String getprefix(){
-        return this.prefix;
-    }
-    public HashSet<Pheno> gethsp(){
-        return this.hsp;
-    }
-    public HashMap<String, Pheno> gethmp(){
-        return this.hmp;
-    }
-    public HashMap<String, IndividualPair> gethmpair(){
-        return this.hmpair;
-    }
 
-    public void setobolink(String obolink){
-        this.obolink = obolink;
-    }
-    public void setprefix(String prefix){
-        this.prefix = prefix;
-    }
-    public void sethsp(HashSet<Pheno> hsp){
-        this.hsp = hsp;
-    }
-    public void sethmp(HashMap<String, Pheno> hmp){
-        this.hmp = hmp;
-    }
-    public void sethmpair(HashMap<String, IndividualPair> hmpair){
-        this.hmpair = hmpair;
-    }
-
-    private HashSet<Pheno> ptc(String obolink, String prefix, HashSet<Pheno> hsp, HashSet<Pheno> hsp2, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair) {
+    public static HashSet<Pheno> performtransiviteclosure(String obolink, String prefix, HashSet<Pheno> hsp, HashSet<Pheno> hsp2, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair) {
         HashSet<Pheno> aux = new HashSet<Pheno>();
         try {
             ParserWrapper pw = new ParserWrapper();
@@ -117,10 +77,5 @@ public class PhenoTransitiveClosure {
         }
 
         return aux;
-    }
-
-
-    public HashSet<Pheno> performtransiviteclosure(String obolink, String prefix, HashSet<Pheno> hsp, HashSet<Pheno> hsp2, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair){
-        return ptc(obolink, prefix, hsp, hsp2, hmp, hmpair);
     }
 }
