f84e33f3eab1013eb14bd76624fd55f6bbb96843
beladia <beladia@6f0e8829-b336-0410-acfb-cb9b228023ad>
2011-11-23 05:29:10 +0000

    Modified performTransitiveClosure method. Added an extra condition to prevent addition of phenotypes already present in other species base gene-pheno mapping.

--- OWLTools/src/owltools/phenolog/PhenoTransitiveClosure.java
+++ OWLTools/src/owltools/phenolog/PhenoTransitiveClosure.java
@@ -53,7 +53,7 @@ public class PhenoTransitiveClosure {
         this.hmpair = hmpair;
     }
 
-    private HashSet<Pheno> ptc(String obolink, String prefix, HashSet<Pheno> hsp, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair) {
+    private HashSet<Pheno> ptc(String obolink, String prefix, HashSet<Pheno> hsp, HashSet<Pheno> hsp2, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair) {
         HashSet<Pheno> aux = new HashSet<Pheno>();
         try {
             ParserWrapper pw = new ParserWrapper();
@@ -71,12 +71,16 @@ public class PhenoTransitiveClosure {
                 tset = p.getancestors();
                 ancs = owlg.getNamedAncestorsReflexive(owlg.getOWLObjectByIdentifier(p.getId()));
 
-                for (OWLObject c : ancs) {                
+                for (OWLObject c : ancs) {
+                	// If the ancestor is the same as p, set the label of p if its not already set
                     if (owlg.getIdentifier(c).equals(p.getId()) && p.getLabel()==null){
                         p.setLabel(owlg.getLabel(c));
                     }
                     
-                    if (owlg.getIdentifier(c).contains(prefix) && !(owlg.getIdentifier(c).equals(p.getId()))) {
+                    // If the the ancestor is not the same as p, add all genes associated with p to the ancestor
+                    // If the ancestor is not already present in the phenotype set, add it in (if its not a phenotype already present in the other "species base phenotype set")
+                    // base phenotype set = phenotype set based on raw input data (i.e. without new phenotype additions resulting from transitive closure) 
+                    if (!(hsp2.contains(new Pheno(owlg.getIdentifier(c)))) && owlg.getIdentifier(c).contains(prefix) && !(owlg.getIdentifier(c).equals(p.getId()))) {
                         if (hmp.get(owlg.getIdentifier(c)) == null) {
                             tmpi = new HashSet<Individual>();
                             tmpi.addAll(p.getIndividuals());
@@ -115,10 +119,8 @@ public class PhenoTransitiveClosure {
         return aux;
     }
 
-    public HashSet<Pheno> performtransiviteclosure(){
-        return ptc(this.obolink, this.prefix, this.hsp, this.hmp, this.hmpair);
-    }
-    public HashSet<Pheno> performtransiviteclosure(String obolink, String prefix, HashSet<Pheno> hsp, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair){
-        return ptc(obolink, prefix, hsp, hmp, hmpair);
+
+    public HashSet<Pheno> performtransiviteclosure(String obolink, String prefix, HashSet<Pheno> hsp, HashSet<Pheno> hsp2, HashMap<String, Pheno> hmp, HashMap<String, IndividualPair> hmpair){
+        return ptc(obolink, prefix, hsp, hsp2, hmp, hmpair);
     }
 }
