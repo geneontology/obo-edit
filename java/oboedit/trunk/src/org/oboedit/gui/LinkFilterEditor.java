3e02bed708b99a3bd476827a62e2422032dd05ca
Nomi Harris <nlharris@users.noreply.github.com>
2011-12-21 18:34:29 +0000

    Got rid of an unneeded logger.debug call

--- java/oboedit/trunk/src/org/oboedit/gui/LinkFilterEditor.java
+++ java/oboedit/trunk/src/org/oboedit/gui/LinkFilterEditor.java
@@ -31,7 +31,6 @@ public class LinkFilterEditor extends TermFilterEditor {
 
 	public LinkFilterEditor() {
 		super();
-		logger.debug("LinkFilterEditor init"); // DEL
 		aspectBox.addActionListener(linkAspectBoxListener);
 	}
 	
