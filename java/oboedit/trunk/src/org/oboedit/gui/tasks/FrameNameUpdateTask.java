7f5fc1235c9ba47b867878067388dfd018675f0e
Nomi Harris <nlharris@users.noreply.github.com>
2012-01-17 06:18:51 +0000

    [ geneontology-Feature Requests-3469108 ] Titlebar should show last saved-as filename 1 i Now listens for reconfigEvents to know when to update filename in titlebar

--- java/oboedit/trunk/src/org/oboedit/gui/tasks/FrameNameUpdateTask.java
+++ java/oboedit/trunk/src/org/oboedit/gui/tasks/FrameNameUpdateTask.java
@@ -3,6 +3,8 @@ package org.oboedit.gui.tasks;
 import org.bbop.framework.GUIManager;
 import org.obo.datamodel.OBOSession;
 import org.oboedit.controller.SessionManager;
+import org.oboedit.gui.event.ReconfigEvent;
+import org.oboedit.gui.event.ReconfigListener;
 import org.oboedit.gui.Preferences;
 
 import org.apache.log4j.*;
@@ -12,14 +14,25 @@ public class FrameNameUpdateTask extends AbstractReloadTask {
 	//initialize logger
 	protected final static Logger logger = Logger.getLogger(FrameNameUpdateTask.class);
 
+
+	protected ReconfigListener listener = new ReconfigListener() {
+                // This is triggered when the user saves to a file. When this happens, we
+		// want to update the frame title.
+		public void configReloaded(ReconfigEvent e) {
+		    //		    logger.debug("configReloaded " + e); // DEL
+		    reload();
+		}
+	};
+
 	public FrameNameUpdateTask() {
+		Preferences.getPreferences().addReconfigListener(listener);
 		// Set initial title
 		reload();
 	}
 
 	@Override
 	public void reload() {
-		//logger.debug("FrameNameUpdateTask: setting title to " + getFrameTitle());
+	    //	        logger.debug("FrameNameUpdateTask: setting title to " + getFrameTitle());
 	        GUIManager.getManager().getFrame().setTitle(getFrameTitle());
 	}
 
