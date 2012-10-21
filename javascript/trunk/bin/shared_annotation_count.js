b3e4cd806a04c379c6029db1ee02d58eb4eb80a7
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2012-10-21 05:33:47 +0000

    added a script to do shared annotation counts; test for val

--- javascript/trunk/bin/shared_annotation_count.js
+++ javascript/trunk/bin/shared_annotation_count.js
@@ -11,13 +11,13 @@
  * : shared_annotation_count.js GO:0043473 GO:0009987 GO:0022008 ...
  */
 
-// Loading the necessary files.
-// TODO/BUG: These should be pointing at the remote files, not the
-// local ones.
-load('../staging/bbop.js');
-load('../_data/golr.js');
+// Load the necessary remote files.
+print('Downloading libraries...');
+eval(readUrl('http://cdn.berkeleybop.org/jsapi/bbop_0.9.min.js'));
+eval(readUrl('http://cdn.berkeleybop.org/jsapi/amigo_0.9.min.js'));
 
 // First, collect all of our input.
+print('Collecting input.');
 var term_accs = [];
 for(var arg_i = 0; arg_i < arguments.length; arg_i++){
     var arg_acc = arguments[arg_i];
@@ -25,15 +25,17 @@ for(var arg_i = 0; arg_i < arguments.length; arg_i++){
 }
 
 // Next, setup the manager environment.
+print('Setting up manager.');
 var gconf = new bbop.golr.conf(amigo.data.golr);
 var go = new bbop.golr.manager.rhino('http://golr.berkeleybop.org/', gconf);
 go.add_query_filter('document_category', 'annotation', ['*']);
 go.set_personality('bbop_ann');
-//go.debug(false); // I think the default is still on?
+go.debug(false); // I think the default is still on?
 
 // Now, cycle though all of the posible pairs of terms while setting
 // and unsetting the query filter on the manager. Print the output as
 // we progress.
+print('Gathering data...');
 for(var v_i = 0; v_i < term_accs.length; v_i++){
     for(var h_i = 0; h_i < v_i; h_i++){
 	var v = term_accs[v_i];
@@ -50,3 +52,5 @@ for(var v_i = 0; v_i < term_accs.length; v_i++){
 	print(h + ', ' + v + ': ' + count);
    }
 }
+
+print('Done.');
