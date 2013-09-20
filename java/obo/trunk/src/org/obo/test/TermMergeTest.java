c5f0bd5c6d85385b55aaf34cc757e98d22e6f29b
heikodietze <heikodietze@6f0e8829-b336-0410-acfb-cb9b228023ad>
2013-09-20 18:36:16 +0000

    * Bug fix: merge def db xref during term merge * Extend test case

--- java/obo/trunk/src/org/obo/test/TermMergeTest.java
+++ java/obo/trunk/src/org/obo/test/TermMergeTest.java
@@ -7,8 +7,10 @@ package org.obo.test;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
+import org.obo.datamodel.Dbxref;
 import org.obo.datamodel.OBOClass;
 import org.obo.datamodel.Synonym;
 import org.obo.datamodel.SynonymType;
@@ -138,9 +140,73 @@ public class TermMergeTest extends AbstractOBOTest {
 				assertNotNull(synonymType);
 				String id = synonymType.getID();
 				found = "st1".equals(id);
+				break;
+			}
+		}
+		assertTrue(found);
+	}
+	
+	public void testMergeXrefs() throws Exception {
+		OBOClass master = (OBOClass) session.getObject("Test:00060000");
+		OBOClass slave = (OBOClass) session.getObject("Test:00060001");
+		
+		HistoryItem item = new TermMergeHistoryItem(master, slave);
+		operationModel = new DefaultOperationModel();
+		operationModel.setSession(session);
+		operationModel.apply(item);
+		
+		OBOClass mergedObj = (OBOClass) session.getObject("Test:00060000");
+		Set<Dbxref> dbxrefs = mergedObj.getDbxrefs();
+		assertEquals(2, dbxrefs.size());
+		Set<String> expectedXrefs = new HashSet<String>(Arrays.asList("FOO:bar1", "FOO:bar2"));
+		for(Dbxref dbxref : dbxrefs) {
+			String id = dbxref.getDatabase() + ":" + dbxref.getDatabaseID();
+			assertTrue(expectedXrefs.contains(id));
+		}
+	}
+	
+	public void testMergeSynonymDbXrefs() throws Exception {
+		OBOClass master = (OBOClass) session.getObject("Test:00060002");
+		OBOClass slave = (OBOClass) session.getObject("Test:00060003");
+		
+		HistoryItem item = new TermMergeHistoryItem(master, slave);
+		operationModel = new DefaultOperationModel();
+		operationModel.setSession(session);
+		operationModel.apply(item);
+		OBOClass mergedObj = (OBOClass) session.getObject("Test:00060002");
+		Set<Synonym> synonyms = mergedObj.getSynonyms();
+		// two synonyms, the merged term label and the merged synonyms with the type
+		assertEquals(2, synonyms.size());
+		boolean found = false;
+		Set<String> expectedXrefs = new HashSet<String>(Arrays.asList("FOO:bar1", "FOO:bar2"));
+		for (Synonym synonym : synonyms) {
+			String text = synonym.getText();
+			if ("foo bar".equals(text)) {
+				Collection<Dbxref> xrefs = synonym.getXrefs();
+				assertEquals(2, xrefs.size());
+				for (Dbxref dbxref : xrefs) {
+					String id = dbxref.getDatabase() + ":" + dbxref.getDatabaseID();
+					assertTrue(expectedXrefs.contains(id));
+				}
+				found = true;
+				break;
 			}
 		}
 		assertTrue(found);
 	}
 
+	
+	public void testMergeDefXrefs() throws Exception {
+		OBOClass master = (OBOClass) session.getObject("Test:00060004");
+		OBOClass slave = (OBOClass) session.getObject("Test:00060005");
+		
+		HistoryItem item = new TermMergeHistoryItem(master, slave);
+		operationModel = new DefaultOperationModel();
+		operationModel.setSession(session);
+		operationModel.apply(item);
+		
+		OBOClass mergedObj = (OBOClass) session.getObject("Test:00060004");
+		Set<Dbxref> defDbxrefs = mergedObj.getDefDbxrefs();
+		assertEquals(2, defDbxrefs.size());
+	}
 }
