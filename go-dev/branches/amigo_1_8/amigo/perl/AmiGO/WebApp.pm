cc01f696aed322f0101ce1488a6735cbdd0b4746
sjcarbon <sjcarbon@6f0e8829-b336-0410-acfb-cb9b228023ad>
2014-02-25 23:41:28 +0000

    add a flexible page head notice/message and variables

--- go-dev/branches/amigo_1_8/amigo/perl/AmiGO/WebApp.pm
+++ go-dev/branches/amigo_1_8/amigo/perl/AmiGO/WebApp.pm
@@ -515,6 +515,9 @@ sub _common_params_settings {
   $params->{public_base} = $self->{CORE}->amigo_env('AMIGO_PUBLIC_CGI_URL');
   $params->{BETA} = $self->{CORE}->amigo_env('AMIGO_BETA') || 0;
   $params->{EOL} = int($self->{CORE}->amigo_env('AMIGO_EOL')) || 0;
+  $params->{NOTICE} = $self->{CORE}->amigo_env('AMIGO_NOTICE_SWITCH') || 0;
+  $params->{NOTICE_MESSAGE} =
+    $self->{CORE}->amigo_env('AMIGO_NOTICE_MESSAGE') || '';
   $params->{TROUBLE} = $self->{CORE}->amigo_env('AMIGO_TROUBLE_SWITCH') || 0;
   $params->{TROUBLE_MESSAGE} =
     $self->{CORE}->amigo_env('AMIGO_TROUBLE_MESSAGE') || '';
